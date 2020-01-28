aa088ceb99f62d5abcac55a6095a5cc6f1ee9027
Karl Glatz <karl@glatz.biz>
2020-01-28 15:04:24 +0100

    WEBP-169: improve radio button test; add count of input fields (WEBP-169: Frontend: rewrite single selection widget (selection))

--- test/unit/RadioButtons.test.js
+++ test/unit/RadioButtons.test.js
@@ -4,24 +4,26 @@ import RadioButtons from '~/ui/components/forms/RadioButtons';
 
 describe('RadioButtons.vue', () => {
     it('renders', () => {
+        let propsData = {
+            possibleValues: [{
+                id: 'test1',
+                text: 'test1'
+            }, {
+                id: 'test2',
+                text: 'test2'
+            }, {
+                id: 'test3',
+                text: 'test3'
+            }],
+            value: '',
+            placeholder: ''
+        };
         const wrapper = mount(RadioButtons, {
-            propsData: {
-                possibleValues: [{
-                    id: 'test1',
-                    text: 'test1'
-                }, {
-                    id: 'test2',
-                    text: 'test2'
-                }, {
-                    id: 'test3',
-                    text: 'test3'
-                }],
-                value: '',
-                placeholder: ''
-            }
+            propsData
         });
         expect(wrapper.html()).toBeTruthy();
         expect(wrapper.isVisible()).toBeTruthy();
+        expect(wrapper.findAll('input[type=radio]').length).toBe(propsData.possibleValues.length);
     });
 
     it('sets the values to the checked value', () => {
