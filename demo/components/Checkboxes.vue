cbe41ccdd60fbfaa419ef5c1ad266a9dd57b6b2a
Karl Glatz <karl@glatz.biz>
2020-02-14 11:30:30 +0100

    WEBP-169: fix raw code loader demo paths
    (WEBP-169: Frontend: rewrite single selection widget (selection))

--- demo/components/Checkboxes.vue
+++ demo/components/Checkboxes.vue
@@ -1,7 +1,7 @@
 <script>
 import CodeExample from './demo/CodeExample';
 import Checkboxes from '../../ui/components/forms/Checkboxes';
-import code from '!!raw-loader!../../ui/components/forms/Multiselect';
+import code from '!!raw-loader!../../ui/components/forms/Checkboxes';
 
 const codeExample = `<Checkboxes
   v-model="selected"
