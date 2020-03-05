63992580fe5ef9d22c37c3ab69126fb7415c9a64
Karl Glatz <karl@glatz.biz>
2020-03-05 10:25:54 +0100

    WEBP-169: add class to radio buttons to fix ellipises and more
    (WEBP-169: Frontend: rewrite single selection widget (selection))

--- ui/components/forms/RadioButtons.vue
+++ ui/components/forms/RadioButtons.vue
@@ -66,7 +66,7 @@ export default {
 
 <template>
   <div
-    :class="alignment"
+    :class="['radio-buttons', alignment]"
     role="radiogroup"
   >
     <label
@@ -91,91 +91,93 @@ export default {
 <style lang="postcss" scoped>
 @import "webapps-common/ui/css/variables";
 
-label {
-  position: relative;
-  font-weight: 300;
-  font-size: 14px;
-  line-height: 18px;
-  padding: 3px 0 3px 23px;
-
-  & input {
-    opacity: 0;
-    position: absolute;
-
-    & + span {
-      display: inline-block;
-      width: 100%;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
-
-    & + span::before { /* ◯ */
-      border: 1px solid var(--theme-color-stone-gray);
-      display: inline-block;
-      content: '';
-      width: 14px;
-      height: 14px;
-      border-radius: 100%;
-      left: 0;
-      top: 4px;
-      position: absolute;
-      vertical-align: top;
-      cursor: pointer;
-      text-align: center;
-    }
-
-    &:checked + span::before { /* 🔘 */
-      background: var(--theme-color-white);
-      border-color: var(--theme-color-masala);
-      content: '';
-      box-shadow: inset 0 0 0 4px var(--theme-color-masala);
-    }
-
-    /* checked labels are medium */
-    &:checked + span {
-      font-weight: 500;
-    }
-  }
-
-  /* keyboard focus; :focus-visible would be better once browser support
-       is there https://caniuse.com/#feat=css-focus-visible */
-  & input:focus + span::before { /* ◯ */
-    background: var(--theme-color-porcelain);
-  }
-
-  /* hover state */
-  &:hover input + span::before { /* ◯ */
-    background: var(--theme-color-porcelain);
-  }
-
-  /* keyboard focus; :focus-visible would be better once browser support
-     is there https://caniuse.com/#feat=css-focus-visible */
-  & input:checked:focus + span::before { /* 🔘 */
-    background: var(--theme-color-masala);
-    box-shadow: inset 0 0 0 4px var(--theme-color-porcelain);
-  }
-
-  /* hover state  checked */
-  &:hover input:checked + span::before { /* 🔘 */
-    background: var(--theme-color-masala);
-    box-shadow: inset 0 0 0 4px var(--theme-color-porcelain);
-  }
-}
-
-.vertical label {
-  display: block;
-}
-
-.horizontal {
-  display: flex;
-  flex-wrap: wrap;
-
+.radio-buttons {
   & label {
-    display: block;
-    min-width: 0; /* sizing and text overflow with flexbox - see https://stackoverflow.com/a/26535469 */
+    position: relative;
+    font-weight: 300;
+    font-size: 14px;
+    line-height: 18px;
+    padding: 3px 0 3px 23px;
 
-    &:not(:last-of-type) {
-      padding-right: 12px;
+    & input {
+      opacity: 0;
+      position: absolute;
+
+      & + span {
+        display: inline-block;
+        width: 100%;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+
+      & + span::before { /* ◯ */
+        border: 1px solid var(--theme-color-stone-gray);
+        display: inline-block;
+        content: '';
+        width: 14px;
+        height: 14px;
+        border-radius: 100%;
+        left: 0;
+        top: 4px;
+        position: absolute;
+        vertical-align: top;
+        cursor: pointer;
+        text-align: center;
+      }
+
+      &:checked + span::before { /* 🔘 */
+        background: var(--theme-color-white);
+        border-color: var(--theme-color-masala);
+        content: '';
+        box-shadow: inset 0 0 0 4px var(--theme-color-masala);
+      }
+
+      /* checked labels are medium */
+      &:checked + span {
+        font-weight: 500;
+      }
+    }
+
+    /* keyboard focus; :focus-visible would be better once browser support
+         is there https://caniuse.com/#feat=css-focus-visible */
+    & input:focus + span::before { /* ◯ */
+      background: var(--theme-color-porcelain);
+    }
+
+    /* hover state */
+    &:hover input + span::before { /* ◯ */
+      background: var(--theme-color-porcelain);
+    }
+
+    /* keyboard focus; :focus-visible would be better once browser support
+       is there https://caniuse.com/#feat=css-focus-visible */
+    & input:checked:focus + span::before { /* 🔘 */
+      background: var(--theme-color-masala);
+      box-shadow: inset 0 0 0 4px var(--theme-color-porcelain);
+    }
+
+    /* hover state  checked */
+    &:hover input:checked + span::before { /* 🔘 */
+      background: var(--theme-color-masala);
+      box-shadow: inset 0 0 0 4px var(--theme-color-porcelain);
+    }
+  }
+
+  &.vertical label {
+    display: block;
+  }
+
+  &.horizontal {
+    display: flex;
+    flex-wrap: wrap;
+
+    & label {
+      display: block;
+      min-width: 0; /* sizing and text overflow with flexbox - see https://stackoverflow.com/a/26535469 */
+
+      &:not(:last-of-type) {
+        padding-right: 12px;
+      }
     }
   }
 }
