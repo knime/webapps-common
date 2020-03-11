bbaa2939e6c24b8b7f79aaadff6d0b43d4009b6e
Karl Glatz <karl@glatz.biz>
2020-03-11 15:07:43 +0100

    WEBP-167: add more test coverage for Checkboxes
    (WEBP-167: Frontend: rewrite mutliple selection widget (selection))

--- test/unit/Checkboxes.test.js
+++ test/unit/Checkboxes.test.js
@@ -1,4 +1,4 @@
-import { mount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 
 import Checkboxes from '~/ui/components/forms/Checkboxes';
 import Checkbox from '~/ui/components/forms/Checkbox';
@@ -16,7 +16,6 @@ describe('Checkboxes.vue', () => {
                 id: 'test3',
                 text: 'test3'
             }],
-            value: []
         };
         const wrapper = mount(Checkboxes, {
             propsData
@@ -24,6 +23,7 @@ describe('Checkboxes.vue', () => {
         expect(wrapper.html()).toBeTruthy();
         expect(wrapper.isVisible()).toBeTruthy();
         expect(wrapper.findAll(Checkbox).length).toBe(propsData.possibleValues.length);
+        expect(wrapper.vm.hasSelection()).toBe(false);
     });
 
     it('sets the values to the checked value', () => {
@@ -43,8 +43,23 @@ describe('Checkboxes.vue', () => {
             }
         });
         let input = wrapper.findAll(Checkbox).at(1);
+        // test check
         input.vm.onInput({ target: { checked: true } });
         expect(wrapper.emitted().input[0][0]).toStrictEqual(['test1', 'test2']);
+
+        // test uncheck
+        input.vm.onInput({ target: { checked: false } });
+        expect(wrapper.emitted().input[1][0]).toStrictEqual(['test1']);
+    });
+
+    it('validation of possibleValues', () => {
+        const wrapper = shallowMount(Checkboxes);
+
+        const propPossibleValues = wrapper.vm.$options.props.possibleValues;
+        expect(propPossibleValues.required).toBeFalsy();
+        expect(propPossibleValues.type).toBe(Array);
+        expect(propPossibleValues.validator && propPossibleValues.validator('str')).toBeFalsy();
+        expect(propPossibleValues.validator && propPossibleValues.validator([])).toBeTruthy();
     });
 
 });
