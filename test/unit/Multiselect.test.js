c9ccb1ff54afd46d40b4b8f1b81723bfb731a840
Karl Glatz <karl@glatz.biz>
2020-03-23 10:47:46 +0100

    WEBP-167: add key nav tests for Multiselect component (not used in widgets)
    (WEBP-167: Frontend: rewrite mutliple selection widget (selection))

--- test/unit/Multiselect.test.js
+++ test/unit/Multiselect.test.js
@@ -105,6 +105,48 @@ describe('Multiselect.vue', () => {
         expect(wrapper.vm.checkedValue).toHaveLength(0);
     });
 
+    describe('keyboard interaction', () => {
+        it('show options on enter', () => {
+            const wrapper = mount(Multiselect, {
+                propsData: {
+                    possibleValues: [{
+                        id: 'test1',
+                        text: 'test1'
+                    }, {
+                        id: 'test2',
+                        text: 'test2'
+                    }, {
+                        id: 'test3',
+                        text: 'test3'
+                    }]
+                }
+            });
+            let button = wrapper.find('[role=button]');
+            button.trigger('keydown.enter');
+            expect(wrapper.vm.collapsed).toBe(false);
+        });
+        it('hide options on esc', () => {
+            const wrapper = mount(Multiselect, {
+                propsData: {
+                    possibleValues: [{
+                        id: 'test1',
+                        text: 'test1'
+                    }, {
+                        id: 'test2',
+                        text: 'test2'
+                    }, {
+                        id: 'test3',
+                        text: 'test3'
+                    }]
+                }
+            });
+            let button = wrapper.find('[role=button]');
+            wrapper.vm.collapsed = false;
+            button.trigger('keydown.esc');
+            expect(wrapper.vm.collapsed).toBe(true);
+        });
+    });
+
     it('uses the title text until options have been selected', () => {
         const wrapper = mount(Multiselect, {
             propsData: {
