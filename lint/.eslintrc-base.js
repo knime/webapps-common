e13fc362ca06f730780ad83095262d63a10ed605
Thomas Rosenau <thomas.rosenau@knime.com>
2020-09-23 10:18:05 +0200

    Fix lints

--- lint/.eslintrc-base.js
+++ lint/.eslintrc-base.js
@@ -83,7 +83,7 @@ module.exports = {
         'no-extend-native': 'warn',
         'no-extra-boolean-cast': 'warn',
         'no-extra-label': 'error',
-        'no-extra-parens': ['warn', 'all', { "nestedBinaryExpressions": false }],
+        'no-extra-parens': ['warn', 'all', { nestedBinaryExpressions: false }],
         'no-implicit-coercion': 'warn',
         'no-implied-eval': 'error',
         'no-invalid-this': 'warn',
@@ -214,7 +214,7 @@ module.exports = {
         rules: {
             'no-magic-numbers': 'off'
         }
-    },{
+    }, {
         files: ['*.test.js'],
         rules: {
             'no-undefined': 'off'
