1b6821e97097aa139f8f5fb781d768e809931381
Thomas Rosenau <thomas.rosenau@knime.com>
2020-10-08 16:09:29 +0200

    NXT-296: Normalize color demo page
    NXT-296 (Display workflow metadata/description)

--- demo/components/Colors.vue
+++ demo/components/Colors.vue
@@ -20,8 +20,8 @@ export default {
       <div class="grid-item-12">
         <h2>Colors</h2>
         <p>The following colors are pre-defined:</p>
-        
-        <h4>Primary Colors from the style guide</h4>
+
+        <h4>Primary Colors from the corporate style guide</h4>
         <div class="demo">
           <div style="--bg: var(--knime-yellow);">--knime-yellow</div>
           <div style="--bg: var(--knime-masala); --font: white;">--knime-masala</div>
@@ -30,8 +30,8 @@ export default {
           <div style="--bg: var(--knime-porcelain);">--knime-porcelain</div>
           <div style="--bg: var(--knime-white);">--knime-white</div>
         </div>
-        
-        <h4>Additional colors</h4>
+
+        <h4>Additional colors not defined in the corporate style guide</h4>
         <div class="demo">
           <div style="--bg: var(--knime-black); --font: white;">--knime-black</div>
           <div style="--bg: var(--knime-masala-semi); --font: white;">--knime-masala-semi</div>
@@ -42,66 +42,70 @@ export default {
           <div style="--bg: var(--knime-gray-light-semi);">--knime-gray-light-semi</div>
           <div style="--bg: var(--knime-gray-ultra-light);">--knime-gray-ultra-light</div>
         </div>
-        
-        <h4>Secondary colors from the style guide</h4>
-        <ul class="color-list">
-          <li style="--bg: var(--knime-avocado-light);">--knime-avocado-light</li>
-          <li style="--bg: var(--knime-avocado);">--knime-avocado</li>
-          <li style="--bg: var(--knime-avocado-dark); --font: white;">--knime-avocado-dark</li>
-          <li style="--bg: var(--knime-meadow-light);">--knime-meadow-light</li>
-          <li style="--bg: var(--knime-meadow);">--knime-meadow</li>
-          <li style="--bg: var(--knime-meadow-dark); --font: white;">--knime-meadow-dark</li>
-          <li style="--bg: var(--knime-petrol-light);">--knime-petrol-light</li>
-          <li style="--bg: var(--knime-petrol);">--knime-petrol</li>
-          <li style="--bg: var(--knime-petrol-dark); --font: white;">--knime-petrol-dark</li>
-          <li style="--bg: var(--knime-aquamarine-light);">--knime-aquamarine-light</li>
-          <li style="--bg: var(--knime-aquamarine);">--knime-aquamarine</li>
-          <li style="--bg: var(--knime-aquamarine-dark); --font: white;">--knime-aquamarine-dark</li>
-          <li style="--bg: var(--knime-cornflower-light);">--knime-cornflower-light</li>
-          <li style="--bg: var(--knime-cornflower); --font: white;">--knime-cornflower</li>
-          <li style="--bg: var(--knime-cornflower-dark); --font: white;">--knime-cornflower-dark</li>
-          <li style="--bg: var(--knime-lavender-light);">--knime-lavender-light</li>
-          <li style="--bg: var(--knime-lavender); --font: white;">--knime-lavender</li>
-          <li style="--bg: var(--knime-lavender-dark); --font: white;">--knime-lavender-dark</li>
-          <li style="--bg: var(--knime-hibiscus-light);">--knime-hibiscus-light</li>
-          <li style="--bg: var(--knime-hibiscus);">--knime-hibiscus</li>
-          <li style="--bg: var(--knime-hibiscus-dark); --font: white;">--knime-hibiscus-dark</li>
-          <li style="--bg: var(--knime-coral-light);">--knime-coral-light</li>
-          <li style="--bg: var(--knime-coral); --font: white;">--knime-coral</li>
-          <li style="--bg: var(--knime-coral-dark); --font: white;">--knime-coral-dark</li>
-          <li style="--bg: var(--knime-carrot-light);">--knime-carrot-light</li>
-          <li style="--bg: var(--knime-carrot); --font: white;">--knime-carrot</li>
-          <li style="--bg: var(--knime-carrot-dark); --font: white;">--knime-carrot-dark</li>
-          <li style="--bg: var(--knime-wood-light);">--knime-wood-light</li>
-          <li style="--bg: var(--knime-wood);">--knime-wood</li>
-          <li style="--bg: var(--knime-wood-dark); --font: white;">--knime-wood-dark</li>
-          <li style="--bg: var(--knime-stone-light);">--knime-stone-light</li>
-          <li style="--bg: var(--knime-stone);">--knime-stone</li>
-          <li style="--bg: var(--knime-stone-dark); --font: white;">--knime-stone-dark</li>
-        </ul>
+
+        <h4>Secondary colors from the corporate style guide</h4>
+        <div class="demo">
+          <div style="--bg: var(--knime-avocado-light);">--knime-avocado-light</div>
+          <div style="--bg: var(--knime-avocado);">--knime-avocado</div>
+          <div style="--bg: var(--knime-avocado-dark); --font: white;">--knime-avocado-dark</div>
+          <div style="--bg: var(--knime-meadow-light);">--knime-meadow-light</div>
+          <div style="--bg: var(--knime-meadow); --font: white;">--knime-meadow</div>
+          <div style="--bg: var(--knime-meadow-dark); --font: white;">--knime-meadow-dark</div>
+          <div style="--bg: var(--knime-petrol-light);">--knime-petrol-light</div>
+          <div style="--bg: var(--knime-petrol); --font: white;">--knime-petrol</div>
+          <div style="--bg: var(--knime-petrol-dark); --font: white;">--knime-petrol-dark</div>
+          <div style="--bg: var(--knime-aquamarine-light);">--knime-aquamarine-light</div>
+          <div style="--bg: var(--knime-aquamarine);">--knime-aquamarine</div>
+          <div style="--bg: var(--knime-aquamarine-dark); --font: white;">--knime-aquamarine-dark</div>
+          <div style="--bg: var(--knime-cornflower-light);">--knime-cornflower-light</div>
+          <div style="--bg: var(--knime-cornflower); --font: white;">--knime-cornflower</div>
+          <div style="--bg: var(--knime-cornflower-dark); --font: white;">--knime-cornflower-dark</div>
+          <div style="--bg: var(--knime-lavender-light);">--knime-lavender-light</div>
+          <div style="--bg: var(--knime-lavender); --font: white;">--knime-lavender</div>
+          <div style="--bg: var(--knime-lavender-dark); --font: white;">--knime-lavender-dark</div>
+          <div style="--bg: var(--knime-hibiscus-light);">--knime-hibiscus-light</div>
+          <div style="--bg: var(--knime-hibiscus); --font: white;">--knime-hibiscus</div>
+          <div style="--bg: var(--knime-hibiscus-dark); --font: white;">--knime-hibiscus-dark</div>
+          <div style="--bg: var(--knime-coral-light);">--knime-coral-light</div>
+          <div style="--bg: var(--knime-coral); --font: white;">--knime-coral</div>
+          <div style="--bg: var(--knime-coral-dark); --font: white;">--knime-coral-dark</div>
+          <div style="--bg: var(--knime-carrot-light);">--knime-carrot-light</div>
+          <div style="--bg: var(--knime-carrot); --font: white;">--knime-carrot</div>
+          <div style="--bg: var(--knime-carrot-dark); --font: white;">--knime-carrot-dark</div>
+          <div style="--bg: var(--knime-wood-light);">--knime-wood-light</div>
+          <div style="--bg: var(--knime-wood); --font: white;">--knime-wood</div>
+          <div style="--bg: var(--knime-wood-dark); --font: white;">--knime-wood-dark</div>
+          <div style="--bg: var(--knime-stone-light);">--knime-stone-light</div>
+          <div style="--bg: var(--knime-stone); --font: white;">--knime-stone</div>
+          <div style="--bg: var(--knime-stone-dark); --font: white;">--knime-stone-dark</div>
+        </div>
 
         <h4>Node Backgorund Colors</h4>
-        <p>To reference the colors use <code>--knime-node-my-node-type</code></p>
-        <div class="node-demo">
-          <div style="--bg: var(--knime-node-component);">Component</div>
-          <div style="--bg: var(--knime-node-container);">Container</div>
-          <div style="--bg: var(--knime-node-learner);">Learner</div>
-          <div style="--bg: var(--knime-node-loop-start);">Loop Start</div>
-          <div style="--bg: var(--knime-node-loop-end);">Loop End</div>
-          <div style="--bg: var(--knime-node-manipulator);">Manipulator</div>
-          <div style="--bg: var(--knime-node-metanode); --font: white;">Metanode</div>
-          <div style="--bg: var(--knime-node-other);">Other</div>
-          <div style="--bg: var(--knime-node-predictor);">Predictor</div>
-          <div style="--bg: var(--knime-node-quick-form);">Quick Form</div>
-          <div style="--bg: var(--knime-node-scope-start); --font: white;">Scope Start</div>
-          <div style="--bg: var(--knime-node-scope-end); --font: white;">Scope End</div>
-          <div style="--bg: var(--knime-node-sink); --font: white;">Sink</div>
-          <div style="--bg: var(--knime-node-source); --font: white;">Source</div>
-          <div style="--bg: var(--knime-node-virtual-in);">Virtual in</div>
-          <div style="--bg: var(--knime-node-virtual-out);">Virtual out</div>
-          <div style="--bg: var(--knime-node-visualizer); --font: white;">Visualizer</div>
+        <p>
+          These are aliases for the secondary colors defined above.
+        </p>
+        <div class="demo node">
+          <div style="--bg: var(--knime-node-component);">--knime-node-component</div>
+          <div style="--bg: var(--knime-node-container);">--knime-node-container</div>
+          <div style="--bg: var(--knime-node-learner);">--knime-node-learner</div>
+          <div style="--bg: var(--knime-node-loop-start);">--knime-node-loop-start</div>
+          <div style="--bg: var(--knime-node-loop-end);">--knime-node-loop-end</div>
+          <div style="--bg: var(--knime-node-manipulator);">--knime-node-manipulator</div>
+          <div style="--bg: var(--knime-node-metanode);">--knime-node-metanode</div>
+          <div style="--bg: var(--knime-node-other);">--knime-node-other</div>
+          <div style="--bg: var(--knime-node-predictor);">--knime-node-predictor</div>
+          <div style="--bg: var(--knime-node-quick-form);">--knime-node-quick-form</div>
+          <div style="--bg: var(--knime-node-scope-start);">--knime-node-scope-start</div>
+          <div style="--bg: var(--knime-node-scope-end);">--knime-node-scope-end</div>
+          <div style="--bg: var(--knime-node-sink);">--knime-node-sink</div>
+          <div style="--bg: var(--knime-node-source);">--knime-node-source</div>
+          <div style="--bg: var(--knime-node-virtual-in);">--knime-node-virtual-in</div>
+          <div style="--bg: var(--knime-node-virtual-out);">--knime-node-virtual-out</div>
+          <div style="--bg: var(--knime-node-visualizer);">--knime-node-visualizer</div>
+          <div style="--bg: var(--knime-node-widget);">--knime-node-widget</div>
         </div>
-        
+
+        <h4>Theme Colors</h4>
         <p>The following `--theme` colors should be used in place of '--knime' variables if theming is desired:</p>
         <div class="demo">
           <div style="--bg: var(--theme-color-error); --font: white;">--theme-color-error</div>
@@ -144,7 +148,7 @@ export default {
   flex-wrap: wrap;
   margin: -3px;
 
-  & > * {
+  & > div {
     --bg: none;
     --font: black;
 
@@ -161,22 +165,25 @@ export default {
     white-space: pre-wrap;
     margin: 4px;
   }
-}
 
-.node-demo {
-  display: flex;
-  flex-wrap: wrap;
+  &.node > div {
+    --bg: none;
 
-  & > * {
-    width: 74px;
-    height: 74px;
+    padding-bottom: 0;
+    background: transparent;
     border-radius: 2px;
-    padding: 4px;
-    background: var(--bg);
-    color: var(--font);
-    font-size: 18px;
-    word-wrap: break-word;
-    margin: 4px;
+    border: none;
+    height: auto;
+
+    &::before {
+      content: '';
+      display: block;
+      margin: auto;
+      width: 5vmin;
+      height: 5vmin;
+      border-radius: 0.5vmin;
+      background: var(--bg);
+    }
   }
 }
 </style>
