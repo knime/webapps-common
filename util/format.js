3951552664c57e90c86a9959b58c96f037ef1e62
Philipp Schimmelfennig <philipp.schimmelfennig@knime.com>
2020-10-12 16:38:22 +0200

    NXT-296: Move not shared functions back to Hub
    NXT-296 (Display workflow metadata/description)

--- util/format.js
+++ util/format.js
@@ -1,37 +1,6 @@
-export const formatVersionNumber = function (version) {
-    let match = version.match(/^\d+\.\d+\.\d+/);
-    if (match) {
-        return match[0];
-    }
-    return version;
-};
-
-/**
- * Group digits in groups of three, separated by a thin space
- * @param {Number} number The number to format
- * @param {Boolean} padding `true` to pad with zeroes. Defaults to false
- * @returns {String} formatted string
-*/
-export const groupDigits = (number, padding = false) => {
-    if (padding) {
-        if (number < 10) { // eslint-disable-line no-magic-numbers
-            return `00${number}`;
-        }
-        if (number < 100) {
-            return `0${number}`;
-        }
-    }
-    if (number < 1000) {
-        return String(number);
-    }
-    let lastGroup = number % 1000;
-    let firstGroup = (number - lastGroup) / 1000;
-    return `${groupDigits(firstGroup)}\u{2009}${groupDigits(lastGroup, true)}`;
-};
-
 /**
  * Converts the specified date String to human readable text containing the day, month, and year.
- * Example: '2018-07-31T09:44:31+00:00' => '31. Jul 2018'
+ * Example: '2018-07-31T09:44:31+00:00' => '31 Jul 2018'
  *
  * @param {String} dateString String representation of the Date to format
  * @returns {String} human readable date String
