40fcd35b0624feb8ddeb29b64916b9c850874825
Philipp Schimmelfennig <philipp.schimmelfennig@knime.com>
2020-10-20 15:27:39 +0200

    NXT-297: Unify NodePreview between KnimeUI and Hub; Move NodeTorsoNormal from KnimeUI
    NXT-297 (Display component metadata / description)

--- ui/components/node/NodePreview.vue
+++ ui/components/node/NodePreview.vue
@@ -1,26 +1,14 @@
 <script>
 import PortIcon from './PortIcon';
-import backgroundFillColors from '../../colors/nodeColors';
-
-const backgroundPaths = {
-    // paths extracted from
-    // https://bitbucket.org/KNIME/knime-workbench/src/bb437123743b220db34e39e4ffd887a307276ff8/org.knime.workbench.editor/icons/node/*.svg
-    default: 'M0,29.2L0,2.8C0,1.3,1.3,0,2.8,0l26.3,0C30.7,0,32,1.3,32,2.8v26.3c0,1.6-1.3,2.8-2.8,2.8H2.8C1.3,32,0,30.' +
-        '7,0,29.2z',
-    LoopEnd: 'M32,2.8v26.3c0,1.6-1.3,2.8-2.8,2.8H4L0,16.1L4,0l25.2,0C30.7,0,32,1.3,32,2.8z',
-    LoopStart: 'M0,29.2L0,2.8C0,1.3,1.3,0,2.8,0L32,0l-4,15.9L32,32H2.8C1.3,32,0,30.7,0,29.2z',
-    ScopeEnd: 'M32,2.8v26.3c0,1.6-1.3,2.8-2.8,2.8H4L0,16.1L4,0l25.2,0C30.7,0,32,1.3,32,2.8z',
-    ScopeStart: 'M0,29.2L0,2.8C0,1.3,1.3,0,2.8,0L32,0l-4,15.9L32,32H2.8C1.3,32,0,30.7,0,29.2z',
-    VirtualIn: 'M32,2.8v26.3c0,1.6-1.3,2.8-2.8,2.8H6.5L0,25.9l5.2-10L0.7,7.2L6.5,0l22.7,0C30.7,0,32,1.3,32,2.8z',
-    VirtualOut: 'M0,29.2L0,2.8C0,1.3,1.3,0,2.8,0L32,0l-5.8,7.2l4.5,8.7l-5.2,10L32,32H2.8C1.3,32,0,30.7,0,29.2z'
-};
+import NodeTorsoNormal from './NodeTorsoNormal';
 
 /**
  * SVG icon preview for a node or component, generated from its attributes
  */
 export default {
     components: {
-        PortIcon
+        PortIcon,
+        NodeTorsoNormal
     },
     /** Hub-Format of Ports expected */
     props: {
@@ -28,9 +16,9 @@ export default {
          * Type of node; determines the background color.
          * @example 'Reader'
          */
-        nodeType: {
+        type: {
             type: String,
-            default: ''
+            default: null
         },
         /**
          * Use alternative rendering style:
@@ -63,10 +51,12 @@ export default {
         },
         /**
          * URL of icon that is rendered inside the node. (Possibly `data:` URL)
+         * Passed through to NodeTorsoNormal
          */
         icon: {
             type: String,
-            default: ''
+            default: null,
+            validator: url => url.startsWith('data:image/')
         }
     },
     data() {
@@ -79,28 +69,6 @@ export default {
             */
             viewBox: '-19 -19 70 70'
         };
-    },
-    computed: {
-        unknownNodeColor() {
-            return '#DC2C87'; // "unknown"
-        },
-        backgroundPath() {
-            return backgroundPaths[this.nodeType] || backgroundPaths.default;
-        },
-        backgroundFill() {
-            // Components always have a gray background
-            if (this.isComponent) {
-                return backgroundFillColors.Component;
-            }
-            return backgroundFillColors[this.nodeType] || this.unknownNodeColor;
-        },
-        innerFill() {
-            // Nodes have no inner fill
-            if (!this.isComponent) {
-                return null;
-            }
-            return backgroundFillColors[this.nodeType];
-        }
     },
     methods: {
         // top edge of port icon relative to 32x32 background
@@ -126,36 +94,27 @@ export default {
 
 <template>
   <svg :viewBox="viewBox">
-    <path
-      :d="backgroundPath"
-      :fill="backgroundFill"
+    <NodeTorsoNormal
+      :type="type"
+      :icon="icon"
+      :is-component="isComponent"
     />
-    <path
-      v-if="innerFill"
-      :d="backgroundPath"
-      :fill="innerFill"
-      transform="translate(16 16) scale(.75) translate(-16 -16)"
+    <PortIcon
+      v-for="(port, index) in inPorts"
+      :key="`in-${index}`"
+      :color="port.color"
+      :filled="!port.optional"
+      :data-type="port.dataType"
+      :transform="`translate(-4.5, ${ yPortShift(index, inPorts.length) })`"
     />
-    <g transform="translate(-5)">
-      <PortIcon
-        v-for="(port, index) in inPorts"
-        :key="index"
-        :color="port.color"
-        :filled="!port.optional"
-        :data-type="port.dataType"
-        :transform="`translate(0, ${ yPortShift(index, inPorts.length) })`"
-      />
-    </g>
-    <g transform="translate(37)">
-      <PortIcon
-        v-for="(port, index) in outPorts"
-        :key="index"
-        :color="port.color"
-        :filled="!port.optional"
-        :data-type="port.dataType"
-        :transform="`translate(0, ${ yPortShift(index, outPorts.length) })`"
-      />
-    </g>
+    <PortIcon
+      v-for="(port, index) in outPorts"
+      :key="`out-${index}`"
+      :color="port.color"
+      :filled="!port.optional"
+      :data-type="port.dataType"
+      :transform="`translate(36.5, ${ yPortShift(index, outPorts.length) })`"
+    />
     <g
       v-if="hasDynPorts"
       stroke="none"
@@ -176,13 +135,5 @@ export default {
         cy="27"
       />
     </g>
-    <image
-      v-if="icon"
-      x="8"
-      y="8"
-      width="16"
-      height="16"
-      :xlink:href="icon"
-    />
   </svg>
 </template>
---
+++ ui/components/node/NodeTorsoNormal.vue
@@ -0,0 +1,86 @@
+<script>
+import nodeBackgroundColors from '../../colors/nodeColors';
+import knimeColors from '../../colors/knimeColors';
+
+const nodeSize = 32;
+const backgroundPaths = {
+    default: 'M0,29.2L0,2.8C0,1.3,1.3,0,2.8,0l26.3,0C30.7,0,32,1.3,32,2.8v26.3c0,1.6-1.3,2.8-2.8,2.8H2.8C1.3,32,0,30.' +
+        '7,0,29.2z',
+    LoopEnd: 'M32,2.8v26.3c0,1.6-1.3,2.8-2.8,2.8H4L0,16.1L4,0l25.2,0C30.7,0,32,1.3,32,2.8z',
+    LoopStart: 'M0,29.2L0,2.8C0,1.3,1.3,0,2.8,0L32,0l-4,15.9L32,32H2.8C1.3,32,0,30.7,0,29.2z',
+    ScopeEnd: 'M32,2.8v26.3c0,1.6-1.3,2.8-2.8,2.8H4L0,16.1L4,0l25.2,0C30.7,0,32,1.3,32,2.8z',
+    ScopeStart: 'M0,29.2L0,2.8C0,1.3,1.3,0,2.8,0L32,0l-4,15.9L32,32H2.8C1.3,32,0,30.7,0,29.2z',
+    VirtualIn: 'M32,2.8v26.3c0,1.6-1.3,2.8-2.8,2.8H6.5L0,25.9l5.2-10L0.7,7.2L6.5,0l22.7,0C30.7,0,32,1.3,32,2.8z',
+    VirtualOut: 'M0,29.2L0,2.8C0,1.3,1.3,0,2.8,0L32,0l-5.8,7.2l4.5,8.7l-5.2,10L32,32H2.8C1.3,32,0,30.7,0,29.2z'
+};
+export const componentBackgroundPortion = 0.75;
+
+export default {
+    props: {
+        /**
+         * Node type, e.g. "Learner", "Visualizer"
+         * Is undefined for MetaNodes
+         */
+        type: { type: String, default: null },
+        /**
+         * Differentiate between a native node and a component node
+         */
+        isComponent: {
+            type: Boolean,
+            default: false
+        },
+        /**
+         * data-url containing Base64-encoded icon
+         */
+        icon: {
+            type: String,
+            default: null,
+            validator: url => url.startsWith('data:image/')
+        }
+    },
+    computed: {
+        backgroundPath() {
+            return backgroundPaths[this.type] || backgroundPaths.default;
+        },
+        backgroundColor() {
+            // In case of unknown type, use Hibiscus Dark
+            return nodeBackgroundColors[this.type] || knimeColors.HibiscusDark;
+        },
+        componentColor() {
+            return nodeBackgroundColors.Component;
+        },
+        componentBackgroundTransformation() {
+            let offset = nodeSize / 2;
+            let scaleFactor = componentBackgroundPortion;
+            return `translate(${offset}, ${offset}) scale(${scaleFactor}) translate(-${offset}, -${offset})`;
+        }
+    }
+};
+</script>
+
+<template>
+  <g>
+    <path
+      class="bg"
+      :d="backgroundPath"
+      :fill="isComponent ? componentColor : backgroundColor"
+    />
+    <!-- components may have two layers of background. This is the inner part, a shrunk version of the outer frame -->
+    <path
+      v-if="isComponent && type"
+      class="bg"
+      :d="backgroundPath"
+      :fill="backgroundColor"
+      :transform="componentBackgroundTransformation"
+    />
+    <image
+      v-if="icon"
+      :xlink:href="icon"
+      x="8"
+      y="8"
+      width="16"
+      height="16"
+      pointer-events="none"
+    />
+  </g>
+</template>
