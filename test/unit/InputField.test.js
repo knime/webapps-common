576339114e898a25fd35775888f5707e133ae6fa
David Kolb <david.kolb@knime.com>
2020-12-22 10:49:57 +0100

    HUB-1531: Fix comment
    HUB-1531 (UI: Cant delete space with special characters)

--- test/unit/InputField.test.js
+++ test/unit/InputField.test.js
@@ -90,7 +90,8 @@ describe('InputField.vue', () => {
 
     /**
      * This fails due to bug in jsdom https://github.com/jsdom/jsdom/issues/2494
-     * The bug is fixed with jest 16.4.0, the test can be reactivated when we upgrade
+     * The bug is fixed with jest 26.5.0 https://github.com/facebook/jest/pull/10578
+     * The test can be reactivated when we upgrade
      */
     xit('invalidates wrong unicode pattern', () => {
         const wrapper = mount(InputField, {
