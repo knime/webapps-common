2561b6a0625fae48c3023ef973e875bcc551a07e
Thomas Rosenau <thomas.rosenau@knime.com>
2021-03-12 15:25:30 +0100

    HUB-1642: Fix svg loader for Node14 usage
    HUB-1642 (Update Node to 14 LTM)

--- webpack/webpack.svg.config.js
+++ webpack/webpack.svg.config.js
@@ -1,32 +1,32 @@
 /*
-Custom image rule for SVGs including optimization (inspired by https://github.com/sam3d/nuxt-svg)
+ Custom image rule for SVGs including optimization (inspired by https://github.com/sam3d/nuxt-svg)
 
-Example usage in Nuxt nuxt.config.js:
-    import svgConfig from '~/webapps-common/webpack/webpack.svg.config';
-    export default {
-        build: {
-            extend(config) {
-                // remove Nuxt's default svg loader
-                const imgRule = config.module.rules.find(
-                    rule => String(rule.test) === String(/\.(png|jpe?g|gif|svg|webp)$/i)
-                );
-                imgRule.test = /\.(png|jpe?g|gif|webp)$/i;
+ Example usage in Nuxt nuxt.config.js:
+ import svgConfig from '~/webapps-common/webpack/webpack.svg.config';
+ export default {
+ build: {
+ extend(config) {
+ // remove Nuxt's default svg loader
+ const imgRule = config.module.rules.find(
+ rule => String(rule.test) === String(/\.(png|jpe?g|gif|svg|webp)$/i)
+ );
+ imgRule.test = /\.(png|jpe?g|gif|webp)$/i;
 
-                // add our svg loader
-                config.module.rules.push(svgConfig);
-            }
-        }
-    };
+ // add our svg loader
+ config.module.rules.push(svgConfig);
+ }
+ }
+ };
 
-Example usage in Vue postcss.config.js:
-    const svgConfig = require('webapps-common/webpack/webpack.svg.config');
-    module.exports = {
-        chainWebpack: config => {
-            config.module.rule('svg').uses.clear();
-            config.merge({ module: { rule: { svg: svgConfig } } });
-        }
-    };
-*/
+ Example usage in Vue postcss.config.js:
+ const svgConfig = require('webapps-common/webpack/webpack.svg.config');
+ module.exports = {
+ chainWebpack: config => {
+ config.module.rule('svg').uses.clear();
+ config.merge({ module: { rule: { svg: svgConfig } } });
+ }
+ };
+ */
 
 const svgoOptions = {
     plugins: [
@@ -38,34 +38,29 @@ module.exports = {
     test: /\.svg$/i,
     oneOf: [{
         resourceQuery: /\?inline$/,
-        use: [
-            {
-                loader: 'vue-svg-loader',
-                options: {
-                    svgo: svgoOptions
-                }
+        use: [{
+            loader: 'vue-svg-loader',
+            options: {
+                svgo: svgoOptions
             }
-        ]
+        }]
     }, {
         resourceQuery: /\?data$/,
-        use: [
-            {
-                loader: 'url-loader'
-            },
-            {
-                loader: 'svgo-loader',
-                options: svgoOptions
-            }
-        ]
+        use: [{
+            loader: 'url-loader'
+        }, {
+            loader: 'svgo-loader',
+            options: svgoOptions
+        }]
     }, {
-        use: [
-            {
-                loader: 'file-loader?name=img/[name].[hash:7].[ext]'
-            },
-            {
-                loader: 'svgo-loader',
-                options: svgoOptions
+        use: [{
+            loader: 'file-loader?name=img/[name].[hash:7].[ext]',
+            options: {
+                esModule: false
             }
-        ]
+        }, {
+            loader: 'svgo-loader',
+            options: svgoOptions
+        }]
     }]
 };
