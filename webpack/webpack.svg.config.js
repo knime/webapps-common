47878df298717859d4008332a99c2cfac1db573c
Jakob Schröter <jakob.schroeter@knime.com>
2021-03-19 13:58:16 +0100

    HUB-1642: Fix code formatting
    HUB-1642 (Update Node to 14 LTM)

--- webpack/webpack.svg.config.js
+++ webpack/webpack.svg.config.js
@@ -1,32 +1,32 @@
 /*
- Custom image rule for SVGs including optimization (inspired by https://github.com/sam3d/nuxt-svg)
+Custom image rule for SVGs including optimization (inspired by https://github.com/sam3d/nuxt-svg)
 
- Example usage in Nuxt nuxt.config.js:
- import svgConfig from '~/webapps-common/webpack/webpack.svg.config';
- export default {
- build: {
- extend(config) {
- // remove Nuxt's default svg loader
- const imgRule = config.module.rules.find(
- rule => String(rule.test) === String(/\.(png|jpe?g|gif|svg|webp)$/i)
- );
- imgRule.test = /\.(png|jpe?g|gif|webp)$/i;
+Example usage in Nuxt nuxt.config.js:
+    import svgConfig from '~/webapps-common/webpack/webpack.svg.config';
+    export default {
+        build: {
+            extend(config) {
+                // remove Nuxt's default svg loader
+                const imgRule = config.module.rules.find(
+                    rule => String(rule.test) === String(/\.(png|jpe?g|gif|svg|webp|avif)$/i)
+                );
+                imgRule.test = /\.(png|jpe?g|gif|webp|avif)$/i;
 
- // add our svg loader
- config.module.rules.push(svgConfig);
- }
- }
- };
+                // add our svg loader
+                config.module.rules.push(svgConfig);
+            }
+        }
+    };
 
- Example usage in Vue postcss.config.js:
- const svgConfig = require('webapps-common/webpack/webpack.svg.config');
- module.exports = {
- chainWebpack: config => {
- config.module.rule('svg').uses.clear();
- config.merge({ module: { rule: { svg: svgConfig } } });
- }
- };
- */
+Example usage in Vue postcss.config.js:
+    const svgConfig = require('webapps-common/webpack/webpack.svg.config');
+    module.exports = {
+        chainWebpack: config => {
+            config.module.rule('svg').uses.clear();
+            config.merge({ module: { rule: { svg: svgConfig } } });
+        }
+    };
+*/
 
 const svgoOptions = {
     plugins: [
