619bf4c0b8e57a02e6463c24218bc0de8f61ff07
Daniel Bogenrieder <daniel.bogenrieder@knime.com>
2021-06-10 17:43:24 +0200

    WEBP-179: Abstracted icons import from FileLink into fileTypeIcons.js
    WEBP-179 (Frontend: rewrite file chooser input widget (input))

--- ui/components/FileLink.vue
+++ ui/components/FileLink.vue
@@ -1,62 +1,10 @@
 <script>
 import filesize from 'filesize';
-// file icons
-import fileIcon from '../assets/img/icons/file-question.svg?inline';
-// file type icons
-import csvIcon from '../assets/img/icons/file-csv.svg?inline';
-import docxIcon from '../assets/img/icons/file-docx.svg?inline';
-import htmlIcon from '../assets/img/icons/file-html.svg?inline';
-import mdIcon from '../assets/img/icons/file-md.svg?inline';
-import odpIcon from '../assets/img/icons/file-odp.svg?inline';
-import odsIcon from '../assets/img/icons/file-ods.svg?inline';
-import odtIcon from '../assets/img/icons/file-odt.svg?inline';
-import pdfIcon from '../assets/img/icons/file-pdf.svg?inline';
-import pptxIcon from '../assets/img/icons/file-pptx.svg?inline';
-import psIcon from '../assets/img/icons/file-ps.svg?inline';
-import txtIcon from '../assets/img/icons/file-text.svg?inline';
-import xlsIcon from '../assets/img/icons/file-xls.svg?inline';
-import xlsxIcon from '../assets/img/icons/file-xlsx.svg?inline';
-import xmlIcon from '../assets/img/icons/file-xml.svg?inline';
-import zipIcon from '../assets/img/icons/file-zip.svg?inline';
-import exeIcon from '../assets/img/icons/file-zip-exe.svg?inline';
-
+import * as icons from '../util/fileTypeIcons';
 
 export default {
     components: {
-        // eslint-disable-next-line vue/no-unused-components
-        csvIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        docxIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        htmlIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        mdIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        odpIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        odsIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        odtIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        pdfIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        pptxIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        psIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        txtIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        xlsIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        xlsxIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        xmlIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        zipIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        exeIcon,
-        // eslint-disable-next-line vue/no-unused-components
-        fileIcon
+        ...icons.default
     },
     props: {
         /** display text for the download link */
