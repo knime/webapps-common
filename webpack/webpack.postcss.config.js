3c239e23d3abb5105aaa1f7f63c3ac633e4467d2
Tobias Schmidt <tobias.schmidt@knime.com>
2021-08-05 15:37:02 +0200

    HUB-1707: Adjust postcss config
    HUB-1707 (Remove IE11 support)

--- webpack/webpack.postcss.config.js
+++ webpack/webpack.postcss.config.js
@@ -21,19 +21,13 @@ module.exports = {
     preset: { // config for postcss-preset-env which converts modern CSS into something most browsers can understand
         stage: 2,
         features: {
-            'nesting-rules': true,
-            // only because of IE11: the 'custom-properties' plugin doesn't support pseudo elements and
-            // only the :root scope, so we'll use 'postcss-css-variables'
-            'custom-properties': false
+            'nesting-rules': true
         }
     },
     order: 'presetEnvAndCssnanoLast',
     plugins: {
         'postcss-import': {},
         'postcss-url': {},
-        'postcss-css-variables': { // only because of IE11: add fallbacks for css custom properties
-            preserve: true // ...but keep native CSS custom props, so modern browser will use them
-        },
         'postcss-calc': {}
     }
 };
