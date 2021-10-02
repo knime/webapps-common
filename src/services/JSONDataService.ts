d7ec712cade6513f0a8050a4b4a7100b7c89723a
Viktor Trukhan <viktor.trukhan@redfield.se>
2021-10-02 19:20:30 +0300

    NXTEXT-65: rebase to master; moved sketch code to comments
    NXTEXT-65 (Adapt knime service with rpc data service)

--- src/services/JSONDataService.ts
+++ src/services/JSONDataService.ts
@@ -20,3 +20,35 @@ export class JSONDataService<T = any> {
         return Promise.resolve(this.initData);
     }
 }
+
+/*
+export class JSONDataService {
+    constructor(knimeService) {
+        this.knimeService = knimeService;
+        this.initData = null;
+
+        const { initData } = this.knimeService.extInfo;
+        if (initData) {
+            this.initData = typeof initData === 'string' ? JSON.parse(initData) : initData;
+        }
+    }
+
+    async getInitialData() {
+        // TODO error handling
+        return JSON.parse(await this.knimeService.getInitialData());
+    }
+
+    async getData(method, params) {
+        let jsonRpcRequest = {
+            jsonrpc: '2.0',
+            method,
+            params,
+            id: 1 // fixed for now
+        };
+
+        // TODO error handling
+        let response = JSON.parse(await this.knimeService.getData(jsonRpcRequest));
+        return Promise.resolve(response.result);
+    }
+  }
+ */
--- src/services/KnimeService.ts
+++ src/services/KnimeService.ts
@@ -7,3 +7,52 @@ export class KnimeService<T = any> {
         this.extInfo = extInfo;
     }
 }
+
+/*
+
+// move to static method
+const callDataService = async (serviceType, request) => {
+    if (this.jsonrpcSupported) {
+        let jsonRpcRequest = {
+            jsonrpc: '2.0',
+            method: 'NodeService.callNodeViewDataService',
+            params: [this.extInfo.projectId, this.extInfo.workflowId, this.extInfo.nodeId, serviceType, request],
+            id: this.requestId++;
+        };
+        return Promise.resolve(window.jsonrpc(JSON.stringify(jsonRpcRequest)));
+    }
+}
+
+export class KnimeService {
+    constructor(extInfo) {
+        this.extInfo = extInfo;
+        this.requestId = 0;
+
+        // TODO check for window.jsonrpc here
+        if (window.jsonrpc) {
+            this.jsonrpcSupported = true;
+        } else {
+            throw new Error(`current environment doesn't support window.jsonrpc()`);
+        }
+
+    }
+
+    getInitialData() { // private
+        // TODO if (this.knimeService.extInfo.hasInitData) {
+        if (this.extInfo.initData) {
+            return Promise.resolve(this.initData);
+        } else {
+            return callDataService('initial_data');
+        }
+     }
+
+    getData(request) { // private
+       return callDataService('data', request);
+    }
+
+    applyData(request) { // private
+        return callDataService('apply_data', request);
+    }
+
+}
+ */
