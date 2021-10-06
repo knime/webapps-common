31461c78d2162dc642768bdec9961fc555f8648d
Viktor Trukhan <viktor.trukhan@redfield.se>
2021-10-07 00:00:15 +0300

    NXTEXT-65: added KnimeService test
    NXTEXT-65 (Adapt knime service with rpc data service)

--- test/unit/suites/components/KnimeService.test.ts
+++ test/unit/suites/components/KnimeService.test.ts
@@ -1,5 +1,15 @@
 import { KnimeService } from 'src';
+import { JSONRpcMethods } from 'src/types/JSONRpcMethods';
+import { ServiceTypes } from 'src/types/serviceTypes';
 import { extInfo } from 'test/mocks/extInfo';
+
+window.jsonrpc = (requestJSON: string) => {
+    const request = JSON.parse(requestJSON);
+
+    if (request.method === JSONRpcMethods.CALL_NODE_VIEW_DATA_SERVICE) {
+        return JSON.stringify({ result: JSON.stringify({}) });
+    }
+};
 
 describe('KnimeService', () => {
     it('Creates KnimeService', () => {
@@ -8,5 +18,15 @@ describe('KnimeService', () => {
         expect(knime).toHaveProperty('extInfo');
 
         expect(knime.extInfo).toEqual(extInfo);
+    });
+
+    it('Calls data service', () => {
+        const knime = new KnimeService();
+
+        knime.callService(
+            JSONRpcMethods.CALL_NODE_VIEW_DATA_SERVICE,
+            ServiceTypes.INITIAL_DATA,
+            '',
+        );
     });
 });
