518fc8578b8de0d23dbaa951ef892017c7c9099a
Viktor Trukhan <viktor.trukhan@redfield.se>
2021-10-25 16:07:14 +0300

    added basic typescript eslint config

---
+++ lint/.eslintrc-typescript.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['./.eslintrc-base.js'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+    },
+};
