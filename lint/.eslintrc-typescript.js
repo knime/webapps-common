a428613324b9be6b13ce39eea80bd3271c2f7a12
Jakob Schröter <jakob.schroeter@knime.com>
2021-11-22 11:26:59 +0100

    eslint: fix no-extra-parens rule for typescript

--- lint/.eslintrc-typescript.js
+++ lint/.eslintrc-typescript.js
@@ -4,7 +4,9 @@ module.exports = {
     plugins: ['@typescript-eslint'],
     rules: {
         'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': 'error'
+        '@typescript-eslint/no-unused-vars': 'error',
+        'no-extra-parens': 'off',
+        '@typescript-eslint/no-extra-parens': 'error'
     },
     overrides: [{
         files: ['*.json'],
