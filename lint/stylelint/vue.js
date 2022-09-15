92a42e32109185affecd5d991096c328c21faaa2
Jakob Schröter <jakob.schroeter@knime.com>
2022-09-15 17:11:34 +0200

    add stylelint config

---
+++ lint/stylelint/vue.js
@@ -0,0 +1,29 @@
+module.exports = {
+    // this works, but we had some overriden rules that were not based on 'recommended-vue' so maybe worth reconsidering
+    extends: ['stylelint-config-standard-vue'],
+    rules: {
+        'color-no-hex': true,
+        'custom-property-empty-line-before': null,
+        'declaration-colon-newline-after': null,
+        'max-empty-lines': 2,
+        'no-empty-source': null,
+        'property-no-unknown': [true, {
+            // at the time of writing, content-visibility is only supported in Chrome,
+            // but doesn't cause any harm elsewhere
+            ignoreProperties: ['content-visibility']
+        }],
+        'selector-nested-pattern': '^&',
+        'selector-type-case': ['lower', {
+            ignoreTypes: [
+                'altGlyph', 'altGlyphDef', 'altGlyphItem', 'animateColor', 'animateMotion', 'animateTransform',
+                'clipPath', 'feBlend', 'feColorMatrix', 'feComponentTransfer', 'feComposite', 'feConvolveMatrix',
+                'feDiffuseLighting', 'feDisplacementMap', 'feDistantLight', 'feDropShadow', 'feFlood', 'feFuncA',
+                'feFuncB', 'feFuncG', 'feFuncR', 'feGaussianBlur', 'feImage', 'feMerge', 'feMergeNode', 'feMorphology',
+                'feOffset', 'fePointLight', 'feSpecularLighting', 'feSpotLight', 'feTile', 'feTurbulence',
+                'foreignObject', 'glyphRef', 'linearGradient', 'radialGradient', 'textPath'
+            ]
+        }],
+        'value-list-comma-newline-after': null
+    }
+};
+  
