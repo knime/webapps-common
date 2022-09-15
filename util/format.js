f2bb151c162e18528021c62dcd4a5dedb2bc859f
Tobias Schmidt <tobias.schmidt@knime.com>
2022-09-15 12:40:55 +0200

    HUB-3224: Add docs
    HUB-3224 (Create Hub ToS page on hub)

--- util/format.js
+++ util/format.js
@@ -1,6 +1,6 @@
 /**
  * Converts the specified date String to human readable text containing the day, month, and year.
- * Example: '2018-07-31T09:44:31+00:00' => 'Jul 31 2018'
+ * Example: '2018-07-31T09:44:31+00:00' => 'Jul 31, 2018'
  *
  * @param {String} dateString String representation of the Date to format
  * @returns {String} human readable date String
@@ -10,6 +10,7 @@ export const formatDateString = (dateString) => {
     if (isNaN(date)) {
         throw Error('Invalid Date format');
     }
-
+    // This only works for the default locale of en-US, as support of other locals in node js might not be available
+    // See for example this issue https://github.com/nodejs/node/issues/8500
     return date.toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' });
 };
