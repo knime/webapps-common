24cab82f7de22c220c61d3d4a4e2e309dbc34c0a
Ben Laney <ben.laney@knime.com>
2022-10-29 23:25:41 +0200

    HUB-2745: Add date/time formatting to format util
    HUB-2745 (UI: Show 'Unversioned Changes' content of a space)

--- test/unit/util/format.test.js
+++ test/unit/util/format.test.js
@@ -1,15 +1,21 @@
-import { formatDateString } from '~/util/format';
+import { formatDateString, formatDateTimeString, formatTimeString } from '~/util/format';
 
 describe('formatDateString', () => {
     let validFixtures = [{
         input: 0,
-        expected: 'Jan 1, 1970'
+        expectedDate: 'Jan 1, 1970',
+        expectedTime: '1:00 AM',
+        expectedDateTime: 'Jan 1, 1970 1:00 AM'
     }, {
         input: '2018-07-31T09:44:31+00:00',
-        expected: 'Jul 31, 2018'
+        expectedDate: 'Jul 31, 2018',
+        expectedTime: '11:44 AM',
+        expectedDateTime: 'Jul 31, 2018 11:44 AM'
     }, {
         input: 'December 17, 1995 03:24:00',
-        expected: 'Dec 17, 1995'
+        expectedDate: 'Dec 17, 1995',
+        expectedTime: '3:24 AM',
+        expectedDateTime: 'Dec 17, 1995 3:24 AM'
     }];
 
     let invalidFixtures = [{
@@ -19,14 +25,38 @@ describe('formatDateString', () => {
     }];
 
     it('formats date strings', () => {
-        validFixtures.forEach(({ input, expected }) => {
-            expect(formatDateString(input)).toEqual(expected);
+        validFixtures.forEach(({ input, expectedDate }) => {
+            expect(formatDateString(input)).toEqual(expectedDate);
         });
     });
 
-    it('throws error on invalid format', () => {
+    it('formats time strings', () => {
+        validFixtures.forEach(({ input, expectedTime }) => {
+            expect(formatTimeString(input)).toEqual(expectedTime);
+        });
+    });
+
+    it('formats date/time strings', () => {
+        validFixtures.forEach(({ input, expectedDateTime }) => {
+            expect(formatDateTimeString(input)).toEqual(expectedDateTime);
+        });
+    });
+
+    it('format date throws error on invalid format', () => {
         invalidFixtures.forEach(({ input }) => {
             expect(() => formatDateString(input)).toThrowError();
+        });
+    });
+
+    it('format time throws error on invalid format', () => {
+        invalidFixtures.forEach(({ input }) => {
+            expect(() => formatTimeString(input)).toThrowError();
+        });
+    });
+
+    it('format date/time throws error on invalid format', () => {
+        invalidFixtures.forEach(({ input }) => {
+            expect(() => formatDateTimeString(input)).toThrowError();
         });
     });
 });
