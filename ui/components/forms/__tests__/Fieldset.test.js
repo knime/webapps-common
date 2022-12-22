08610d2b43aecca0ba9f6d1e15ee2a15e53ad19d
Jakob Schröter <jakob.schroeter@knime.com>
2022-12-22 09:32:37 -0500

    add missing test for Fieldset.vue

---
+++ ui/components/forms/__tests__/Fieldset.test.js
@@ -0,0 +1,43 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+import { shallowMount } from '@vue/test-utils';
+
+import Label from '../Fieldset.vue';
+
+describe('Fieldset.vue', () => {
+    let props;
+
+    beforeEach(() => {
+        props = {
+            text: 'Testing fieldset'
+        };
+    });
+
+    it('renders', () => {
+        const wrapper = shallowMount(Label, {
+            props,
+            slots: {
+                default: 'slot content'
+            }
+        });
+        expect(wrapper.html()).toBeTruthy();
+        expect(wrapper.isVisible()).toBeTruthy();
+        expect(wrapper.get('legend').text()).toContain(props.text);
+        expect(wrapper.text()).toContain('slot content');
+    });
+
+    it('renders icon slot', () => {
+        const wrapper = shallowMount(Label, {
+            slots: {
+                icon: 'icon content'
+            }
+        });
+        expect(wrapper.get('legend').text()).toContain('icon content');
+    });
+
+    it('provides compactLabels to child components', () => {
+        const wrapper = shallowMount(Label);
+        expect(wrapper.vm.$options.provide).toEqual({
+            compactLabels: true
+        });
+    });
+});
