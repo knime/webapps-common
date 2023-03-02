1c229c9abfce4288caad9369a585ecaf7bd483a1
Jakob Schröter <jakob.schroeter@knime.com>
2023-03-02 16:43:53 +0100

    deploy demo to GitHub Pages

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -6,6 +6,7 @@ import svgLoader from 'vite-svg-loader';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), svgLoader()],
+    base: '/webapps-common/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
