f95cb6153df032dc08f2eaaa9086d0da6ad351fd
Jakob Schröter <jakob.schroeter@knime.com>
2023-03-03 09:30:16 +0100

    fix missing base for router

--- demo/src/main.js
+++ demo/src/main.js
@@ -10,7 +10,7 @@ window.consola = consola.create({
 
 const app = createApp(App);
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
