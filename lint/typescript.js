51f43bb675aafcb6c3fc093f4a4af27049cc4a6c
Helian Rivera <helian.rivera@knime.com>
2023-03-10 16:53:53 +0100

    update base typescript config

--- lint/typescript.js
+++ lint/typescript.js
@@ -1,18 +1,20 @@
 module.exports = {
-    extends: ['./base.js'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'import'],
-    rules: {
-        'no-duplicate-imports': 'off',
-        'import/no-duplicates': 'error',
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
-        'no-extra-parens': 'off',
-        '@typescript-eslint/no-extra-parens': 'error',
-        'prefer-const': 'error'
-    },
-    overrides: [{
-        files: ['*.json'],
-        parser: 'jsonc-eslint-parser'
-    }]
+    overrides: [
+        {
+            extends: ['./base.js'],
+            files: ['**/*.ts'],
+            parser: '@typescript-eslint/parser',
+            plugins: ['@typescript-eslint', 'import'],
+            rules: {
+                'no-duplicate-imports': 'off',
+                'import/no-duplicates': 'error',
+                'no-unused-vars': 'off',
+                '@typescript-eslint/no-unused-vars': 'off',
+                'no-extra-parens': 'off',
+                '@typescript-eslint/no-extra-parens': 'error',
+                'prefer-const': 'error'
+            },
+            env: { browser: true, node: true }
+        }
+    ]
 };
