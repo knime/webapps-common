82005fc22d304d0f1a073817aadac3e5eb1334bd
DanielBogenrieder <daniel.bogenrieder@knime.com>
2023-03-23 15:22:23 +0100

    UIEXT-788: reorganized tests
    UIEXT-788 (Implement ? for the wildcard search in twinlist)

--- util/__tests__/filters.test.js
+++ util/__tests__/filters.test.js
@@ -72,7 +72,36 @@ describe('filters', () => {
         expect(filter.test('--A--', normalized, isCaseSensitive, isInverted)).toBeFalsy();
     });
 
-    it('test wildcard "*" search matches correctly', () => {
+    it('matches the wildcard general cases', () => {
+        const filter = filters.wildcard;
+
+        // partial match --> do not match
+        let isCaseSensitive = false;
+        let isInverted = false;
+        let normalized = filter.normalize('A', isCaseSensitive, isInverted);
+        expect(filter.test('--A--', normalized, isCaseSensitive, isInverted)).toBeFalsy();
+
+        // empty search term
+        isCaseSensitive = false;
+        isInverted = false;
+        normalized = filter.normalize('', isCaseSensitive, isInverted);
+        expect(filter.test('--A--', normalized, isCaseSensitive, isInverted)).toBeFalsy();
+
+        // case sensitive
+        isCaseSensitive = true;
+        isInverted = false;
+        normalized = filter.normalize('--A--', isCaseSensitive, isInverted);
+        expect(filter.test('--a--', normalized, isCaseSensitive, isInverted)).toBeFalsy();
+
+        // inverted
+        isCaseSensitive = false;
+        isInverted = true;
+        normalized = filter.normalize('--a--', isCaseSensitive, isInverted);
+        expect(filter.test('--A--', normalized, isCaseSensitive, isInverted)).toBeFalsy();
+        expect(filter.test('--B--', normalized, isCaseSensitive, isInverted)).toBeTruthy();
+    });
+
+    it('matches wildcard "*" search correctly', () => {
         const filter = filters.wildcard;
         let searchTerm = '*A*';
 
@@ -81,34 +110,9 @@ describe('filters', () => {
         let normalized = filter.normalize(searchTerm, isCaseSensitive, isInverted);
         expect(filter.test('--a--', normalized, isCaseSensitive, isInverted)).toBeTruthy();
         expect(filter.test('--b--', normalized, isCaseSensitive, isInverted)).toBeFalsy();
-
-        // case sensitive
-        isCaseSensitive = true;
-        isInverted = false;
-        normalized = filter.normalize(searchTerm, isCaseSensitive, isInverted);
-        expect(filter.test('--a--', normalized, isCaseSensitive, isInverted)).toBeFalsy();
-
-        // inverted
-        isCaseSensitive = false;
-        isInverted = true;
-        normalized = filter.normalize(searchTerm, isCaseSensitive, isInverted);
-        expect(filter.test('--A--', normalized, isCaseSensitive, isInverted)).toBeFalsy();
-        expect(filter.test('--B--', normalized, isCaseSensitive, isInverted)).toBeTruthy();
-
-        // partial match --> do not match
-        isCaseSensitive = false;
-        isInverted = false;
-        normalized = filter.normalize('A', isCaseSensitive, isInverted);
-        expect(filter.test('--A--', normalized, isCaseSensitive, isInverted)).toBeFalsy();
-
-        // empty search term
-        isCaseSensitive = false;
-        isInverted = false;
-        normalized = filter.normalize('', isCaseSensitive, isInverted);
-        expect(filter.test('--A--', normalized, isCaseSensitive, isInverted)).toBeFalsy();
     });
 
-    it('tests wildcard "?" search matches correctly', () => {
+    it('matches wildcard "?" search correctly', () => {
         const filter = filters.wildcard;
         let searchTerm = '??A??';
 
