292567dd04e4ce5d436d2bdfaaf352394d262b71
Christian Albrecht <christian.albrecht@knime.com>
2023-05-04 16:09:51 +0200

    WEBP-937: added linting rules for nuxt3 projects
    WEBP-937 (Define linting rules in webapps-common)

---
+++ lint/nuxt3.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: ['@nuxt/eslint-config', './vue3', 'prettier'],
+    globals: {
+        createError: true,
+        defineNuxtPlugin: true,
+        defineNuxtRouteMiddleware: true,
+        markRaw: true,
+        navigateTo: true,
+        useAsyncData: true,
+        useNuxtApp: true,
+        useRoute: true
+    }
+};
