968b47e50d6b756189e189e50bc17f1bd64f76db
Paul Bärnreuther <paul.baernreuther@tngtech.com>
2023-08-02 15:23:17 +0200

    UIEXT-1145: Format all files
    UIEXT-1145 (Warnings in knime-core-ui unit tests )

--- org.knime.core.ui/js-src/src/nodeDialog/uiComponents/__tests__/RichTextInput.test.js
+++ org.knime.core.ui/js-src/src/nodeDialog/uiComponents/__tests__/RichTextInput.test.js
@@ -1,121 +1,142 @@
-import { afterEach, beforeEach, beforeAll, describe, expect, it, vi } from 'vitest';
-import { mountJsonFormsComponent, initializesJsonFormsControl }
-    from '@@/test-setup/utils/jsonFormsTestUtils';
-import RichTextEditor from 'webapps-common/ui/components/forms/RichTextEditor/RichTextEditor.vue';
-import RichTextInput from '../RichTextInput.vue';
-import { inputFormats } from '@/nodeDialog/constants';
-  
-describe('RichTextInput.vue', () => {
-    let props, wrapper, onChangeSpy, component, stubs;
+import {
+  afterEach,
+  beforeEach,
+  beforeAll,
+  describe,
+  expect,
+  it,
+  vi,
+} from "vitest";
+import {
+  mountJsonFormsComponent,
+  initializesJsonFormsControl,
+} from "@@/test-setup/utils/jsonFormsTestUtils";
+import RichTextEditor from "webapps-common/ui/components/forms/RichTextEditor/RichTextEditor.vue";
+import RichTextInput from "../RichTextInput.vue";
+import { inputFormats } from "@/nodeDialog/constants";
 
-    beforeAll(() => {
-        onChangeSpy = vi.spyOn(RichTextInput.methods, 'onChange');
-    });
+describe("RichTextInput.vue", () => {
+  let props, wrapper, onChangeSpy, component, stubs;
 
-    beforeEach(() => {
-        props = {
-            control: {
-                path: 'richTextContent',
-                visible: true,
-                enabled: true,
-                data: 'test',
-                schema: {
-                    properties: {
-                        richTextContent: {
-                            type: 'string'
-                        }
-                    }
-                },
-                uischema: {
-                    type: 'Control',
-                    scope: '#/properties/view/properties/richTextContent',
-                    options: {
-                        format: inputFormats.richTextInput
-                    }
-                },
-                rootSchema: {
-                    hasNodeView: true,
-                    flowVariablesMap: {
-                        flowVar1: 'flow value'
-                    }
-                }
-            }
-            
-        };
-        stubs = {
-            EditorContent: true
-        };
-        component = mountJsonFormsComponent(RichTextInput, { props, stubs });
-        wrapper = component.wrapper;
-    });
+  beforeAll(() => {
+    onChangeSpy = vi.spyOn(RichTextInput.methods, "onChange");
+  });
 
-    afterEach(() => {
-        vi.clearAllMocks();
-    });
-
-    it('renders', () => {
-        expect(wrapper.findComponent(RichTextInput).exists()).toBeTruthy();
-        expect(wrapper.findComponent(RichTextEditor).exists()).toBeTruthy();
-    });
-
-    it('initializes jsonforms', () => {
-        initializesJsonFormsControl(component);
-    });
-
-    it('calls onChange when html content is changed', async () => {
-        const dirtySettingsMock = vi.fn();
-        const { wrapper, updateData } = await mountJsonFormsComponent(RichTextInput, {
-            props,
-            modules: {
-                'pagebuilder/dialog': {
-                    actions: { dirtySettings: dirtySettingsMock },
-                    namespaced: true
-                }
+  beforeEach(() => {
+    props = {
+      control: {
+        path: "richTextContent",
+        visible: true,
+        enabled: true,
+        data: "test",
+        schema: {
+          properties: {
+            richTextContent: {
+              type: "string",
             },
-            stubs: {
-                EditorContent: true
-            }
-        });
-        const changedRichTextInput = 'abcdefg';
-        wrapper.findComponent(RichTextEditor).vm.$emit('update:modelValue', changedRichTextInput);
-        expect(onChangeSpy).toHaveBeenCalledWith(changedRichTextInput);
-        expect(updateData).toHaveBeenCalledWith(
-            expect.anything(), props.control.path, changedRichTextInput
-        );
-        expect(dirtySettingsMock).not.toHaveBeenCalled();
-    });
+          },
+        },
+        uischema: {
+          type: "Control",
+          scope: "#/properties/view/properties/richTextContent",
+          options: {
+            format: inputFormats.richTextInput,
+          },
+        },
+        rootSchema: {
+          hasNodeView: true,
+          flowVariablesMap: {
+            flowVar1: "flow value",
+          },
+        },
+      },
+    };
+    stubs = {
+      EditorContent: true,
+    };
+    component = mountJsonFormsComponent(RichTextInput, { props, stubs });
+    wrapper = component.wrapper;
+  });
 
-    it('sets correct initial value', () => {
-        expect(wrapper.findComponent(RichTextEditor).vm.modelValue).toBe(props.control.data);
-    });
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
 
-    it('sets editor to editable', () => {
-        const editorComponent = wrapper.findComponent(RichTextEditor);
-        expect(editorComponent.vm.editable).toBeTruthy();
-    });
+  it("renders", () => {
+    expect(wrapper.findComponent(RichTextInput).exists()).toBeTruthy();
+    expect(wrapper.findComponent(RichTextEditor).exists()).toBeTruthy();
+  });
 
-    it('disables editor if content is overwritten by flow variable', async () => {
-        props = {
-            ...props,
-            control: {
-                ...props.control,
-                rootSchema: {
-                    ...props.control.rootSchema,
-                    flowVariablesMap: {
-                        richTextContent: {
-                            leaf: true,
-                            controllingFlowVariableAvailable: true,
-                            controllingFlowVariableName: 'string-input',
-                            exposedFlowVariableName: null
-                        }
-                    }
-                }
-            }
-        };
-        let localComponent = await mountJsonFormsComponent(RichTextInput, { props, stubs });
-        let localWrapper = localComponent.wrapper;
-        expect(localWrapper.vm.disabled).toBeTruthy();
-        const editorComponent = localWrapper.findComponent(RichTextEditor);
-        expect(editorComponent.vm.editable).toBeFalsy();
+  it("initializes jsonforms", () => {
+    initializesJsonFormsControl(component);
+  });
+
+  it("calls onChange when html content is changed", async () => {
+    const dirtySettingsMock = vi.fn();
+    const { wrapper, updateData } = await mountJsonFormsComponent(
+      RichTextInput,
+      {
+        props,
+        modules: {
+          "pagebuilder/dialog": {
+            actions: { dirtySettings: dirtySettingsMock },
+            namespaced: true,
+          },
+        },
+        stubs: {
+          EditorContent: true,
+        },
+      },
+    );
+    const changedRichTextInput = "abcdefg";
+    wrapper
+      .findComponent(RichTextEditor)
+      .vm.$emit("update:modelValue", changedRichTextInput);
+    expect(onChangeSpy).toHaveBeenCalledWith(changedRichTextInput);
+    expect(updateData).toHaveBeenCalledWith(
+      expect.anything(),
+      props.control.path,
+      changedRichTextInput,
+    );
+    expect(dirtySettingsMock).not.toHaveBeenCalled();
+  });
+
+  it("sets correct initial value", () => {
+    expect(wrapper.findComponent(RichTextEditor).vm.modelValue).toBe(
+      props.control.data,
+    );
+  });
+
+  it("sets editor to editable", () => {
+    const editorComponent = wrapper.findComponent(RichTextEditor);
+    expect(editorComponent.vm.editable).toBeTruthy();
+  });
+
+  it("disables editor if content is overwritten by flow variable", async () => {
+    props = {
+      ...props,
+      control: {
+        ...props.control,
+        rootSchema: {
+          ...props.control.rootSchema,
+          flowVariablesMap: {
+            richTextContent: {
+              leaf: true,
+              controllingFlowVariableAvailable: true,
+              controllingFlowVariableName: "string-input",
+              exposedFlowVariableName: null,
+            },
+          },
+        },
+      },
+    };
+    let localComponent = await mountJsonFormsComponent(RichTextInput, {
+      props,
+      stubs,
     });
+    let localWrapper = localComponent.wrapper;
+    expect(localWrapper.vm.disabled).toBeTruthy();
+    const editorComponent = localWrapper.findComponent(RichTextEditor);
+    expect(editorComponent.vm.editable).toBeFalsy();
+  });
 });
--- org.knime.core.ui/js-src/test-setup/utils/jsonFormsTestUtils.js
+++ org.knime.core.ui/js-src/test-setup/utils/jsonFormsTestUtils.js
@@ -2,103 +2,114 @@
  * within a JSONForms context.
  * A component can be mounted using composition API and the correct initialization of JSONForms can be verified on a
  * given vue test utils wrapper. */
-import { expect, vi } from 'vitest';
+import { expect, vi } from "vitest";
 
-import { mount } from '@vue/test-utils';
-import { createStore } from 'vuex';
+import { mount } from "@vue/test-utils";
+import { createStore } from "vuex";
 
-import { useJsonFormsLayout, useJsonFormsArrayControl } from '@jsonforms/vue';
+import { useJsonFormsLayout, useJsonFormsArrayControl } from "@jsonforms/vue";
 
-import * as jsonFormsControlWithUpdateModule from '@/nodeDialog/uiComponents/composables/jsonFormsControlWithUpdate';
+import * as jsonFormsControlWithUpdateModule from "@/nodeDialog/uiComponents/composables/jsonFormsControlWithUpdate";
 
 export const mountJsonFormsComponent = (
-    component, { props, provide, modules = null, showAdvanced = false, stubs = {} }
+  component,
+  { props, provide, modules = null, showAdvanced = false, stubs = {} },
 ) => {
-    const useJsonFormsControlSpy = vi.spyOn(jsonFormsControlWithUpdateModule, 'useJsonFormsControlWithUpdate');
-    const callbacks = [];
-    const { getDataMock, updateDataMock, sendAlertMock } = provide || {};
-    const updateData = updateDataMock || vi.fn((handleChange, path, value) => handleChange(path, value));
-    const getData = getDataMock || vi.fn();
-    const sendAlert = sendAlertMock || vi.fn();
-    const wrapper = mount(
-        component,
-        {
-            props,
-            global: {
-                provide: {
-                    getKnimeService: () => ({
-                        extensionConfig: {
-                            nodeId: 'nodeId'
-                        },
-                        callService: vi.fn().mockResolvedValue({}),
-                        registerDataGetter: vi.fn(),
-                        addEventCallback: vi.fn(),
-                        createAlert: vi.fn(),
-                        sendWarning: vi.fn()
-                    }),
-                    registerWatcher: (
-                        { transformSettings, init, dependencies }
-                    ) => {
-                        callbacks.push({ transformSettings, init, dependencies });
-                        if (typeof init === 'function') {
-                            init({});
-                        }
-                    },
-                    updateData,
-                    getData,
-                    sendAlert
-                },
-                stubs: {
-                    DispatchRenderer: true,
-                    ...stubs
-                },
-                mocks: {
-                    $store: createStore({ modules })
-                }
-            },
-            provide: {
-                jsonforms: {
-                    core: {
-                        schema: {
-                            showAdvancedSettings: showAdvanced
-                        }
-                    }
-                }
-            }
-        }
-    );
-    return { wrapper, callbacks, updateData, useJsonFormsControlSpy, sendAlert, getData };
+  const useJsonFormsControlSpy = vi.spyOn(
+    jsonFormsControlWithUpdateModule,
+    "useJsonFormsControlWithUpdate",
+  );
+  const callbacks = [];
+  const { getDataMock, updateDataMock, sendAlertMock } = provide || {};
+  const updateData =
+    updateDataMock ||
+    vi.fn((handleChange, path, value) => handleChange(path, value));
+  const getData = getDataMock || vi.fn();
+  const sendAlert = sendAlertMock || vi.fn();
+  const wrapper = mount(component, {
+    props,
+    global: {
+      provide: {
+        getKnimeService: () => ({
+          extensionConfig: {
+            nodeId: "nodeId",
+          },
+          callService: vi.fn().mockResolvedValue({}),
+          registerDataGetter: vi.fn(),
+          addEventCallback: vi.fn(),
+          createAlert: vi.fn(),
+          sendWarning: vi.fn(),
+        }),
+        registerWatcher: ({ transformSettings, init, dependencies }) => {
+          callbacks.push({ transformSettings, init, dependencies });
+          if (typeof init === "function") {
+            init({});
+          }
+        },
+        updateData,
+        getData,
+        sendAlert,
+      },
+      stubs: {
+        DispatchRenderer: true,
+        ...stubs,
+      },
+      mocks: {
+        $store: createStore({ modules }),
+      },
+    },
+    provide: {
+      jsonforms: {
+        core: {
+          schema: {
+            showAdvancedSettings: showAdvanced,
+          },
+        },
+      },
+    },
+  });
+  return {
+    wrapper,
+    callbacks,
+    updateData,
+    useJsonFormsControlSpy,
+    sendAlert,
+    getData,
+  };
 };
 
 const hasBasicProps = (props) => {
-    expect(props.hasOwnProperty('schema')).toBe(true);
-    expect(props.hasOwnProperty('uischema')).toBe(true);
-    expect(props.hasOwnProperty('path')).toBe(true);
+  expect(props.hasOwnProperty("schema")).toBe(true);
+  expect(props.hasOwnProperty("uischema")).toBe(true);
+  expect(props.hasOwnProperty("path")).toBe(true);
 };
 
-export const initializesJsonFormsControl = ({ wrapper, useJsonFormsControlSpy }) => {
-    const props = wrapper.props();
-    hasBasicProps(props);
-    expect(props.hasOwnProperty('control')).toBe(true);
-    expect(props.control.schema).toBeDefined();
-    expect(props.control.uischema).toBeDefined();
-    expect(useJsonFormsControlSpy).toHaveBeenCalled();
+export const initializesJsonFormsControl = ({
+  wrapper,
+  useJsonFormsControlSpy,
+}) => {
+  const props = wrapper.props();
+  hasBasicProps(props);
+  expect(props.hasOwnProperty("control")).toBe(true);
+  expect(props.control.schema).toBeDefined();
+  expect(props.control.uischema).toBeDefined();
+  expect(useJsonFormsControlSpy).toHaveBeenCalled();
 };
 
 export const initializesJsonFormsLayout = (wrapper) => {
-    const props = wrapper.props();
-    hasBasicProps(props);
-    expect(props.hasOwnProperty('layout')).toBe(true);
-    expect(props.layout.schema).toBeDefined();
-    expect(props.layout.uischema).toBeDefined();
-    expect(useJsonFormsLayout).toHaveBeenCalled();
+  const props = wrapper.props();
+  hasBasicProps(props);
+  expect(props.hasOwnProperty("layout")).toBe(true);
+  expect(props.layout.schema).toBeDefined();
+  expect(props.layout.uischema).toBeDefined();
+  expect(useJsonFormsLayout).toHaveBeenCalled();
 };
 
 export const initializesJsonFormsArrayControl = (wrapper) => {
-    const props = wrapper.props();
-    hasBasicProps(props);
-    expect(props.hasOwnProperty('control')).toBe(true);
-    expect(props.control.schema).toBeDefined();
-    expect(props.control.uischema).toBeDefined();
-    expect(useJsonFormsArrayControl).toHaveBeenCalled();
+  const props = wrapper.props();
+  hasBasicProps(props);
+  expect(props.hasOwnProperty("control")).toBe(true);
+  expect(props.control.schema).toBeDefined();
+  expect(props.control.uischema).toBeDefined();
+  expect(useJsonFormsArrayControl).toHaveBeenCalled();
 };
