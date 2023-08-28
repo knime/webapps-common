1751548315db39f7ab07d8681cd57b10a4f76439
Martin Horn <martin.horn@knime.com>
2023-08-28 15:49:22 +0200

    UIEXT-1261: 'getFlowVariableSettings' removed from DialogService
    Because the flow variable info is now for each dialog-implementation
    supplied individually via the initial data.
    
    Will be consistent with applying data - where the flow variable settings
    are supplied individually, too (and not on 'framework' level).
    
    UIEXT-1261 (Communicate selected flow variables on \)

--- src/services/DialogService.ts
+++ src/services/DialogService.ts
@@ -1,6 +1,5 @@
 import { IFrameKnimeService } from "src";
 import { KnimeService } from "./KnimeService";
-import { createFlowVariablesMap } from "src/utils/flowVariablesUtils";
 
 /**
  * A utility class to interact with Dialog settings implemented by a UI Extension node.
@@ -14,16 +13,6 @@ export class DialogService<T = any> {
    */
   constructor(knimeService: IFrameKnimeService | KnimeService<T>) {
     this.knimeService = knimeService;
-  }
-
-  /**
-   * @returns {FlowVariableSettings | null} - maps of model and view flow variables settings.
-   */
-  async getFlowVariableSettings() {
-    const flowSettings = await Promise.resolve(
-      this.knimeService.extensionConfig?.flowVariableSettings || {},
-    );
-    return createFlowVariablesMap(flowSettings);
   }
 
   /**
--- test/unit/suites/services/DialogService.test.ts
+++ test/unit/suites/services/DialogService.test.ts
@@ -1,7 +1,6 @@
 import { DialogService } from "src/services/DialogService";
 import { extensionConfig } from "test/mocks";
 import { KnimeService } from "src/services/KnimeService";
-import { createFlowVariablesMap } from "src/utils/flowVariablesUtils";
 
 describe("DialogService", () => {
   describe("initialization", () => {
@@ -9,25 +8,7 @@ describe("DialogService", () => {
       const knimeService = new KnimeService(extensionConfig);
       const dialogService = new DialogService(knimeService);
 
-      expect(dialogService).toHaveProperty("getFlowVariableSettings");
-    });
-  });
-
-  describe("service.getFlowVariableSettings", () => {
-    it("Fetches flowVariablesSettings if it's passed to constructor", async () => {
-      const knimeService = new KnimeService(extensionConfig);
-      const dialogService = new DialogService(knimeService);
-
-      expect(await dialogService.getFlowVariableSettings()).toEqual(
-        createFlowVariablesMap(extensionConfig.flowVariableSettings),
-      );
-    });
-
-    it("Returns empty settings object if extensionConfig is not passed to constructor", async () => {
-      const knimeService = new KnimeService();
-      const dialogService = new DialogService(knimeService);
-
-      expect(await dialogService.getFlowVariableSettings()).toEqual({});
+      expect(dialogService).toHaveProperty("hasNodeView");
     });
   });
 });
