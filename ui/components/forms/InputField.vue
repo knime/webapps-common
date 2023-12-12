0a59b39a4a8a9816fcd06bcf04cbb9e4e2bf5709
Malte Ulfers <malte.ulfers@tngtech.com>
2023-12-12 12:22:00 +0100

    HUB-6587: support emitting full event
    HUB-6587 (Investigate file upload for secrets)

--- ui/components/forms/InputField.vue
+++ ui/components/forms/InputField.vue
@@ -1,115 +1,114 @@
 <script>
 export default {
-    props: {
-        value: {
-            default: '',
-            type: [Number, String]
-        },
-        id: {
-            type: String,
-            default: null
-        },
-        name: {
-            type: String,
-            default: null
-        },
-        /**
-         * Sets the error styling, validity needs to be controlled by the parent component to be flexible
-         */
-        isValid: {
-            default: true,
-            type: Boolean
-        },
-        type: {
-            default: 'text',
-            type: String
-        },
-        pattern: {
-            default: null,
-            type: String
-        },
-        placeholder: {
-            default: null,
-            type: String
-        },
-        autocomplete: {
-            default: null,
-            type: String
-        },
-        autofocus: {
-            default: false,
-            type: Boolean
-        },
-        inputClasses: {
-            default: '',
-            type: String
-        },
-        disabled: {
-            default: false,
-            type: Boolean
-        }
+  props: {
+    value: {
+      default: "",
+      type: [Number, String],
     },
-    computed: {
-        hasLeftIcon() {
-            return this.$slots.icon && this.$slots.icon.length;
-        },
-        hasRightIcon() {
-            return this.$slots.iconRight && this.$slots.iconRight.length;
-        },
-        inputClassList() {
-            let classes = this.inputClasses;
-            if (this.hasLeftIcon) {
-                classes += ' with-icon';
-            }
-            if (this.hasRightIcon) {
-                classes += ' with-icon-right';
-            }
-            if (!this.isValid) {
-                classes += ' invalid';
-            }
-            return classes;
-        }
+    id: {
+      type: String,
+      default: null,
     },
-    methods: {
-        getValue() {
-            return this.$refs.input.value;
-        },
-        onInput() {
-            this.$emit('input', this.getValue());
-        },
-        focus() {
-            this.$refs.input.focus();
-        },
-        /*
-         * checks if value matches the provided pattern
-         */
-        validate() {
-            let isValid = true;
-            let errorMessage = null;
-            const value = this.getValue();
-            if (typeof value === 'undefined') {
-                isValid = false;
-                errorMessage = 'Invalid input';
-            } else if (this.pattern) {
-                if (this.$refs.input.validity.patternMismatch) {
-                    isValid = false;
-                    errorMessage = 'Input does not match the expected pattern';
-                }
-            }
-            return { isValid, errorMessage };
+    name: {
+      type: String,
+      default: null,
+    },
+    /**
+     * Sets the error styling, validity needs to be controlled by the parent component to be flexible
+     */
+    isValid: {
+      default: true,
+      type: Boolean,
+    },
+    type: {
+      default: "text",
+      type: String,
+    },
+    pattern: {
+      default: null,
+      type: String,
+    },
+    placeholder: {
+      default: null,
+      type: String,
+    },
+    autocomplete: {
+      default: null,
+      type: String,
+    },
+    autofocus: {
+      default: false,
+      type: Boolean,
+    },
+    inputClasses: {
+      default: "",
+      type: String,
+    },
+    disabled: {
+      default: false,
+      type: Boolean,
+    },
+    emitFullEvent: {
+      default: false,
+      type: Boolean,
+    },
+  },
+  computed: {
+    hasLeftIcon() {
+      return this.$slots.icon && this.$slots.icon.length;
+    },
+    hasRightIcon() {
+      return this.$slots.iconRight && this.$slots.iconRight.length;
+    },
+    inputClassList() {
+      let classes = this.inputClasses;
+      if (this.hasLeftIcon) {
+        classes += " with-icon";
+      }
+      if (this.hasRightIcon) {
+        classes += " with-icon-right";
+      }
+      if (!this.isValid) {
+        classes += " invalid";
+      }
+      return classes;
+    },
+  },
+  methods: {
+    getValue() {
+      return this.$refs.input.value;
+    },
+    onInput(event) {
+      this.$emit("input", this.emitFullEvent ? event : this.getValue());
+    },
+    focus() {
+      this.$refs.input.focus();
+    },
+    /*
+     * checks if value matches the provided pattern
+     */
+    validate() {
+      let isValid = true;
+      let errorMessage = null;
+      const value = this.getValue();
+      if (typeof value === "undefined") {
+        isValid = false;
+        errorMessage = "Invalid input";
+      } else if (this.pattern) {
+        if (this.$refs.input.validity.patternMismatch) {
+          isValid = false;
+          errorMessage = "Input does not match the expected pattern";
         }
-    }
+      }
+      return { isValid, errorMessage };
+    },
+  },
 };
 </script>
 
 <template>
-  <div
-    class="input-wrapper"
-  >
-    <div
-      v-if="hasLeftIcon"
-      class="icon"
-    >
+  <div class="input-wrapper">
+    <div v-if="hasLeftIcon" class="icon">
       <slot name="icon" />
     </div>
     <input
@@ -129,16 +128,11 @@ export default {
       @keyup="$emit('keyup', $event)"
       @keypress="$emit('keypress', $event)"
       @keydown="$emit('keydown', $event)"
-    >
-    <div
-      v-if="hasRightIcon"
-      class="icon icon-right"
-    >
+    />
+    <div v-if="hasRightIcon" class="icon icon-right">
       <slot name="iconRight" />
     </div>
-    <span
-      class="invalid-marker"
-    />
+    <span class="invalid-marker" />
   </div>
 </template>
 
@@ -170,7 +164,9 @@ export default {
       vertical-align: top;
       width: calc(var(--icon-size) * 1px);
       height: calc(var(--icon-size) * 1px);
-      stroke-width: calc(32px / var(--icon-size)); /* TODO: See ticket UIEXT-590, the stroke-width mixin should be used here. */
+      stroke-width: calc(
+        32px / var(--icon-size)
+      ); /* TODO: See ticket UIEXT-590, the stroke-width mixin should be used here. */
       stroke: var(--knime-masala);
     }
   }
