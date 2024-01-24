6cb09e5f98ce69ce4511cbc91eb27c6a21737f49
Yassine Alikhbari <yassine.alikhbari@knime.com>
2024-01-24 16:33:02 +0100

    HUB-6745: Adjust demo component
    HUB-6745 (Extract new file selector for WAC)

--- demo/src/components/FileSelector.vue
+++ demo/src/components/FileSelector.vue
@@ -2,22 +2,25 @@
 import CodeExample from "./demo/CodeExample.vue";
 import FileSelector from "webapps-common/ui/components/FileSelector.vue";
 import fileSelectorCode from "webapps-common/ui/components/FileSelector.vue?raw";
+import Dropdown from "webapps-common/ui/components/forms/Dropdown.vue";
 
 const codeExample = `<FileSelector
-                        accepted-file-types="*"
-                        label="Select a file"
-                        :multiple="true"
-                    />`;
+  accepted-file-types="*"
+  label="Select a file"
+  :multiple="true"
+/>`;
 
 export default {
   components: {
     FileSelector,
     CodeExample,
+    Dropdown,
   },
   data() {
     return {
       codeExample,
       fileSelectorCode,
+      acceptedFileTypes: "*",
     };
   },
 };
@@ -29,20 +32,57 @@ export default {
       <div class="grid-item-12">
         <p>
           The file selector allows the user to choose a file or multiple files
-          from the file system.
+          from the file system. Accepted file types can be set via the
+          <code>"accepted-file-types"</code> property.
         </p>
-        <p>Select multiple files</p>
-        <FileSelector
-          accepted-file-types="*"
-          label="Select a file"
-          :multiple="true"
-        />
-        <p>Select a single file</p>
-        <FileSelector
-          accepted-file-types="*"
-          label="Select a file"
-          :multiple="false"
-        />
+        <div class="grid-container selector-container">
+          <div class="grid-item-3">
+            <p>Select a single file</p>
+            <FileSelector
+              :accepted-file-types="acceptedFileTypes"
+              label="Select a file"
+              :multiple="false"
+            />
+          </div>
+          <div class="grid-item-3">
+            <p>Example of accepted file types</p>
+            <Dropdown
+              v-model="acceptedFileTypes"
+              aria-label="Accepted file types"
+              :possible-values="[
+                {
+                  id: '*',
+                  text: '*',
+                },
+                {
+                  id: 'text/plain',
+                  text: 'text/plain',
+                },
+                {
+                  id: 'image/png',
+                  text: 'image/png',
+                },
+                {
+                  id: 'video/*',
+                  text: 'video/*',
+                },
+                {
+                  id: '.pdf',
+                  text: '.pdf',
+                },
+              ]"
+            />
+          </div>
+        </div>
+        <div class="selector-container">
+          <p>Select multiple files</p>
+          <FileSelector
+            accepted-file-types="*"
+            label="Select files"
+            :multiple="true"
+          />
+        </div>
+
         <CodeExample summary="Show usage example">{{
           codeExample
         }}</CodeExample>
@@ -53,3 +93,13 @@ export default {
     </div>
   </section>
 </template>
+
+<style lang="postcss" scoped>
+.grid-container {
+  justify-content: start;
+}
+
+.selector-container {
+  margin-bottom: 30px;
+}
+</style>
