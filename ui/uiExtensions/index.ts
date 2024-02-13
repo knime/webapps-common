23eb5b81a3487c4f1c601000d43f3bf4a869d62f
Karl Glatz <karl@glatz.biz>
2024-02-13 09:34:42 +0100

    UIEXT-1530: add index.ts to uiExtensions with public exports
    UIEXT-1530 (Split UI Extension component from page builder)

---
+++ ui/uiExtensions/index.ts
@@ -0,0 +1,11 @@
+import UIExtension from "./UIExtension.vue";
+import type { ExtensionConfig } from "./types/ExtensionConfig";
+import type { UIExtensionAPILayer } from "./types/UIExtensionAPILayer";
+import { ResourceTypes } from "./types/ResourceTypes";
+
+export {
+  UIExtension,
+  type UIExtensionAPILayer,
+  type ExtensionConfig,
+  ResourceTypes,
+};
