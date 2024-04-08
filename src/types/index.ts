12c236f4c8eb57e677e02ddae18dd05bd0ae143b
Christian Albrecht <christian.albrecht@knime.com>
2024-04-08 11:14:00 +0200

    Include DialogSettings type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,6 @@ export * from "./alert";
 export * from "./pushEvents";
 export * from "./NodeInfo";
 export * from "./DataServiceType";
+export * from "./DialogSettings";
 export * from "./ExtensionTypes";
 export * from "./uiExtensionService";
