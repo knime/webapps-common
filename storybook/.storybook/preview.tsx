d0eb13029e7b1bdf1f91a9a98d545132600bf043
Yassine Alikhbari <yassine.alikhbari@knime.com>
2024-06-05 17:21:27 +0200

    Add consola and support modelValue in storybook

--- storybook/.storybook/preview.tsx
+++ storybook/.storybook/preview.tsx
@@ -1,6 +1,12 @@
-import type { Preview } from "@storybook/vue3";
-
 import "@knime/styles/css/index.css";
+
+import type { Preview } from "@storybook/vue3";
+import { useArgs } from "@storybook/preview-api";
+import consola from "consola";
+
+window.consola = consola.create({
+  level: 4,
+});
 
 const preview: Preview = {
   parameters: {
@@ -11,6 +17,19 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    //support for v-model
+    (story, context) => {
+      const [args, updateArgs] = useArgs();
+      if ("modelValue" in args) {
+        args["onUpdate:modelValue"] = (...values) => {
+          const modelValue = values[0] === undefined ? null : values[0];
+          updateArgs({ modelValue });
+        };
+      }
+      return story({ ...context, updateArgs });
+    },
+  ],
 };
 
 export default preview;
