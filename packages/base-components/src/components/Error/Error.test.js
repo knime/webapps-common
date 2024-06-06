5b5ddd15dd10b0c4925734e2d86c095eb168434d
Ahmed Ramzi <ramzi.muthana@knime.com>
2024-06-06 12:17:41 +0200

    Fixed some tests

--- packages/base-components/src/components/Error/Error.test.js
+++ packages/base-components/src/components/Error/Error.test.js
@@ -1,7 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { shallowMount } from "@vue/test-utils";
 
-import Error from "../Error.vue";
+import Error from "./Error.vue";
 
 describe("Error.vue", () => {
   it("renders default", () => {
