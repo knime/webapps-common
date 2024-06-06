a4dc299df7ec20a42e5d5e22c90aa5e5b891f969
Christian Albrecht <christian.albrecht@knime.com>
2024-06-06 14:43:12 +0200

    Remove already moved to packages folders

--- vitest.setup.ts
+++
@@ -1,8 +0,0 @@
-import consola from "consola";
-import { config } from "@vue/test-utils";
-
-// @ts-ignore
-window.global.consola = consola;
-
-// see https://test-utils.vuejs.org/migration/#shallowmount-and-renderstubdefaultslot
-config.global.renderStubDefaultSlot = true;
