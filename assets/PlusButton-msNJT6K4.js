ec5d94c31f894d79598ee0e453a2e674694df2dc
bitbucket-pipelines <commits-noreply@bitbucket.org>
2024-07-02 08:04:42 +0000

    Deploy to GitHub pages

---
+++ assets/PlusButton-msNJT6K4.js
@@ -0,0 +1,112 @@
+import{C as f}from"./CodeExample-0hsQSmQx.js";import{B as h}from"./Button-2RymXQpp.js";import{u as v,T as k,_ as p,r as l,o as d,j as P,w as s,d as t,y,k as B,c as x,b as e,e as u,t as i}from"./index-zKANDt-b.js";import{P as g}from"./plus-small-8DBAb0_G.js";import{b as C}from"./Button-owOaj85Z.js";const w=v({components:{Button:h,Tooltip:k,PlusIcon:g},inheritAttrs:!1,props:{title:{type:String,default:null}}});function $(r,c,m,b,a,_){const n=l("PlusIcon"),o=l("Button");return d(),P(B(r.title?"Tooltip":"div"),{text:r.title},{default:s(()=>[t(o,y({class:"plus-button"},r.$attrs),{default:s(()=>[t(n)]),_:1},16)]),_:1},8,["text"])}const I=p(w,[["render",$],["__scopeId","data-v-8570bfe7"]]),T=`<script lang="ts">
+import { defineComponent } from "vue";
+import Button from "./Button.vue";
+import Tooltip from "./Tooltip.vue";
+import PlusIcon from "../assets/img/icons/plus-small.svg";
+
+export default defineComponent({
+  components: {
+    Button,
+    Tooltip,
+    PlusIcon,
+  },
+  inheritAttrs: false,
+  props: {
+    /**
+     * @see {@link Button.vue}
+     */
+
+    /**
+     * Button's title rendered as tooltip
+     * if null, the tooltip won't be rendered
+     */
+    title: {
+      type: String,
+      default: null,
+    },
+  },
+});
+<\/script>
+
+<template>
+  <Component :is="title ? 'Tooltip' : 'div'" :text="title">
+    <Button class="plus-button" v-bind="$attrs">
+      <PlusIcon />
+    </Button>
+  </Component>
+</template>
+
+<style lang="postcss" scoped>
+.tooltip {
+  width: 60px;
+}
+
+.plus-button {
+  --theme-button-border-radius: 9999px;
+
+  width: 60px;
+  height: 60px;
+  border: 0;
+  cursor: pointer;
+  background-color: transparent;
+  box-shadow:
+    0 0 10px var(--knime-gray-dark-semi),
+    0 0 4px var(--knime-gray-dark-semi);
+  display: flex;
+  justify-content: center;
+
+  &:hover {
+    box-shadow:
+      0 0 10px var(--knime-gray-dark-semi),
+      0 0 10px var(--knime-gray-dark-semi);
+  }
+
+  &.button {
+    display: flex;
+
+    & :deep(svg) {
+      width: 60px;
+      height: 60px;
+      stroke: var(--knime-dove-gray);
+      stroke-width: calc(32px / 60);
+      position: absolute;
+      margin: 0;
+      top: 0;
+    }
+  }
+
+  &.primary:hover {
+    background-color: var(--theme-button-background-color);
+
+    & :deep(svg) {
+      stroke: var(--theme-button-foreground-color);
+    }
+  }
+}
+</style>
+`,S=`
+  <PlusButton title="Plus button"/>
+
+  <PlusButton
+    title="Plus button on dark"
+    on-dark
+    with-border
+  />
+
+  <PlusButton
+    title="Primary plus button"
+    primary
+  />
+
+  <PlusButton
+    title="Primary plus button on dark"
+    primary
+    on-dark
+  />
+
+  <PlusButton
+    title="Disabled plus button"
+    primary
+    disabled
+  />
+`,E={components:{PlusButton:I,CodeExample:f},data(){return{PlusButtonCode:T,buttonCode:C,codeExample:S}}},D={class:"grid-container"},N={class:"grid-item-12"},V={class:"wrapper"},j={class:"background"},A={class:"background"};function q(r,c,m,b,a,_){const n=l("PlusButton",!0),o=l("CodeExample");return d(),x("section",null,[e("div",D,[e("div",N,[e("div",V,[t(n,{title:"Plus button"}),e("div",j,[t(n,{title:"Plus button on dark","on-dark":!0,"with-border":""})]),t(n,{title:"Primary plus button",primary:""}),e("div",A,[t(n,{title:"Primary plus button on dark",primary:"","on-dark":!0})]),t(n,{title:"Disabled plus button",primary:"",disabled:""})]),t(o,{summary:"Show usage example"},{default:s(()=>[u(i(a.codeExample),1)]),_:1}),t(o,{summary:"Show PlusButton.vue source code"},{default:s(()=>[u(i(a.PlusButtonCode),1)]),_:1}),t(o,{summary:"Show Button.vue source code"},{default:s(()=>[u(i(a.buttonCode),1)]),_:1})])])])}const K=p(E,[["render",q],["__scopeId","data-v-d4cf8648"]]);export{K as default};
--- assets/svgWithTitle-quNzbsAM.js
+++
@@ -1,1 +0,0 @@
-import{a3 as n}from"./index--jPh-4CA.js";const o=(r,s)=>({render(){const t=r.render.apply(this,n),e=n("title",s);return e.ns="svg",t.children.unshift(e),t}});export{o as s};
