05785fe34df62043d0cb1475ffc19c1dc7089be5
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2024-07-03 14:20:38 +0200

    Restructuring - setup vitest workspaces

--- packages/ui-extensions-renderer/vite.config.ts
+++ packages/ui-extensions-renderer/vite.config.ts
@@ -6,19 +6,6 @@ export default defineConfig({
   test: {
     include: ["**/__tests__/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     environment: "jsdom",
-    reporters: ["default", "junit"],
-    coverage: {
-      all: true,
-      exclude: [
-        "test-results/**",
-        "dist/**",
-        "**/*.d.ts",
-        "**/__tests__/**",
-        "**/*.config.{js,cjs,mjs,ts}",
-        "**/.{eslint,prettier,stylelint}rc.{js,cjs,yml}",
-      ],
-      reporter: ["html", "text", "lcov"],
-    },
     outputFile: {
       junit: "test-results/junit.xml", // needed for Bitbucket Pipeline, see https://support.atlassian.com/bitbucket-cloud/docs/test-reporting-in-pipelines/
     },
