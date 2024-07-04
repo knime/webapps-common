d953d7874930a1c06a62fb8d690d3bb7a4a106d4
Helian Rivera <helian.rivera@knime.com>
2024-07-04 15:04:22 +0200

    add coverage reporting

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    reporters: ["default", "junit"],
+    coverage: {
+      all: true,
+      exclude: [
+        "demo/",
+        "lint/",
+        "buildtools/",
+        "install-subDependencies.js",
+        "test-results/**",
+        "dist/**",
+        "**/*.d.ts",
+        "**/types.ts",
+        "**/__tests__/**",
+        "**/*.config.{js,cjs,mjs,ts}",
+        "**/.{eslint,prettier,stylelint}rc.{js,cjs,yml}",
+      ],
+      reporter: ["html", "text", "lcov"],
+    },
+  },
+});
