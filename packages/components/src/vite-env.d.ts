d78720a5e4cb70faf5723b8fd9b6e7fc5868ce77
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2024-07-05 09:48:08 +0200

    Fix type-check
    Remove vite-env.d.ts everywhere but in @knime/components.
    Otherwise, all of them are loaded and they overwrite each other.

--- packages/components/src/vite-env.d.ts
+++ packages/components/src/vite-env.d.ts
@@ -1,9 +1,3 @@
 /// <reference types="@knime/styles/config/svg.d.ts" />
 /// <reference types="vite/client" />
 /// <reference types="vite-svg-loader" />
-
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
