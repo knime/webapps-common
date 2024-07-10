c8d2cd29b2a836e2cd5562586e17d58c00b4bb9e
Helian Rivera <helian.rivera@knime.com>
2024-07-10 15:13:17 +0200

    add missing FileExplorer type exports

--- packages/components/src/components/FileExplorer/index.ts
+++ packages/components/src/components/FileExplorer/index.ts
@@ -1,5 +1,6 @@
 import FileExplorer from "./components/FileExplorer.vue";
 export * from "./composables/useItemDragging";
 export * from "./utils/dragGhostHelpers";
+export * from "./types";
 
 export { FileExplorer };
