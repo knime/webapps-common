44881aa75ef5a741c4c70a5be75949371e1975a6
Manuel Hotz <manuel.hotz@knime.com>
2024-07-22 16:14:21 +0200

    AP-22914: Add method for conversion to target type to `DynamicValuesInput`
    This fixes the problem that we clear user-supplied input when the user swaps between input columns that take
    values of the same format, e.g. Local Date and String cells using the ISO format.
    
    AP-22914 (Row Filter unexpectedly offers "ordering" comparison operators for some data types)

--- org.knime.core.ui.tests/src/eclipse/org/knime/core/webui/node/dialog/defaultdialog/widget/dynamic/DynamicValuesInputTest.java
+++ org.knime.core.ui.tests/src/eclipse/org/knime/core/webui/node/dialog/defaultdialog/widget/dynamic/DynamicValuesInputTest.java
@@ -48,14 +48,18 @@
  */
 package org.knime.core.webui.node.dialog.defaultdialog.widget.dynamic;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.knime.chem.types.SmilesAdapterCell;
 import org.knime.chem.types.SmilesCell;
 import org.knime.core.data.DataColumnSpecCreator;
+import org.knime.core.data.DataType;
+import org.knime.core.data.collection.CollectionCellFactory;
 import org.knime.core.data.def.StringCell;
 import org.knime.core.data.time.localdate.LocalDateCellFactory;
 import org.knime.core.node.InvalidSettingsException;
@@ -87,14 +91,40 @@ class DynamicValuesInputTest {
             .validate(dcs);
     }
 
+    @Test
+    void testIllegalMissingCellInput() throws IllegalArgumentException, InvalidSettingsException {
+        final var dcs = new DataColumnSpecCreator("stringCol", StringCell.TYPE).createSpec();
+        try {
+            new DynamicValuesInput(DataType.getMissingCell().getType(), DataType.getMissingCell()) //
+                .validate(dcs);
+        } catch (final IllegalArgumentException e) {
+            // expected
+            return;
+        }
+        failBecauseExceptionWasNotThrown(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testIllegalCollectionInput() throws IllegalArgumentException, InvalidSettingsException {
+        final var collection = CollectionCellFactory.createListCell(List.of(new StringCell("test")));
+        final var dcs = new DataColumnSpecCreator("stringCol", collection.getType()).createSpec();
+        try {
+            new DynamicValuesInput(collection.getType(), collection) //
+                .validate(dcs);
+        } catch (final IllegalArgumentException e) {
+            // expected
+            return;
+        }
+        failBecauseExceptionWasNotThrown(IllegalArgumentException.class);
+    }
+
     /**
      * Tests that the widget does not validate if error was stored (as StringCell).
      * @throws InvalidSettingsException expected since provided input was not valid
      */
     @Test
-    void testErrorHandling() throws InvalidSettingsException {
-        final var cell = LocalDateCellFactory.create(LocalDate.of(2024, 7, 10));
-        final var dcs = new DataColumnSpecCreator("localDateCol", cell.getType()).createSpec();
+    void testParseErrorHandling() throws InvalidSettingsException {
+        final var dcs = new DataColumnSpecCreator("localDateCol", LocalDateCellFactory.TYPE).createSpec();
         try {
             new DynamicValuesInput(dcs.getType(), new StringCell("Non-parseable-input")).validate(dcs);
         } catch (final InvalidSettingsException e) {
@@ -102,6 +132,91 @@ class DynamicValuesInputTest {
             return;
         }
         failBecauseExceptionWasNotThrown(InvalidSettingsException.class);
+    }
+
+    /**
+     * Tests that the widget does not validate if types do not fit.
+     * @throws InvalidSettingsException expected since provided input was not valid
+     */
+    @Test
+    void testTypeErrorHandling() throws InvalidSettingsException {
+        final var dcs = new DataColumnSpecCreator("localDateCol", LocalDateCellFactory.TYPE).createSpec();
+        try {
+            new DynamicValuesInput(StringCell.TYPE, new StringCell("Non-parseable-input")).validate(dcs);
+        } catch (final InvalidSettingsException e) {
+            // expected
+            return;
+        }
+        failBecauseExceptionWasNotThrown(InvalidSettingsException.class);
+    }
+
+    /**
+     * Tests that a round-trip conversion from local date to string and back works.
+     */
+    @Test
+    void testSuccessfulConversion() {
+        final var dateCell = LocalDateCellFactory.create(LocalDate.of(2024, 7, 10));
+        final var dateInput = new DynamicValuesInput(LocalDateCellFactory.TYPE, dateCell);
+
+        final var converted = dateInput
+            .convertToType(DynamicValuesInput.singleValueWithCaseMatchingForStringWithDefault(StringCell.TYPE));
+
+        assertThat(converted).isPresent().as("Local date value was successfully converted");
+        assertThat(converted).contains(new DynamicValuesInput(StringCell.TYPE, new StringCell("2024-07-10")))
+            .as("Local date value was converted to correct string value");
+
+        final var dateCellAgain = converted.orElseThrow().convertToType(
+            new DynamicValuesInput(LocalDateCellFactory.TYPE, LocalDateCellFactory.create(LocalDate.of(1970, 1, 1))));
+        assertThat(dateCellAgain).isPresent().as("String value was successfully converted");
+        assertThat(dateCellAgain).contains(dateInput)
+            .as("String value was successfully converted back to original local date");
+    }
+
+    /**
+     * Tests that a no-op conversion also round-trips.
+     */
+    @Test
+    void testNoopConversion() {
+        final var dateCell = LocalDateCellFactory.create(LocalDate.of(2024, 7, 10));
+        final var dateInput = new DynamicValuesInput(LocalDateCellFactory.TYPE, dateCell);
+
+        final var template =
+            new DynamicValuesInput(LocalDateCellFactory.TYPE, LocalDateCellFactory.create(LocalDate.of(1970, 1, 1)));
+        final var converted = dateInput.convertToType(template);
+
+        assertThat(converted).isPresent().as("Local date value was successfully converted");
+        assertThat(converted) //
+            .contains( //
+                new DynamicValuesInput( //
+                    LocalDateCellFactory.TYPE, LocalDateCellFactory.create(LocalDate.of(2024, 7, 10)))) //
+            .as("Local date value was used as-is");
+
+        final var dateCellAgain = converted.orElseThrow().convertToType(template);
+        assertThat(dateCellAgain).isPresent().as("Value was successfully converted");
+        assertThat(dateCellAgain) //
+            .contains(dateInput) //
+            .as("Value was successfully converted back to original local date");
+    }
+
+    /**
+     * Tests that a conversion from a string that does not represent a local date returns the template value.
+     */
+    @Test
+    void testConversionNotPossible() {
+        final var stringInput = DynamicValuesInput.singleValueWithCaseMatchingForStringWithDefault(StringCell.TYPE);
+        final var template =
+            new DynamicValuesInput(LocalDateCellFactory.TYPE, LocalDateCellFactory.create(LocalDate.of(2024, 7, 10)));
+        final var converted = stringInput.convertToType(template);
+        assertThat(converted).isPresent().as("Some value is returned");
+        assertThat(converted).contains(template).as("Template value is returned");
+    }
+
+    @Test
+    void testConversionErrorHandling() {
+        final var input =
+            new DynamicValuesInput(LocalDateCellFactory.TYPE, LocalDateCellFactory.create(LocalDate.of(2024, 7, 10)));
+        final var converted = input.convertToType(DynamicValuesInput.emptySingle());
+        assertThat(converted).isEmpty().as("Conversion should fail due to mismatched input kind");
     }
 
 }
