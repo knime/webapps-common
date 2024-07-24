b5c040aeff9741d05071bd5e2d1a56441fb7a93a
bitbucket-pipelines <commits-noreply@bitbucket.org>
2024-07-24 09:52:59 +0000

    Deploy to GitHub pages

---
+++ assets/Dropdown-ub5TYXRG.js
@@ -0,0 +1,70 @@
+import{C as z}from"./CodeExample-BbvffYQH.js";import{o as c,c as p,b as e,_ as y,r as x,d as l,t as s,w as u,e as a,j as g,k as w,a as I,p as E,f as k}from"./index-C28WOZ7t.js";import{D as A}from"./Dropdown-CD_CiVKC.js";import{L as T}from"./LoadingIcon-CVkKUNEo.js";import"./arrow-dropdown-Cuntu68t.js";import"./svgWithTitle-CGxZAqQv.js";const G={xmlns:"http://www.w3.org/2000/svg",fill:"none",stroke:"#000","stroke-linejoin":"round",viewBox:"0 0 32 32"},M=e("path",{d:"M27.5 3.214h-7c-.825 0-1.5.675-1.5 1.5v7c0 .825.675 1.5 1.5 1.5h7c.825 0 1.5-.675 1.5-1.5v-7c0-.825-.675-1.5-1.5-1.5zm-16 15.572h-7c-.825 0-1.5.675-1.5 1.5v7c0 .825.675 1.5 1.5 1.5h7c.825 0 1.5-.675 1.5-1.5v-7c0-.825-.675-1.5-1.5-1.5zm6.996-.068 4.879 4.879m-1.006-4.613A10.96 10.96 0 0 0 24 13.214M12.991 24.223c2.116 0 4.092-.597 5.77-1.632m-15.32-3.364 9.118 9.118m6.882-24.69 9.118 9.118"},null,-1),U=[M];function F(r,o){return c(),p("svg",G,[...U])}const S={render:F},L={xmlns:"http://www.w3.org/2000/svg",fill:"none",stroke:"#000","stroke-linejoin":"round",viewBox:"0 0 32 32"},C=e("path",{d:"M15.935 25.421c1.97-.503 4.186-1.836 6.674-4.325 9.805-9.805 6.442-17.866 6.442-17.866s-2.94-1.227-7.428-.039c-2.944.779-6.554 2.597-10.438 6.481-2.489 2.489-3.822 4.704-4.325 6.674zm9.125-7.087c1.379 1.379.76 4.234-1.382 6.376l-4.227 4.227-3.516-3.516M13.949 7.223c-1.379-1.379-4.234-.76-6.376 1.382l-4.227 4.227 3.514 3.514m22.392-6.371-6.947-6.948M9.238 18.724 5.502 22.46m-1.495 1.495-.854.854m4.864-.545-5.409 5.409m9.004-4.839-3.79 3.79"},null,-1),P=e("circle",{cx:"20.06",cy:"11.961",r:"3.858"},null,-1),j=[C,P];function H(r,o){return c(),p("svg",L,[...j])}const B={render:H},N={xmlns:"http://www.w3.org/2000/svg",fill:"none",stroke:"#000","stroke-linejoin":"round",viewBox:"0 0 32 32"},W=e("path",{d:"m16 27.716 13.785-16.782H2.215zM6.81 4.284l-4.595 6.65h9.189zm9.189 0-4.595 6.65h9.189zm9.189 0-4.595 6.65h9.19zm-4.595 6.65 4.595-6.65h-9.189zm-9.189 0 4.595-6.65H6.81zm9.189 0L16 27.716l-4.596-16.782"},null,-1),R=[W];function K(r,o){return c(),p("svg",N,[...R])}const f={render:K},q={xmlns:"http://www.w3.org/2000/svg",fill:"none",stroke:"#000","stroke-linejoin":"round",viewBox:"0 0 32 32"},J=e("path",{d:"M15.19 6.12c1.35-2.5 4-4.19 7.04-4.19 4.42 0 8 3.58 8 8 0 3.57-2.34 6.6-5.58 7.63m1.27-5.39-3.7-2.13V5.45M11.51 20.12a5 5 0 0 1-.23-1.51v-1.76m4.98 8.46c-.66.2-1.35.31-2.07.31a6.97 6.97 0 0 1-6.97-6.97c0-1.93.78-3.67 2.04-4.93a4.52 4.52 0 0 1-3.21-1.33c-1.77-1.77-1.77-4.65 0-6.42s4.65-1.77 6.42 0l15.59 19.58-9.4-1.82H16.4c-1.77 0-3.33-.9-4.25-2.27m2.04 4.16v3.47h-3.11M4.76 8.6l-2.98 1.53 3.44 1.13"},null,-1),O=e("line",{"stroke-linecap":"round",transform:"matrix(1.6 0 0 1.6 8.3 9)"},null,-1),Q=[J,O];function X(r,o){return c(),p("svg",q,[...Q])}const D={render:X},Y="",Z=`<Dropdown
+  v-model="selected"
+  aria-label="A Dropdown"
+  :possible-values="[{
+    id: 'foo',
+    text: 'Foo'
+  }, {
+    id: 'bar',
+    text: 'Bar'
+  }, {
+    id: 'baz',
+    text: 'Baz'
+  }]"
+/>`,$=`<Dropdown
+  v-model="slottedSelected"
+  aria-label="A Slotted dropdown"
+  :possible-values="[{
+      id: '1',
+      text: 'The Sundering',
+      slotData: {
+          icon: DisconnectIcon,
+          title: 'The Sundering',
+          subtitle: 'Gods of the Earth',
+          year: '2008'
+      }
+  }, {
+      id: '2',
+      text: 'Iron Swan',
+      slotData: {
+          icon: RocketIcon,
+          title: 'Iron Swan',
+          subtitle: 'Age of Winters',
+          year: '2006'
+      }
+  }, {
+      id: '3',
+      text: 'The Dreamthieves',
+      slotData: {
+          icon: DiamondIcon,
+          title: 'The Dreamthieves',
+          subtitle: 'Low Country',
+          year: '2016'
+      }
+  }, {
+      id: '4',
+      text: 'Twilight Sunrise',
+      slotData: {
+          icon: EarlyBirdIcon,
+          title: 'Twilight Sunrise',
+          subtitle: 'Used Future',
+          year: '2018'
+      }
+  }]"
+>
+  <template
+    #option="{ slotData: { icon, title, subtitle, year } } = {
+      slotData: {},
+    }"
+  >
+    <div class="slot-option">
+      <component :is="icon" />
+      <div class="description">
+        <div class="title">{{ title }}</div>
+        <div class="subtitle">{{ subtitle }}</div>
+      </div>
+      <div class="year">{{ year }}</div>
+    </div>
+  </template>
+</Dropdown>
+`,ee={components:{Dropdown:A,CodeExample:z,LoadingIcon:T,DisconnectIcon:S,RocketIcon:B,DiamondIcon:f,EarlyBirdIcon:D},data(){return{codeExample:Z,slottedCodeExample:$,selected:"bar",placeholderModel:"",disabledSelected:"",withSlotsSelected:"",slottedSelected:"1",dropupSelected:"bar",withGroupSelected:""}},computed:{slottedExamplePossibleValue(){return[{id:"1",text:"The Sundering",slotData:{icon:S,title:"The Sundering",subtitle:"Gods of the Earth",year:"2008"}},{id:"2",text:"Iron Swan",slotData:{icon:B,title:"Iron Swan",subtitle:"Age of Winters",year:"2006"}},{id:"3",text:"The Dreamthieves",slotData:{icon:f,title:"The Dreamthieves",subtitle:"Low Country",year:"2016"}},{id:"4",text:"Twilight Sunrise",slotData:{icon:D,title:"Twilight Sunrise",subtitle:"Used Future",year:"2018"}}]},code(){return Y}}},n=r=>(E("data-v-a8a7cbc8"),r=r(),k(),r),te=I('<div class="grid-container" data-v-a8a7cbc8><div class="grid-item-12" data-v-a8a7cbc8><p data-v-a8a7cbc8> A list of choices the user must choose one of them, so it emits an <code data-v-a8a7cbc8>input</code> event when something is selected, and it has a <code data-v-a8a7cbc8>value</code>. Keyboard navigation works (<code data-v-a8a7cbc8>Enter</code><code data-v-a8a7cbc8>Up</code>/<code data-v-a8a7cbc8>Down</code> and <code data-v-a8a7cbc8>Home</code>/<code data-v-a8a7cbc8>End</code>, leave with <code data-v-a8a7cbc8>Esc</code>). </p></div></div>',1),oe={class:"grid-container"},ie={class:"grid-item-5"},se={class:"grid-item-5"},le={class:"grid-item-2"},de=n(()=>e("div",{class:"grid-container"},[e("div",{class:"grid-item-12"},[e("p",null,[a(" The "),e("code",null,"placeholder"),a(" will be shown when no or empty "),e("code",null,"value"),a(" is set. Also it provides an invalid ("),e("code",null,"isValid=false"),a(") state. ")])])],-1)),ae={class:"grid-container"},re={class:"grid-item-5"},ne={class:"grid-item-5"},ce={class:"grid-item-2"},ue=n(()=>e("br",null,null,-1)),pe={class:"grid-container"},me={class:"grid-item-5"},he={class:"grid-item-2"},_e=n(()=>e("br",null,null,-1)),ve={class:"grid-container"},be={class:"grid-item-5"},xe={class:"grid-item-2"},ge=n(()=>e("br",null,null,-1)),we={class:"grid-container"},Se={class:"grid-item-5"},Be={class:"grid-item-2"},fe=n(()=>e("div",{class:"grid-container"},[e("div",{class:"grid-item-12"},[e("p",null,[a(" The optional "),e("code",null,"direction"),a(" property can be used to display the dropdown above the input field. ")])])],-1)),De={class:"grid-container"},Ve={class:"grid-item-5"},ze={class:"grid-item-2"},ye=n(()=>e("div",{class:"grid-container"},[e("div",{class:"grid-item-12"},[e("p",null,"Dropdown with option groups")])],-1)),Ie={class:"grid-container"},Ee={class:"grid-item-5"},ke={class:"grid-item-2"},Ae=n(()=>e("div",{class:"grid-container"},[e("div",{class:"grid-item-12"},[e("h4",null,"Slotted Dropdown"),e("p",null,[a(" The optional "),e("code",null,"slotData"),a(" property can be used to incorporate a slot into the dropdown list and render additional data in a styled fashion. The local value is passed through and available as a slot prop. Please keep in mind that the property names must match. ")])])],-1)),Te={class:"grid-container"},Ge={class:"grid-item-12"},Me={class:"grid-container"},Ue={class:"grid-item-5"},Fe={class:"slot-option"},Le={class:"description"},Ce={class:"title"},Pe={class:"subtitle"},je={class:"year"},He={class:"grid-item-2"},Ne=n(()=>e("br",null,null,-1)),We={class:"grid-container"},Re={class:"grid-item-5"},Ke={class:"slot-option"},qe={class:"description"},Je={class:"title"},Oe={class:"subtitle"},Qe={class:"year"},Xe={class:"grid-item-2"},Ye={class:"grid-container"},Ze={class:"grid-item-12"};function $e(r,o,et,tt,t,m){const d=x("Dropdown",!0),V=x("LoadingIcon"),h=x("CodeExample");return c(),p("div",null,[e("section",null,[te,e("div",oe,[e("div",ie,[l(d,{modelValue:t.selected,"onUpdate:modelValue":o[0]||(o[0]=i=>t.selected=i),"aria-label":"A List","possible-values":[{id:"foo",text:"Foo"},{id:"bar",text:"Bar"},{id:"baz",text:"Baz"}]},null,8,["modelValue"])]),e("div",se,[l(d,{modelValue:t.selected,"onUpdate:modelValue":o[1]||(o[1]=i=>t.selected=i),"aria-label":"A limited list",size:"3","possible-values":[{id:"foo",text:"Foo"},{id:"bar",text:"Bar"},{id:"bar2",text:"Bar 2"},{id:"bar3",text:"Bar 3"},{id:"bar4",text:"Bar 4"},{id:"bar5",text:"Bar 5"},{id:"bar6",text:"Bar 6"},{id:"bar7",text:"Bar 8"},{id:"bar9",text:"Bar 9"},{id:"bar10",text:"Bar 10"},{id:"bar11",text:"Bar 11"},{id:"baz",text:"Baz"}]},null,8,["modelValue"])]),e("div",le,"selected id: "+s(t.selected),1)]),de,e("div",ae,[e("div",re,[l(d,{modelValue:t.placeholderModel,"onUpdate:modelValue":o[2]||(o[2]=i=>t.placeholderModel=i),placeholder:"Placeholder…","aria-label":"A List","possible-values":[{id:"foo",text:"Foo"},{id:"bar",text:"Bar"},{id:"baz",text:"Baz"}]},null,8,["modelValue"])]),e("div",ne,[l(d,{modelValue:t.placeholderModel,"onUpdate:modelValue":o[3]||(o[3]=i=>t.placeholderModel=i),placeholder:"Placeholder…","is-valid":!1,"aria-label":"A limited list",size:"3","possible-values":[{id:"foo",text:"Foo"},{id:"bar",text:"Bar"},{id:"bar2",text:"Bar 2"},{id:"bar3",text:"Bar 3"},{id:"bar4",text:"Bar 4"},{id:"bar5",text:"Bar 5"},{id:"bar6",text:"Bar 6"},{id:"bar7",text:"Bar 8"},{id:"bar9",text:"Bar 9"},{id:"bar10",text:"Bar 10"},{id:"bar11",text:"Bar 11"},{id:"baz",text:"Baz"}]},null,8,["modelValue"])]),e("div",ce,"selected id: "+s(t.placeholderModel),1)]),ue,e("div",pe,[e("div",me,[l(d,{modelValue:t.disabledSelected,"onUpdate:modelValue":o[4]||(o[4]=i=>t.disabledSelected=i),placeholder:"Disabled...","aria-label":"A List","possible-values":[{id:"foo",text:"Foo"},{id:"bar",text:"Bar"},{id:"baz",text:"Baz"}],disabled:""},null,8,["modelValue"])]),e("div",he,"selected id: "+s(t.disabledSelected),1)]),_e,e("div",ve,[e("div",be,[l(d,{modelValue:t.withSlotsSelected,"onUpdate:modelValue":o[5]||(o[5]=i=>t.withSlotsSelected=i),placeholder:"With slots...","aria-label":"A List","possible-values":[{id:"foo",text:"Foo"},{id:"bar",text:"Bar"},{id:"baz",text:"Baz"}]},{"icon-right":u(()=>[l(V)]),_:1},8,["modelValue"])]),e("div",xe,"selected id: "+s(t.withSlotsSelected),1)]),ge,e("div",we,[e("div",Se,[l(d,{modelValue:t.withSlotsSelected,"onUpdate:modelValue":o[6]||(o[6]=i=>t.withSlotsSelected=i),placeholder:"In compact mode","aria-label":"A List",compact:"","possible-values":[{id:"foo",text:"Foo"},{id:"bar",text:"Bar"},{id:"baz",text:"Baz"}]},null,8,["modelValue"])]),e("div",Be,"selected id: "+s(t.withSlotsSelected),1)]),fe,e("div",De,[e("div",Ve,[l(d,{modelValue:t.dropupSelected,"onUpdate:modelValue":o[7]||(o[7]=i=>t.dropupSelected=i),"aria-label":"A Dropup","possible-values":[{id:"foo",text:"Foo"},{id:"bar",text:"Bar"},{id:"baz",text:"Baz"}],direction:"up"},null,8,["modelValue"])]),e("div",ze,"selected id: "+s(t.dropupSelected),1)]),ye,e("div",Ie,[e("div",Ee,[l(d,{modelValue:t.withGroupSelected,"onUpdate:modelValue":o[8]||(o[8]=i=>t.withGroupSelected=i),placeholder:"With groups","aria-label":"Dropdown with groups","possible-values":[{id:"foo",text:"Foo",group:"Group a"},{id:"bar",text:"Bar",group:"Group a"},{id:"bar2",text:"Bar 2",group:"Group a"},{id:"bar3",text:"Bar 3",group:"Group b"},{id:"bar4",text:"Bar 4",group:"Group b"},{id:"bar5",text:"Bar 5",group:"Group c"},{id:"bar6",text:"Bar 6",group:"Group c"},{id:"bar7",text:"Bar 8",group:"Group c"},{id:"bar9",text:"Bar 9",group:"Group c"},{id:"bar10",text:"Bar 10",group:"Group c"}]},null,8,["modelValue"])]),e("div",ke,"selected id: "+s(t.withGroupSelected),1)]),Ae]),e("section",null,[e("div",Te,[e("div",Ge,[l(h,{summary:"Show usage example"},{default:u(()=>[a(s(t.codeExample),1)]),_:1})])])]),e("section",null,[e("div",Me,[e("div",Ue,[l(d,{modelValue:t.slottedSelected,"onUpdate:modelValue":o[9]||(o[9]=i=>t.slottedSelected=i),"aria-label":"A limited list",size:"3","possible-values":m.slottedExamplePossibleValue},{option:u(({slotData:{icon:i,title:_,subtitle:v,year:b}}={slotData:{}})=>[e("div",Fe,[(c(),g(w(i))),e("div",Le,[e("div",Ce,s(_),1),e("div",Pe,s(v),1)]),e("div",je,s(b),1)])]),_:1},8,["modelValue","possible-values"])]),e("div",He,"selected id: "+s(t.slottedSelected),1)]),Ne,e("div",We,[e("div",Re,[l(d,{modelValue:t.withSlotsSelected,"onUpdate:modelValue":o[10]||(o[10]=i=>t.withSlotsSelected=i),placeholder:"In compact mode","aria-label":"A limited list",size:"3",compact:"","possible-values":m.slottedExamplePossibleValue},{option:u(({slotData:{icon:i,title:_,subtitle:v,year:b}}={slotData:{}})=>[e("div",Ke,[(c(),g(w(i))),e("div",qe,[e("div",Je,s(_),1),e("div",Oe,s(v),1)]),e("div",Qe,s(b),1)])]),_:1},8,["modelValue","possible-values"])]),e("div",Xe,"selected id: "+s(t.slottedSelected),1)])]),e("section",null,[e("div",Ye,[e("div",Ze,[l(h,{summary:"Show slotted usage example"},{default:u(()=>[a(s(t.slottedCodeExample),1)]),_:1}),l(h,{summary:"Show Dropdown.vue source code"},{default:u(()=>[a(s(m.code),1)]),_:1})])])])])}const rt=y(ee,[["render",$e],["__scopeId","data-v-a8a7cbc8"]]);export{rt as default};
