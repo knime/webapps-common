b3653f08c746894fceb4d18b20373de7ba77bb39
bitbucket-pipelines <commits-noreply@bitbucket.org>
2024-08-14 09:49:23 +0000

    Deploy to GitHub pages

---
+++ assets/useClickOutside-BADj84nE.js
@@ -0,0 +1,1 @@
+import{H as d,$ as r,J as u,B as f,a5 as c}from"./index-pv556zpM.js";const p=({targets:s,callback:n},i=f(!0))=>{let e;const o=()=>{e=c(s[0],n,{ignore:s})},t=()=>{typeof e<"u"&&e()};d(()=>r(i),a=>{a?o():t()},{immediate:!0}),u(t)};export{p as u};
