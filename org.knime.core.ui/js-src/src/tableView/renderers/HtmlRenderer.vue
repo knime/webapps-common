5c8e12124a84437a67bd5795e67eba066e44601f
Robin Gerling <robin.gerling@knime.com>
2024-08-20 17:55:31 +0200

    UIEXT-1337: Adapt HTML renderer to be at maximum as high and as wide as its content
    UIEXT-1337 (Enable "auto" size for row heights in table)

--- org.knime.core.ui/js-src/src/tableView/renderers/HtmlRenderer.vue
+++ org.knime.core.ui/js-src/src/tableView/renderers/HtmlRenderer.vue
@@ -11,5 +11,10 @@ defineProps<{
 <style scoped>
 .knime-cell-content {
   min-width: min-content;
+
+  /* Those properties are needed to ensure the correct calculation of auto column sizes and row heights. The max width
+  is responsible for the correct width and flex results in the div being as high as its content. */
+  max-width: fit-content;
+  display: flex;
 }
 </style>
