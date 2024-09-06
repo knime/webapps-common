dc6e71d405d02ebfab8f20aa32785e2d86ad9d91
bitbucket-pipelines <commits-noreply@bitbucket.org>
2024-09-06 13:12:44 +0000

    Deploy to GitHub pages

---
+++ assets/SplitButton-DbPEHGTN.js
@@ -0,0 +1,67 @@
+import{C as S}from"./CodeExample-Cfkh82Tb.js";import{H as B,S as g}from"./star-H0vgwfAt.js";import{H as x,L as I}from"./heart-D1LHTKpv.js";import{D as v}from"./arrow-dropdown-CUeKLWGw.js";import{_ as m,o as l,c as d,x as k,s,r as e,b as a,d as o,w as n,e as r,t as c,p as y,f as M}from"./index-CbvFh1bW.js";import{S as u}from"./SubMenu-BRmRU3YX.js";import{B as D}from"./Button-DUB3GgFQ.js";import"./MenuItems.vue_vue_type_style_index_0_lang-D5zgpvHG.js";import"./Checkbox-BAr2pgCr.js";import"./arrow-next-BZaosQgl.js";import"./useClickOutside-DX1n26h5.js";const C={},E={class:"split-button"};function H(t,f){return l(),d("div",E,[k(t.$slots,"default")])}const O=m(C,[["render",H],["__scopeId","data-v-fffb68b1"]]),$="",A=`<script>
+import Button from '~/webapps-common/ui/components/Button.vue';
+import SubMenu from '~/webapps-common/ui/components/SubMenu.vue';
+import SplitButton from '~/webapps-common/ui/components/SplitButton.vue';
+import DropdownIcon from '~/@knime/styles/img/icons/arrow-dropdown.svg';
+
+const subMenuItems = [{
+    href: 'https://apple.com',
+    text: 'Apples',
+    icon: HelpIcon
+}, {
+    href: 'https://en.wikipedia.org/wiki/Orange_(colour)',
+    text: 'Oranges',
+    icon: StarIcon
+},  {
+    to: '/testing-nuxt-link',
+    text: 'Ananas',
+    icon: HeartIcon
+}, {
+    href: 'https://www.urbandictionary.com/define.php?term=go%20bananas',
+    text: 'Bananas',
+    icon: LeaveIcon
+}];
+
+export default {
+    components: {
+        Button,
+        SubMenu,
+        SplitButton,
+        DropdownIcon
+    },
+    data() {
+        return {
+            subMenuItems
+        };
+    }
+};
+<\/script>
+
+<template>
+  <SplitButton>
+    <Button primary>Split button with submenu</Button>
+    <SubMenu
+      :items="subMenuItems"
+      class="submenu"
+      button-title="Open my submenu with icons"
+      orientation="left"
+    >
+      <DropdownIcon />
+    </SubMenu>
+  </SplitButton>
+</template>
+
+<style lang="postcss" scoped>
+.submenu {
+  background-color: var(--knime-yellow);
+
+  &:focus-within,
+  &:hover {
+    background-color: var(--knime-masala);
+
+    & .submenu-toggle svg {
+      stroke: var(--knime-white);
+    }
+  }
+}
+</style>`,L=[{href:"https://apple.com",text:"Apples",icon:s(B)},{href:"https://en.wikipedia.org/wiki/Orange_(colour)",text:"Oranges",icon:s(g)},{to:"/testing-nuxt-link",text:"Ananas",icon:s(x)},{href:"https://www.urbandictionary.com/define.php?term=go%20bananas",text:"Bananas",icon:s(I)}],N={components:{SubMenu:u,CodeExample:S,DropdownIcon:v,Button:D,SplitButton:O},data(){return{SubMenu:u,subMenuItems:L,codeExample:A,code:$}}},V=t=>(y("data-v-3714d245"),t=t(),M(),t),R={class:"grid-container"},T={class:"grid-item-12"},j=V(()=>a("p",null," A split button which combines a Button and SubMenu component. Both intentionally need to be added via the default slot to have full flexibility regarding their props and events. ",-1));function q(t,f,z,F,i,G){const _=e("Button"),b=e("DropdownIcon"),h=e("SubMenu"),w=e("SplitButton",!0),p=e("CodeExample");return l(),d("section",null,[a("div",R,[a("div",T,[j,a("div",null,[o(w,null,{default:n(()=>[o(_,{primary:""},{default:n(()=>[r("Split button with submenu")]),_:1}),o(h,{items:i.subMenuItems,class:"submenu","button-title":"Open my submenu with icons",orientation:"left"},{default:n(()=>[o(b)]),_:1},8,["items"])]),_:1})]),o(p,{summary:"Show usage example"},{default:n(()=>[r(c(i.codeExample),1)]),_:1}),o(p,{summary:"Show SplitButton.vue source code"},{default:n(()=>[r(c(i.code),1)]),_:1})])])])}const et=m(N,[["render",q],["__scopeId","data-v-3714d245"]]);export{et as default};
---
+++ assets/svgWithTitle-kdOWnKTM.js
@@ -0,0 +1,1 @@
+import{a6 as n}from"./index-CbvFh1bW.js";const o=(r,s)=>({render(){const t=r.render.apply(this,n),e=n("title",s);return e.ns="svg",t.children.unshift(e),t}});export{o as s};
