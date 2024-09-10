d71db58346697b4b3c95f3fc0732ef0bd118d10e
Paul Bärnreuther <paul.baernreuther@tngtech.com>
2024-09-10 08:45:02 +0200

    UIEXT-2141: Call data service to replace and sanitize
    UIEXT-2141 (TextView: Sanitize after flowVariable-template replacement)

--- org.knime.core.ui/js-src/src/textView/__tests__/TextView.test.js
+++ org.knime.core.ui/js-src/src/textView/__tests__/TextView.test.js
@@ -10,20 +10,14 @@ import flushPromises from "flush-promises";
 describe("TextView.vue", () => {
   let wrapper, initialDataSpy, addSharedDataListenerSpy;
 
-  const defaultContent = "test data";
-  const defaultFlowVariablesMap = {
-    key1: "value1",
-    key2: "value2",
-  };
+  const initialContent = "test data";
+  const dataServiceContent = "from data service";
 
-  const createJsonDataServiceMock = (
-    content = defaultContent,
-    flowVariablesMap = defaultFlowVariablesMap,
-  ) => ({
+  const createJsonDataServiceMock = () => ({
     initialData: vi.fn(() => ({
-      content,
-      flowVariablesMap,
+      content: initialContent,
     })),
+    data: vi.fn(() => dataServiceContent),
   });
 
   const createSharedDataServiceMock = () => ({
@@ -62,98 +56,28 @@ describe("TextView.vue", () => {
 
   it("calls initial data on mount", () => {
     expect(initialDataSpy).toHaveBeenCalled();
-    expect(wrapper.vm.richTextContent).toBe("test data");
+    expect(wrapper.vm.richTextContent).toBe(initialContent);
   });
 
   it("adds on data change callback on mount", () => {
     expect(addSharedDataListenerSpy).toHaveBeenCalled();
   });
 
-  describe("onViewSettingsChange", () => {
-    it("updates content on view settings change", async () => {
-      const data = {
-        data: {
-          view: {
-            richTextContent: "abcdefg",
-          },
+  it("updates content with sanitized data on view settings change", async () => {
+    const data = {
+      data: {
+        view: {
+          richTextContent:
+            'some unsanitized content with templates like $$["key1"] or $$["key2"]',
         },
-        flowVariableSettings: {},
-      };
-      await wrapper.vm.onViewSettingsChange(data);
-      expect(wrapper.vm.richTextContent).toStrictEqual(
-        data.data.view.richTextContent,
-      );
-    });
-
-    it("replaces flow variables in text content on view settings change", async () => {
-      const flowVariablesMap = {
-        key1: "value1",
-        key2: "value2",
-      };
-      const data = {
-        data: {
-          view: {
-            richTextContent: '$$["key1"] abc $$["key2"]',
-          },
-        },
-        flowVariableSettings: {},
-      };
-      wrapper.vm.flowVariablesMap = flowVariablesMap;
-      await wrapper.vm.onViewSettingsChange(data);
-      expect(wrapper.vm.richTextContent).toBe(
-        `${flowVariablesMap.key1} abc ${flowVariablesMap.key2}`,
-      );
-    });
-
-    it("replaces flow variables in text for escaped quotes", async () => {
-      const flowVariablesMap = {
-        key1: "value1",
-        key2: "value2",
-      };
-      const data = {
-        data: {
-          view: {
-            richTextContent: "$$[&#34;key1&#34;] abc $$[&#34;key2&#34;]",
-          },
-        },
-        flowVariableSettings: {},
-      };
-      wrapper.vm.flowVariablesMap = flowVariablesMap;
-      await wrapper.vm.onViewSettingsChange(data);
-      expect(wrapper.vm.richTextContent).toBe(
-        `${flowVariablesMap.key1} abc ${flowVariablesMap.key2}`,
-      );
-    });
-
-    it("does not change richTextContent if it is controlled by flow variable", async () => {
-      const data = {
-        data: {
-          view: {
-            richTextContent: "abcdefg",
-          },
-        },
-        flowVariableSettings: {
-          "view.richTextContent": {
-            controllingFlowVariableAvailable: true,
-          },
-        },
-      };
-      await wrapper.vm.onViewSettingsChange(data);
-      expect(wrapper.vm.richTextContent).toStrictEqual(defaultContent);
-    });
+      },
+      flowVariableSettings: {},
+    };
+    await wrapper.vm.onViewSettingsChange(data);
+    expect(wrapper.vm.richTextContent).toBe(dataServiceContent);
   });
 
-  it("replaces flow variables in text content on mount", async () => {
-    const content = '$$["key1"] $$["key2"]';
-    const jsonDataServiceMock = createJsonDataServiceMock(content);
-    const sharedDataServiceMock = createSharedDataServiceMock();
-    createSpies(jsonDataServiceMock, sharedDataServiceMock);
-    mountWrapper();
-    await wrapper.vm.$nextTick();
-    expect(wrapper.vm.richTextContent).toBe("value1 value2");
-  });
-
-  it("notifies pagebuilder when component is mounted if it is in reporting context", async () => {
+  it("notifies embedder when component is mounted if it is in reporting context", async () => {
     const { wrapper, setRenderCompleted } = shallowMountReportingComponent(
       TextView,
       true,
