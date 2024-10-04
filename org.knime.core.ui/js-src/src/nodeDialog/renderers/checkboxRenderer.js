knime/knime-core-ui

UIEXT-2071: made ui components async components
UIEXT-2071 (Diagnose Performance problems for Dialogs)

org.knime.core.ui/js-src/src/nodeDialog/renderers/checkboxRenderer.js
@@ -1,6 +1,11 @@
 import { rankWith, isBooleanControl } from "@jsonforms/core";
-import CheckboxInput from "../uiComponents/CheckboxInput.vue";
 import { priorityRanks, inputFormats } from "../constants";
+
+import { defineAsyncComponent } from "vue";
+
+const CheckboxInput = defineAsyncComponent(() =>
+  import("../uiComponents/CheckboxInput.vue"),
+);
 
 export const checkboxTester = (uischema, schema) => {
   const isBoolean = isBooleanControl(uischema, schema);

org.knime.core.ui/js-src/src/nodeDialog/renderers/comboBoxRenderer.js
@@ -1,6 +1,11 @@
 import { and, hasType, rankWith, schemaMatches } from "@jsonforms/core";
-import ComboBoxInput from "../uiComponents/ComboBoxInput.vue";
 import { inputFormats, priorityRanks } from "@/nodeDialog/constants";
+
+import { defineAsyncComponent } from "vue";
+
+const ComboBoxInput = defineAsyncComponent(() =>
+  import("../uiComponents/ComboBoxInput.vue"),
+);
 
 const isArray = schemaMatches((s) => hasType(s, "array"));
 const hasComboBoxFormat = (uischema, _schema) =>

org.knime.core.ui/js-src/src/nodeDialog/renderers/dateTimeRenderer.js
@@ -1,6 +1,10 @@
 import { rankWith, isDateTimeControl } from "@jsonforms/core";
 import { priorityRanks } from "../constants";
-import DateTimeInput from "../uiComponents/DateTimeInput.vue";
+import { defineAsyncComponent } from "vue";
+
+const DateTimeInput = defineAsyncComponent(() =>
+  import("../uiComponents/DateTimeInput.vue"),
+);
 
 export const dateTimeRenderer = {
   renderer: DateTimeInput,

org.knime.core.ui/js-src/src/nodeDialog/renderers/dropdownRenderer.js
@@ -1,6 +1,10 @@
 import { isControl, rankWith } from "@jsonforms/core";
-import DropdownInput from "../uiComponents/DropdownInput.vue";
 import { priorityRanks, inputFormats } from "../constants";
+import { defineAsyncComponent } from "vue";
+
+const DropdownInput = defineAsyncComponent(() =>
+  import("../uiComponents/DropdownInput.vue"),
+);
 
 export const dropdownTester = (uischema, _schema) =>
   isControl(uischema) && uischema.options?.format === inputFormats.dropDown;

org.knime.core.ui/js-src/src/nodeDialog/renderers/integerRenderer.js
@@ -1,6 +1,11 @@
 import { rankWith, isIntegerControl } from "@jsonforms/core";
-import IntegerInput from "../uiComponents/IntegerInput.vue";
 import { priorityRanks, inputFormats } from "../constants";
+
+import { defineAsyncComponent } from "vue";
+
+const IntegerInput = defineAsyncComponent(() =>
+  import("../uiComponents/IntegerInput.vue"),
+);
 
 export const integerTester = (uischema, schema) => {
   const isInteger = isIntegerControl(uischema, schema);

org.knime.core.ui/js-src/src/nodeDialog/renderers/sortListRenderer.js
@@ -1,6 +1,11 @@
 import { isControl, rankWith } from "@jsonforms/core";
-import SortListInput from "../uiComponents/SortListInput.vue";
 import { inputFormats, priorityRanks } from "../constants";
+
+import { defineAsyncComponent } from "vue";
+
+const SortListInput = defineAsyncComponent(() =>
+  import("../uiComponents/SortListInput.vue"),
+);
 
 export const sortListTester = (uischema, _schema) =>
   isControl(uischema) && uischema.options?.format === inputFormats.sortList;

org.knime.core.ui/js-src/src/nodeDialog/renderers/textRenderer.js
@@ -1,6 +1,11 @@
 import { rankWith, isStringControl } from "@jsonforms/core";
 import { priorityRanks } from "../constants";
-import TextInput from "../uiComponents/TextInput.vue";
+
+import { defineAsyncComponent } from "vue";
+
+const TextInput = defineAsyncComponent(() =>
+  import("../uiComponents/TextInput.vue"),
+);
 
 export const textTester = isStringControl;
 

knime/webapps-common

org.knime.core.ui/js-src/src/nodeDialog/renderers/checkboxRenderer.js
@@ -1,6 +1,11 @@
 import { rankWith, isBooleanControl } from "@jsonforms/core";
-import CheckboxControl from "../uiComponents/CheckboxControl.vue";
 import { priorityRanks, inputFormats } from "../constants";
+
+import { defineAsyncComponent } from "vue";
+
+const CheckboxControl = defineAsyncComponent(() =>
+  import("../uiComponents/CheckboxControl.vue"),
+);
 
 export const checkboxTester = (uischema, schema) => {
   const isBoolean = isBooleanControl(uischema, schema);

org.knime.core.ui/js-src/src/nodeDialog/renderers/comboBoxRenderer.js
@@ -1,6 +1,11 @@
 import { and, hasType, rankWith, schemaMatches } from "@jsonforms/core";
-import ComboBoxControl from "../uiComponents/ComboBoxControl.vue";
 import { inputFormats, priorityRanks } from "@/nodeDialog/constants";
+
+import { defineAsyncComponent } from "vue";
+
+const ComboBoxControl = defineAsyncComponent(() =>
+  import("../uiComponents/ComboBoxControl.vue"),
+);
 
 const isArray = schemaMatches((s) => hasType(s, "array"));
 const hasComboBoxFormat = (uischema, _schema) =>

org.knime.core.ui/js-src/src/nodeDialog/renderers/dateTimeRenderer.js
@@ -1,6 +1,10 @@
 import { rankWith, isDateTimeControl } from "@jsonforms/core";
 import { priorityRanks } from "../constants";
-import DateTimeControl from "../uiComponents/DateTimeControl.vue";
+import { defineAsyncComponent } from "vue";
+
+const DateTimeControl = defineAsyncComponent(() =>
+  import("../uiComponents/DateTimeControl.vue"),
+);
 
 export const dateTimeRenderer = {
   renderer: DateTimeControl,

org.knime.core.ui/js-src/src/nodeDialog/renderers/dropdownRenderer.js
@@ -1,6 +1,10 @@
 import { isControl, rankWith } from "@jsonforms/core";
-import DropdownControl from "../uiComponents/DropdownControl.vue";
 import { priorityRanks, inputFormats } from "../constants";
+import { defineAsyncComponent } from "vue";
+
+const DropdownControl = defineAsyncComponent(() =>
+  import("../uiComponents/DropdownControl.vue"),
+);
 
 export const dropdownTester = (uischema, _schema) =>
   isControl(uischema) && uischema.options?.format === inputFormats.dropDown;

org.knime.core.ui/js-src/src/nodeDialog/renderers/integerRenderer.js
@@ -1,6 +1,11 @@
 import { rankWith, isIntegerControl } from "@jsonforms/core";
-import IntegerControl from "../uiComponents/IntegerControl.vue";
 import { priorityRanks, inputFormats } from "../constants";
+
+import { defineAsyncComponent } from "vue";
+
+const IntegerControl = defineAsyncComponent(() =>
+  import("../uiComponents/IntegerControl.vue"),
+);
 
 export const integerTester = (uischema, schema) => {
   const isInteger = isIntegerControl(uischema, schema);

org.knime.core.ui/js-src/src/nodeDialog/renderers/sortListRenderer.js
@@ -1,6 +1,11 @@
 import { isControl, rankWith } from "@jsonforms/core";
-import SortListControl from "../uiComponents/SortListControl.vue";
 import { inputFormats, priorityRanks } from "../constants";
+
+import { defineAsyncComponent } from "vue";
+
+const SortListControl = defineAsyncComponent(() =>
+  import("../uiComponents/SortListControl.vue"),
+);
 
 export const sortListTester = (uischema, _schema) =>
   isControl(uischema) && uischema.options?.format === inputFormats.sortList;

org.knime.core.ui/js-src/src/nodeDialog/renderers/textRenderer.js
@@ -1,6 +1,11 @@
 import { rankWith, isStringControl } from "@jsonforms/core";
 import { priorityRanks } from "../constants";
-import TextControl from "../uiComponents/TextControl.vue";
+
+import { defineAsyncComponent } from "vue";
+
+const TextControl = defineAsyncComponent(() =>
+  import("../uiComponents/TextControl.vue"),
+);
 
 export const textTester = isStringControl;
 