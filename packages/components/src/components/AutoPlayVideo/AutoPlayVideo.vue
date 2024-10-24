a31bcdb8a9ec12973175e6b9d6bfa181c4f26158
Karl Glatz <karl@glatz.biz>
2024-10-24 15:09:51 +0200

    NXT-2986: components: improve AutoPlayVideo
    NXT-2986 (Product Hints for Creating Workflow, Adding Node and Connecting Node)

--- packages/components/src/components/AutoPlayVideo/AutoPlayVideo.vue
+++ packages/components/src/components/AutoPlayVideo/AutoPlayVideo.vue
@@ -3,8 +3,8 @@ import { computed, ref, watch } from "vue";
 import { useElementVisibility } from "@vueuse/core";
 
 interface Props {
-  withBorder: boolean;
-  aspectRatio: "256:135" | "16:10" | "16:9";
+  withBorder?: boolean;
+  aspectRatio?: "256:135" | "16:10" | "16:9";
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -33,7 +33,7 @@ watch(videoIsVisible, (isVisible) => {
 
 <template>
   <div :class="[aspectRatioClass, { 'with-border': withBorder }]">
-    <video v-bind="$attrs" ref="video" loop muted playsinline preload>
+    <video v-bind="$attrs" ref="video" loop muted playsinline preload="auto">
       <slot />
     </video>
   </div>
@@ -41,15 +41,16 @@ watch(videoIsVisible, (isVisible) => {
 
 <style lang="postcss" scoped>
 div {
-  position: relative;
-  padding-bottom: calc(100% * 135 / 265);
-
   &.ar-16-10 {
-    padding-bottom: calc(100% * 10 / 16);
+    aspect-ratio: 16 / 10;
   }
 
   &.ar-16-9 {
-    padding-bottom: calc(100% * 9 / 16);
+    aspect-ratio: 16 / 9;
+  }
+
+  &.ar-256-135 {
+    aspect-ratio: 256 / 135;
   }
 
   &.with-border {
@@ -57,9 +58,6 @@ div {
   }
 
   & video {
-    position: absolute;
-    top: 0;
-    left: 0;
     width: 100%;
     height: 100%;
   }
