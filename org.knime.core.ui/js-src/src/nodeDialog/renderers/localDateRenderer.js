52d1ca8039ebd16566716f359faf7b61876dde74
Tobias Kampmann <tobias.kampmann@tngtech.com>
2024-11-11 15:50:51 +0100

    UIEXT-1654: Remove isDateTimeControl (jsonForms internal) tester
    UIEXT-1654 (Web UI for Time Zone Modifier (aka Modify Time Zone))
    UIEXT-1654 ()

--- org.knime.core.ui/js-src/src/nodeDialog/renderers/localDateRenderer.js
+++ org.knime.core.ui/js-src/src/nodeDialog/renderers/localDateRenderer.js
@@ -1,4 +1,4 @@
-import { and, isDateTimeControl, rankWith } from "@jsonforms/core";
+import { rankWith } from "@jsonforms/core";
 import { priorityRanks, inputFormats } from "../constants";
 import { defineAsyncComponent } from "vue";
 
@@ -6,14 +6,11 @@ const DateControl = defineAsyncComponent({
   loader: () => import("../uiComponents/DateControl.vue"),
 });
 
-export const hasLocalTimeFormat = (uischema, _schema) =>
+export const hasLocalDateFormat = (uischema, _schema) =>
   uischema.options?.format === inputFormats.localDate;
 
 export const dateRenderer = {
   name: "DateControl",
   renderer: DateControl,
-  tester: rankWith(
-    priorityRanks.default,
-    and(isDateTimeControl, hasLocalTimeFormat),
-  ),
+  tester: rankWith(priorityRanks.default, hasLocalDateFormat),
 };
