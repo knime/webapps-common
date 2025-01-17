d6c1323ffe22454d86e3c21c669aae20092bf142
bitbucket-pipelines <commits-noreply@bitbucket.org>
2025-01-17 08:39:51 +0000

    Deploy to GitHub pages

---
+++ assets/useClickOutside-DFGdzLTb.js
@@ -0,0 +1,1 @@
+import{Z as u,u as d,am as r,j as f,an as c}from"./index-Dj2AlS1B.js";const p=({targets:s,callback:n},i=f(!0))=>{let e;const o=()=>{e=c(s[0],n,{ignore:s})},t=()=>{typeof e<"u"&&e()};u(()=>d(i),a=>{a?o():t()},{immediate:!0}),r(t)};export{p as u};
