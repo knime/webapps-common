adc04f700cab48aac8b1b5bdb2bbaf36339fc182
bitbucket-pipelines <commits-noreply@bitbucket.org>
2025-01-17 08:26:08 +0000

    Deploy to GitHub pages

---
+++ assets/SideDrawer-D1Iwi11p.js
@@ -0,0 +1,58 @@
+import{C as b}from"./CodeExample-C48iMfwn.js";import{_ as x,o as p,l as g,w as a,c as h,b as e,H as E,m as S,R as D,G as B,r,d as n,e as i,t as c,p as C,f as k}from"./index-BBUP89dB.js";const y={name:"SideDrawer",props:{isExpanded:{type:Boolean,default:!1}}},$={key:0,class:"side-drawer"},I={class:"content"};function q(t,o,u,w,s,d){return p(),g(D,{name:"slide"},{default:a(()=>[u.isExpanded?(p(),h("div",$,[e("div",I,[E(t.$slots,"default",{},void 0,!0)])])):S("",!0)]),_:3})}const N=x(y,[["render",q],["__scopeId","data-v-4a178677"]]),V="",M=`<script>
+import SideDrawer from '~/webapps-common/ui/components/SideDrawer.vue';
+import Button from '~/webapps-common/ui/components/Button.vue';
+
+export default {
+    components: {
+        SideDrawer,
+        Button,
+    },
+    data() {
+        return {
+            isExpanded: false
+        };
+    },
+    computed: {
+        expandedMessage() {
+            return this.isExpanded ? 'expanded' : 'not expanded';
+        }
+    }
+
+};
+<\/script>
+
+<template>
+  <SideDrawer
+      class="side-drawer"
+      :is-expanded="isExpanded"
+    >
+      <div class="contents-side-drawer">
+        <h4>something here</h4>
+        <p>And a lot more Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, nemo nostrum repellat voluptas,
+          nesciunt sequi velit earum iusto iste beatae hic perspiciatis deserunt exercitationem aut sapiente quas culpa
+          sint alias.</p>
+        <Button
+          with-border
+          @click="isExpanded = false"
+        >
+          Close me!
+        </Button>
+      </div>
+    </SideDrawer>
+</template>
+
+<style lang="postcss" scoped>
+
+h4 {
+  margin: 0;
+}
+
+.contents-side-drawer {
+  padding: 30px;
+  box-sizing: border-box;
+  background-color: var(--knime-white);
+  height: 100%;
+}
+
+</style>
+`,A={components:{SideDrawer:N,Button:B,CodeExample:b},data(){return{codeExample:M,isExpanded:!1}},computed:{code(){return V},expandedMessage(){return this.isExpanded?"expanded":"not expanded"}}},l=t=>(C("data-v-29b38b1b"),t=t(),k(),t),L={class:"grid-container"},T={class:"grid-item-12 wrapper"},z=l(()=>e("p",null," Provides an expandable drawer to the right side which can be filled with arbitrary content. On small screens it will take up the whole width. ",-1)),G={class:"contents-side-drawer"},H=l(()=>e("h4",null,"something here",-1)),O=l(()=>e("p",null," And a lot more Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, nemo nostrum repellat voluptas, nesciunt sequi velit earum iusto iste beatae hic perspiciatis deserunt exercitationem aut sapiente quas culpa sint alias. ",-1)),P={class:"grid-container"},R={class:"grid-item-12"};function j(t,o,u,w,s,d){const m=r("Button"),f=r("SideDrawer",!0),_=r("CodeExample");return p(),h("div",null,[e("section",null,[e("div",L,[e("div",T,[z,n(m,{primary:"",onClick:o[0]||(o[0]=v=>s.isExpanded=!s.isExpanded)},{default:a(()=>[i(" Draw it! ")]),_:1}),e("p",null,"I am "+c(d.expandedMessage),1)])])]),n(f,{class:"side-drawer","is-expanded":s.isExpanded},{default:a(()=>[e("div",G,[H,O,n(m,{"with-border":"",onClick:o[1]||(o[1]=v=>s.isExpanded=!1)},{default:a(()=>[i(" Close me! ")]),_:1})])]),_:1},8,["is-expanded"]),e("section",null,[e("div",P,[e("div",R,[n(_,{summary:"Show usage example"},{default:a(()=>[i(c(s.codeExample),1)]),_:1}),n(_,{summary:"Show SideDrawer.vue source code"},{default:a(()=>[i(c(d.code),1)]),_:1})])])])])}const K=x(A,[["render",j],["__scopeId","data-v-29b38b1b"]]);export{K as default};
---
+++ assets/StyledListItem-CI_KoGc_.js
@@ -0,0 +1,26 @@
+import{C as m}from"./CodeExample-C48iMfwn.js";import{_ as r,r as a,o as p,c as _,b as t,d as e,w as d,e as c,t as n,p as u,f as x}from"./index-BBUP89dB.js";import{S}from"./StyledListItem-reMnR1zm.js";const h="",I=`<ul>
+  <StyledListItem
+    text="Normal"
+  />
+  <StyledListItem
+    text="Selected"
+    selected
+  />
+  <StyledListItem
+    text="Special"
+    special
+  />
+  <StyledListItem
+    text="Invalid"
+    invalid
+  />
+  <StyledListItem
+    text="Disabled"
+    disabled
+  />
+  <StyledListItem
+    text="With line height"
+    :line-height="40"
+  />
+</ul>
+`,y={components:{StyledListItem:S,CodeExample:m},data(){return{code:h,codeExample:I}}},v=o=>(u("data-v-cbc292a4"),o=o(),x(),o),f={class:"grid-container"},L={class:"grid-item-12"},g=v(()=>t("p",null,"A li element with several styles",-1)),b={class:"grid-container"},w={class:"grid-item-12"};function C(o,E,N,B,l,D){const s=a("StyledListItem",!0),i=a("CodeExample");return p(),_("section",null,[t("section",null,[t("div",f,[t("div",L,[g,t("ul",null,[e(s,{text:"Normal"}),e(s,{text:"Selected",selected:""}),e(s,{text:"Special",special:""}),e(s,{text:"Invalid",invalid:""}),e(s,{text:"Disabled",disabled:""}),e(s,{text:"With line height","line-height":40})])])])]),t("section",null,[t("div",b,[t("div",w,[e(i,{summary:"Show usage example"},{default:d(()=>[c(n(l.codeExample),1)]),_:1}),e(i,{summary:"Show SubMenu.vue source code"},{default:d(()=>[c(n(l.code),1)]),_:1})])])])])}const W=r(y,[["render",C],["__scopeId","data-v-cbc292a4"]]);export{W as default};
