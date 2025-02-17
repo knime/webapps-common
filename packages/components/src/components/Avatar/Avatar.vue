451658dc37d9c878cf00113a57d4d6e07d18767c
Philipp Bitzer <philipp.bitzer@tngtech.com>
2025-02-17 18:54:27 +0100

    NXT-2989: Add generalized avatar component
    NXT-2989 ()

---
+++ packages/components/src/components/Avatar/Avatar.vue
@@ -0,0 +1,251 @@
+<script setup lang="ts">
+// Port last synced on 2025-02-04
+
+import { computed } from "vue";
+
+import ShieldIcon from "@knime/styles/img/icons/shield.svg";
+
+import { resolveNuxtLinkComponent } from "../nuxtComponentResolver";
+
+import AvatarImagePlaceholder from "./AvatarImagePlaceholder.vue";
+
+/**
+ * BaseAvatar component which displays an avatar image as well as a text.
+ * The text can be either shown below or right next to it.
+ *
+ * The size can be configured via the CSS variable --avatar-size-px
+ */
+
+type AvatarProps = {
+  /**
+   * The image to show as the avatar. If omitted, a placeholder is used.
+   */
+  image?: { url?: string | null; altText?: string };
+  /**
+   * Name that belongs to avatar.
+   */
+  name?: string;
+  /**
+   * Additional descriptor that is shown below name.
+   */
+  descriptor?: string;
+  /**
+   * Custom tooltip to show on the displayed avatar. By default the name is used.
+   */
+  tooltip?: string | null;
+  /**
+   * Show text alongside the avatar.
+   */
+  showText?: "name" | "placeholder" | "none";
+  /**
+   * Display avatar and name either vertically or horizontally next to each other.
+   */
+  layout?: "vertical" | "horizontal";
+  /**
+   * Display "trusted" badge or not.
+   */
+  showTrusted?: boolean;
+  /**
+   * Display BaseAvatar as link if linkUrl is provided.
+   */
+  linkUrl?: string | null;
+};
+
+const props = withDefaults(defineProps<AvatarProps>(), {
+  name: "",
+  descriptor: "",
+  tooltip: null,
+  showText: "none",
+  layout: "vertical",
+  image: () => ({}),
+  linkUrl: null,
+  rootComponent: null,
+});
+
+const title = computed(() => {
+  return props.tooltip || props.name;
+});
+
+const rootComponent = computed(() => {
+  return props.linkUrl ? resolveNuxtLinkComponent() : "div";
+});
+</script>
+
+<template>
+  <Component
+    :is="rootComponent"
+    :to="linkUrl"
+    :class="['base-avatar', layout, { link: Boolean(linkUrl) }]"
+    :title="title"
+  >
+    <div class="avatar-frame">
+      <slot>
+        <img
+          v-if="image?.url"
+          class="avatar-image"
+          :src="image?.url"
+          :alt="image?.altText"
+        />
+        <AvatarImagePlaceholder v-else class="image-placeholder" :name="name" />
+      </slot>
+      <div v-if="$slots.overlay" class="overlay">
+        <slot name="overlay" />
+      </div>
+    </div>
+
+    <div
+      v-if="showText !== 'none'"
+      class="avatar-label"
+      :class="{ 'with-descriptor': descriptor }"
+    >
+      <div v-if="showText === 'placeholder'" class="text-placeholder" />
+      <template v-else>
+        <div>
+          <div class="display-name" :class="{ trusted: showTrusted }">
+            {{ name }}
+          </div>
+          <ShieldIcon v-if="showTrusted" class="trusted" />
+        </div>
+        <div v-if="descriptor" class="descriptor">{{ descriptor }}</div>
+      </template>
+    </div>
+  </Component>
+</template>
+
+<style lang="postcss" scoped>
+.base-avatar {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 2px; /* make room for box-shadow */
+
+  & .avatar-frame {
+    position: relative;
+    border-radius: 50%;
+    overflow: hidden;
+    width: var(--avatar-size-px, 30px);
+    aspect-ratio: 1/1;
+    background-color: var(--knime-porcelain);
+    flex-shrink: 0;
+
+    /* use box-shadow instead of border to prevent zoom issues */
+    box-shadow: 0 0 0 2px var(--box-shadow-color, var(--knime-porcelain));
+
+    & .image-placeholder {
+      --placeholder-size: var(--avatar-size-px, 30px);
+    }
+
+    & img.avatar-image {
+      background-color: var(--knime-porcelain);
+      color: transparent; /* hide alt text while loading in Firefox */
+      width: 100%;
+      height: 100%;
+    }
+
+    & .overlay {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+    }
+  }
+
+  & .avatar-label {
+    display: block;
+    font-weight: 300;
+    font-size: 13px;
+    max-width: 100%;
+    padding-top: 3px; /* center it visually a bit better */
+    overflow: hidden;
+    line-height: 1.2;
+    text-align: left;
+
+    &.with-descriptor {
+      font-weight: 500;
+      font-size: 13px;
+      color: var(--knime-dove-gray);
+      padding-top: 2px;
+      padding-bottom: 2px;
+      padding-right: 4px;
+    }
+
+    & .display-name {
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      display: inline-block;
+      max-width: 100%;
+
+      &.trusted {
+        max-width: calc(100% - 11px); /* make room for icon */
+      }
+    }
+
+    & .descriptor {
+      font-weight: 400;
+      font-size: 11px;
+      color: var(--knime-dove-grey);
+    }
+
+    & svg.trusted {
+      display: inline-block;
+      position: relative;
+      top: -6px;
+      border-radius: 100%;
+      width: 11px;
+      height: 11px;
+      stroke: var(--knime-masala);
+      stroke-width: 2px;
+    }
+
+    & .text-placeholder {
+      display: inline-block;
+      width: 80px;
+      height: 12px;
+      background: linear-gradient(
+        to right,
+        var(--knime-porcelain) 0%,
+        var(--knime-silver-sand-semi) 25%,
+        var(--knime-porcelain) 50%
+      );
+      background-size: 200px 20px;
+      background-position: -50px 0;
+      animation: knightRider 2s linear infinite;
+    }
+  }
+
+  &.vertical {
+    justify-content: center;
+    flex-direction: column;
+
+    & .avatar-label {
+      padding-top: 0;
+      padding-left: 10px;
+      padding-right: 10px;
+    }
+  }
+}
+
+.base-avatar.link {
+  pointer-events: all;
+  max-width: 100%;
+  text-decoration: none;
+
+  &:focus {
+    outline: none;
+  }
+
+  & .avatar-frame {
+    transition: transform 0.15s ease-in-out;
+  }
+
+  &:hover,
+  &:focus {
+    & .avatar-frame {
+      --box-shadow-color: var(--knime-masala);
+
+      transform: scale(1.15);
+    }
+  }
+}
+</style>
---
+++ packages/components/src/components/Avatar/AvatarImagePlaceholder.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+// Port last synced on 2025-02-04
+
+import { computed } from "vue";
+import ColorHash from "color-hash/dist/esm";
+
+const { name } = defineProps<{
+  name: string;
+}>();
+
+// From secondary colors of @knime/styles/colors/knimeColors.js
+/* eslint-disable no-magic-numbers */
+const colors = [
+  [188, 63, 71.4],
+  [193, 60.9, 43.1],
+  [182, 63.3, 88.2],
+  [78, 38.7, 75.7],
+  [60, 23, 49.4],
+  [60, 50.8, 87.3],
+  [29, 100, 59.8],
+  [24, 78.2, 48.6],
+  [40, 100, 80],
+  [0, 100, 64.7],
+  [0, 69.5, 41.2],
+  [0, 91.5, 86.1],
+  [206, 69.7, 38.8],
+  [216, 64.9, 29],
+  [205, 71.9, 74.9],
+  [327, 82.3, 71.2],
+  [329, 71.4, 52],
+  [329, 72.3, 87.3],
+  [305, 27.7, 46.1],
+  [295, 34.2, 29.8],
+  [263, 40.4, 77.6],
+  [128, 50, 47.1],
+  [142, 58.7, 29.4],
+  [70, 78.3, 54.9],
+  [173, 37.2, 43.7],
+  [183, 100, 17.5],
+  [168, 57.4, 78.8],
+  [0, 0, 66.7],
+  [60, 0.4, 48.4],
+  [220, 4.3, 86.5],
+  [24, 46.4, 67.1],
+  [26, 33, 35.1],
+  [30, 100, 85.1],
+];
+/* eslint-enable no-magic-numbers */
+
+const hue = colors.map((color) => ({ min: color[0], max: color[0] }));
+
+const colorHash = new ColorHash({ hue });
+
+const initials = computed(() => {
+  return name
+    .split(/[., _-]/, 2)
+    .map((part) => part.charAt(0).toUpperCase())
+    .join("");
+});
+const styles = computed(() => {
+  return `background-color: ${colorHash.hex(name)}`;
+});
+</script>
+
+<template>
+  <div class="avatar-placeholder avatar-missing" :style="styles">
+    <span>{{ initials }}</span>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.avatar-placeholder {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  background-color: var(--knime-silver-sand);
+  border-radius: 50%;
+
+  /* use box-shadow instead of border to prevent zoom issues */
+  box-shadow: 0 0 0 2px var(--box-shadow-color, var(--knime-porcelain));
+}
+
+span {
+  font-size: calc(var(--placeholder-size, 45px) / 2);
+  color: var(--knime-white);
+  font-weight: 400;
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  line-height: 0;
+  pointer-events: none;
+  opacity: 0.8;
+}
+</style>
