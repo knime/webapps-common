f9474f9d59f2b318a69d72b512043770d7150843
Philipp Bitzer <philipp.bitzer@tngtech.com>
2025-02-17 18:54:27 +0100

    NXT-2989: Add generalized avatar component
    NXT-2989 (Use version panel UI component from hub in AP)

---
+++ demo/src/components/Avatar.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import { Avatar } from "@knime/components";
+</script>
+
+<template>
+  <section>
+    <div class="grid-container">
+      <div class="grid-item-12">
+        <p>A circular avatar, optionally alongside an associated name text.</p>
+      </div>
+    </div>
+
+    <div
+      class="grid-container flex-container"
+      style="--avatar-size: 42px; gap: 50px"
+    >
+      <div>
+        <p>Image avatar:</p>
+        <Avatar :image="{ url: 'favicon.png' }" />
+      </div>
+      <div>
+        <p>Initials placeholder:</p>
+        <Avatar name="KNIME Name" />
+      </div>
+      <div>
+        <p>With multiple text options:</p>
+        <div class="flex-container">
+          <Avatar
+            :image="{ url: 'https://www.knime.com/images/knime-logo.svg' }"
+            name="KNIME Avatar"
+            show-text="name"
+          />
+          <Avatar
+            :image="{ url: 'https://www.knime.com/images/knime-logo.svg' }"
+            name="KNIME Avatar"
+            show-text="placeholder"
+          />
+          <Avatar
+            :image="{ url: 'https://www.knime.com/images/knime-logo.svg' }"
+            name="Trusted KNIME Avatar"
+            layout="horizontal"
+            descriptor="With optional descriptor"
+            show-trusted
+            show-text="name"
+          />
+        </div>
+      </div>
+      <div>
+        <p>With clickable link:</p>
+        <Avatar
+          :image="{ url: 'favicon.png' }"
+          name="KNIME Homepage"
+          show-text="name"
+          :link-url="{ url: 'https://www.knime.com/', external: true }"
+        />
+      </div>
+      <div>
+        <p>With different sizes:</p>
+        <div class="flex-container">
+          <Avatar
+            style="--avatar-size: initial"
+            :image="{ url: 'https://www.knime.com/images/knime-logo.svg' }"
+            name="Default"
+            show-text="name"
+          />
+          <Avatar
+            style="--avatar-size: 42px"
+            :image="{ url: 'https://www.knime.com/images/knime-logo.svg' }"
+            name="42 pixels"
+            show-text="name"
+          />
+          <Avatar
+            style="--avatar-size: 64px"
+            :image="{ url: 'https://www.knime.com/images/knime-logo.svg' }"
+            name="64 pixels"
+            show-text="name"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="postcss" scoped>
+.flex-container {
+  display: flex;
+  align-items: baseline;
+  flex-wrap: wrap;
+  width: 100%;
+  gap: 10px;
+
+  /* Fix missing contrast caused by transparent demo avatar image */
+  & :deep(.avatar-frame) {
+    background-color: var(--knime-white);
+  }
+}
+</style>
---
+++ packages/components/src/components/Avatar/AvatarImagePlaceholder.vue
@@ -0,0 +1,129 @@
+<script setup lang="ts">
+import { computed } from "vue";
+// @ts-ignore No types available for module
+import ColorHash from "color-hash/dist/esm";
+
+import {
+  Aquamarine,
+  AquamarineDark,
+  AquamarineLight,
+  Avocado,
+  AvocadoDark,
+  AvocadoLight,
+  Carrot,
+  CarrotDark,
+  CarrotLight,
+  Coral,
+  CoralDark,
+  CoralLight,
+  Cornflower,
+  CornflowerDark,
+  CornflowerLight,
+  Hibiscus,
+  HibiscusDark,
+  HibiscusLight,
+  Lavender,
+  LavenderDark,
+  LavenderLight,
+  Meadow,
+  MeadowDark,
+  MeadowLight,
+  Petrol,
+  PetrolDark,
+  PetrolLight,
+  Stone,
+  StoneDark,
+  StoneLight,
+  Wood,
+  WoodDark,
+  WoodLight,
+} from "@knime/styles/colors/knimeColors";
+
+const { name } = defineProps<{
+  name: string;
+}>();
+
+const secondaryColors = [
+  Aquamarine,
+  AquamarineDark,
+  AquamarineLight,
+  Avocado,
+  AvocadoDark,
+  AvocadoLight,
+  Carrot,
+  CarrotDark,
+  CarrotLight,
+  Coral,
+  CoralDark,
+  CoralLight,
+  Cornflower,
+  CornflowerDark,
+  CornflowerLight,
+  // CornflowerSemi,
+  Hibiscus,
+  HibiscusDark,
+  HibiscusLight,
+  Lavender,
+  LavenderDark,
+  LavenderLight,
+  Meadow,
+  MeadowDark,
+  MeadowLight,
+  Petrol,
+  PetrolDark,
+  PetrolLight,
+  Stone,
+  StoneDark,
+  StoneLight,
+  Wood,
+  WoodDark,
+  WoodLight,
+].map((hslString) => (hslString.match(/[\d.]+/g) ?? []).map(Number));
+
+const hue = secondaryColors.map((color) => ({ min: color[0], max: color[0] }));
+
+const colorHash = new ColorHash({ hue });
+
+const initials = computed(() => {
+  return name
+    .split(/[., _-]/, 2)
+    .map((part) => part.charAt(0).toUpperCase())
+    .join("");
+});
+const styles = computed(() => {
+  return `background-color: ${colorHash.hex(name)}`;
+});
+</script>
+
+<template>
+  <div class="avatar-placeholder avatar-missing" :style="styles">
+    <span>{{ initials }}</span>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.avatar-placeholder {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  background-color: var(--knime-silver-sand);
+  border-radius: 50%;
+
+  /* use box-shadow instead of border to prevent zoom issues */
+  box-shadow: 0 0 0 2px var(--box-shadow-color, var(--knime-porcelain));
+}
+
+span {
+  font-size: calc(var(--placeholder-size, 45px) / 2);
+  color: var(--knime-white);
+  font-weight: 400;
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  line-height: 0;
+  pointer-events: none;
+  opacity: 0.8;
+}
+</style>
