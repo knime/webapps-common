50dde6a9940b657d0d83c2c885c29dfedfca69b9
Philipp Bitzer <philipp.bitzer@tngtech.com>
2025-02-17 21:51:10 +0100

    NXT-2989: Add hub-feature versions components
    NXT-2989 ()

---
+++ packages/hub-features/src/components/avatars/HubGroupAvatar.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+// TODO: HUB port last synced on 2025-02-04
+
+import { computed } from "vue";
+
+import { Avatar } from "@knime/components";
+import AdminGroupIcon from "@knime/styles/img/icons/team-group-admin.svg";
+import MemberGroupIcon from "@knime/styles/img/icons/team-group.svg";
+
+import { ADMIN_GROUP_NAME } from "../../common/constants";
+
+import type { HubGroupAvatarData } from "./HubAvatar.vue";
+
+/**
+ * Displays the avatar image for a group.
+ * See HubAvatar for more details.
+ */
+const props = defineProps<HubGroupAvatarData>();
+
+const isAdminGroup = computed(() => {
+  return props.name === ADMIN_GROUP_NAME;
+});
+
+const membersCountPostfix = computed(() => {
+  if (props.membersCount === 0) {
+    return " (0 people)";
+  }
+  if (props.membersCount === 1) {
+    return " (1 person)";
+  }
+
+  return ` (${props.membersCount} people)`;
+});
+
+const displayText = computed(() => {
+  return `${props.name}${membersCountPostfix.value}`;
+});
+</script>
+
+<template>
+  <Avatar
+    :name="displayText"
+    :tooltip="displayText"
+    :show-text
+    :layout
+    :link-url
+    descriptor="Group"
+  >
+    <template #default>
+      <div class="container">
+        <AdminGroupIcon v-if="isAdminGroup" class="admin" />
+        <MemberGroupIcon v-else class="member" />
+      </div>
+    </template>
+
+    <template #overlay>
+      <slot />
+    </template>
+  </Avatar>
+</template>
+
+<style lang="postcss" scoped>
+.container {
+  position: relative;
+  background-color: var(--knime-white);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+
+  & > svg {
+    position: absolute;
+    stroke: var(--knime-dove-gray);
+    width: 24px;
+    height: 24px;
+    stroke-width: calc(32px / 24);
+  }
+}
+</style>
