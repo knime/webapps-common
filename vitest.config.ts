73450373dde9a50ac9c1f33dfea19ea3e7267658
Jakob Schröter <jakob.schroeter@knime.com>
2025-02-20 15:30:45 +0100

    try vitest summary: false

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    reporters: ["default", "junit"],
+    reporters: [["default", { summary: false }], "junit"],
     coverage: {
       all: true,
       exclude: [
