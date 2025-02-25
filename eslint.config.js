cd7fa63c539c8263a5efb88488545cebe943c4db
Robin Offel <robin.offel@knime.com>
2025-02-25 14:44:37 +0100

    FSR-64: DROP THIS COMMIT: temporarily disable certain eslint rules while going through to resolve the issues
    FSR-64 (Update to Eslint 9)

--- eslint.config.js
+++ eslint.config.js
@@ -25,4 +25,16 @@ export default [
       "import/extensions": "off",
     },
   },
+  // temporarily disable certain rules to be able to push first lint fixes
+  // and to be able to go through them one by one by commenting one out
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "prefer-const": "off",
+    },
+  },
 ];
