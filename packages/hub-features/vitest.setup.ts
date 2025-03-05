08425cf602f75515680f58b66e7385b90a657524
Jakob Schröter <jakob.schroeter@knime.com>
2025-03-05 15:23:33 +0100

    FSR-64: fix consola type in global window
    FSR-64 (Update to Eslint 9)

--- packages/hub-features/vitest.setup.ts
+++ packages/hub-features/vitest.setup.ts
@@ -2,8 +2,7 @@ import { type Mock, vi } from "vitest";
 import { config } from "@vue/test-utils";
 import consola from "consola";
 
-// @ts-expect-error migrated from ts-ignore to es-expect-error TODO: explain why error is expected
-window.global.consola = consola;
+window.consola = consola;
 
 // see https://test-utils.vuejs.org/migration/#shallowmount-and-renderstubdefaultslot
 config.global.renderStubDefaultSlot = true;
