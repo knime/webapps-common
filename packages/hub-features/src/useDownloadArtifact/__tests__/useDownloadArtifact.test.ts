668503f2f4a7a3262b6e72774564e907f14d6da0
Ramzi <ramzi.muthana@knime.com>
2025-03-06 10:47:32 +0100

    HUB-9830: Implemented HUB feature downloading artificat feature
    HUB-9830 (Implement the downloading logic in WAC)

---
+++ packages/hub-features/src/useDownloadArtifact/__tests__/useDownloadArtifact.test.ts
@@ -0,0 +1,110 @@
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { nextTick } from "vue";
+
+import { useDownloadArtifact } from "../useDownloadArtifact";
+
+describe("useDownloadArtifact", () => {
+  beforeEach(() => {
+    vi.resetAllMocks();
+    $ofetchMock.mockResolvedValueOnce({ downloadId: "testDownloadId" });
+  });
+
+  const doMount = ({ isDownloadReady = true } = {}) => {
+    if (isDownloadReady) {
+      $ofetchMock.mockResolvedValueOnce({
+        status: "READY",
+        downloadUrl: "testDownloadUrl",
+      });
+    }
+    const downloadComposable = useDownloadArtifact({
+      artifactId: "mockArtifact",
+      maxRetries: 3,
+      pollingInterval: 10,
+      customFetchClientOptions: { baseURL: "/_/api" },
+    });
+    const windowOpenSpy = vi.spyOn(window, "open");
+    const consolaErrorSpy = vi.spyOn(consola, "error");
+    return {
+      windowOpenSpy,
+      downloadComposable,
+      consolaErrorSpy,
+    };
+  };
+
+  it("sets isDownloading to true and false around downloadArtifact call", async () => {
+    const { downloadComposable, windowOpenSpy } = doMount();
+    const artifactParams = { itemId: "1234", version: 1 };
+    expect(downloadComposable.isDownloading.value).toBe(false);
+    const downloadPromise = downloadComposable.downloadArtifact(artifactParams);
+    expect(downloadComposable.isDownloading.value).toBe(true);
+    await downloadPromise;
+    expect(downloadComposable.isDownloading.value).toBe(false);
+    expect(windowOpenSpy).toHaveBeenCalledWith("testDownloadUrl", "_parent");
+  });
+
+  it("calls ofetch with correct route and query params for the initial request", async () => {
+    const { downloadComposable, windowOpenSpy } = doMount();
+    const artifactParams = { itemId: "1234", version: 1 };
+    const downloadPromise = downloadComposable.downloadArtifact(artifactParams);
+    expect(downloadComposable.isDownloading.value).toBe(true);
+    await downloadPromise;
+
+    expect($ofetchMock).toHaveBeenCalledTimes(2);
+    expect($ofetchMock).toHaveBeenCalledWith(
+      "/_/api/repository/1234/artifact",
+      {
+        method: "GET",
+        query: {
+          version: 1,
+        },
+      },
+    );
+
+    expect($ofetchMock).toHaveBeenCalledWith(
+      "/_/api/downloads/testDownloadId/status",
+      {
+        method: "GET",
+      },
+    );
+
+    expect(windowOpenSpy).toHaveBeenCalledWith("testDownloadUrl", "_parent");
+    expect(downloadComposable.isDownloading.value).toBe(false);
+  });
+
+  it("poll fetch until the download is ready", async () => {
+    const { downloadComposable, windowOpenSpy } = doMount({
+      isDownloadReady: false,
+    });
+    const artifactParams = { itemId: "1234", version: 1 };
+    const downloadPromise = downloadComposable.downloadArtifact(artifactParams);
+    expect(downloadComposable.isDownloading.value).toBe(true);
+
+    expect($ofetchMock).toHaveBeenCalledWith(
+      "/_/api/repository/1234/artifact",
+      {
+        method: "GET",
+        query: {
+          version: 1,
+        },
+      },
+    );
+    expect($ofetchMock).toHaveBeenCalledTimes(1);
+    await nextTick();
+    expect($ofetchMock).toHaveBeenCalledWith(
+      "/_/api/downloads/testDownloadId/status",
+      {
+        method: "GET",
+      },
+    );
+    expect($ofetchMock).toHaveBeenCalledTimes(2);
+    expect(windowOpenSpy).not.toBeCalled();
+
+    $ofetchMock.mockResolvedValueOnce({
+      status: "READY",
+      downloadUrl: "testDownloadUrl",
+    });
+    await downloadPromise;
+    expect($ofetchMock).toHaveBeenCalledTimes(3);
+    expect(windowOpenSpy).toHaveBeenCalledWith("testDownloadUrl", "_parent");
+  });
+});
---
+++ packages/hub-features/src/useDownloadArtifact/useDownloadArtifact.ts
@@ -0,0 +1,171 @@
+import { ref } from "vue";
+import { FetchError, type FetchOptions } from "ofetch";
+
+import { getFetchClient } from "../common/ofetchClient";
+import { rfcErrors } from "../rfcErrors";
+
+const DEFAULT_API_BASE_URL = "/_/api";
+
+type ArtifactStatus = {
+  downloadId?: string;
+  status?: string;
+  statusMessage?: string;
+  lastUpdated?: string;
+  downloadUrl?: string;
+};
+
+type RequestDownloadResponse = {
+  downloadId: string;
+};
+
+type ItemVersion = number | "current-state" | "most-recent";
+
+/**
+ *  This composable provides a loading state and a method to download an artifact from the repository and poll for the download URL.
+ *  The download is initiated by calling the `downloadArtifact` method with the artifact ID.
+ * The composable will poll the download status until it's ready, at which point it will open the download link in a same tab.
+ *
+ * Usage Example:
+ * ```
+ * const { isDownloading, downloadArtifact } = useDownloadArtifact({
+ *      artifactId: "*123abcd", // The artifact ID to download.
+ *      maxRetries: 10, // Optional: The maximum number of times to poll for the download URL.
+ *      pollingInterval: 2000, // Optional: The interval in milliseconds between each poll.
+ *      customFetchClientOptions: // Optional: Custom options to pass to the fetch client.
+ * });
+ *
+ * await downloadArtifact({ itemId: "1234", version: 1 });
+ * ```
+ *
+ *
+ */
+export const useDownloadArtifact = (options: {
+  /**
+   * The artifact ID to download.
+   */
+  artifactId: string;
+  /**
+   * The maximum number of times to poll for the download URL.
+   */
+  maxRetries?: number;
+  /**
+   * The interval in milliseconds between each poll.
+   */
+  pollingInterval?: number;
+  /**
+   * Custom options to pass to the fetch client.
+   * @example { baseURL: "/_/api" }
+   */
+  customFetchClientOptions?: FetchOptions;
+}) => {
+  const isDownloading = ref(false);
+  const downloadPath = ref("");
+
+  const $ofetch = getFetchClient(options.customFetchClientOptions);
+  const baseUrl =
+    options.customFetchClientOptions?.baseURL ?? DEFAULT_API_BASE_URL;
+
+  // Use fallback values if none are provided
+  const maxRetries = options.maxRetries ?? null;
+  const pollingInterval = options.pollingInterval ?? 2000;
+
+  /**
+   * Poll the artifact status until it's ready (or we run out of retries).
+   * Once ready, open the download link in a new tab.
+   */
+  const checkAndDownloadItem = (downloadId: string) => {
+    let tries = 0;
+
+    return new Promise<void>((resolve, reject) => {
+      const poll = async () => {
+        tries++;
+
+        try {
+          const statusResponse = await $ofetch<ArtifactStatus>(
+            `${baseUrl}/downloads/${downloadId}/status`,
+            {
+              method: "GET",
+            },
+          );
+
+          if (
+            statusResponse?.status === "READY" &&
+            statusResponse?.downloadUrl
+          ) {
+            downloadPath.value = statusResponse.downloadUrl;
+            window.open(downloadPath.value, "_parent");
+            resolve();
+            return;
+          }
+
+          //  If the download is not ready, poll again after the interval.
+          if (!maxRetries || tries < maxRetries) {
+            setTimeout(poll, pollingInterval);
+          } else {
+            reject(
+              new FetchError(
+                `Download not ready after ${maxRetries} attempts.`,
+              ),
+            );
+          }
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      poll();
+    });
+  };
+
+  /**
+   * Initiates the download request and starts polling for to get download URL.
+   */
+  const downloadArtifact = async ({
+    itemId,
+    version,
+  }: {
+    itemId: string;
+    version?: ItemVersion;
+  }) => {
+    isDownloading.value = true;
+
+    try {
+      const downloadResponse = await $ofetch<RequestDownloadResponse>(
+        `${baseUrl}/repository/${itemId}/artifact`,
+        {
+          method: "GET",
+          query: {
+            version,
+          },
+        },
+      );
+
+      if (downloadResponse?.downloadId) {
+        await checkAndDownloadItem(downloadResponse.downloadId);
+      }
+    } catch (error) {
+      consola.error("Error fetching", error);
+      // Rethrow the errors to be handled by toast in hub
+      if (error instanceof FetchError) {
+        throw rfcErrors.tryParse(error);
+      }
+
+      throw error;
+    } finally {
+      isDownloading.value = false;
+    }
+  };
+
+  return {
+    /**
+     * Reactive ref that indicates if a download is in progress.
+     */
+    isDownloading,
+    /**
+     * The download method of the artifact.
+     * @param itemId The item ID of the artifact to download.
+     * @param version The version of the artifact to download.
+     */
+    downloadArtifact,
+  };
+};
