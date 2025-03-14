5e3dc8a8bcfe5291fdb2baba2acde5fb0e6d0eab
David Hickey <david.hickey@tngtech.com>
2025-03-14 16:49:06 +0100

    UIEXT-1803: Export SectionHeading component
    UIEXT-1803 (File Chooser to Support Multiple File Handling)

--- packages/jsonforms/src/layoutComponents/index.ts
+++ packages/jsonforms/src/layoutComponents/index.ts
@@ -1,5 +1,6 @@
 import Form from "./Form.vue";
 import VerticalLayoutBase from "./VerticalLayoutBase.vue";
+import SectionHeading from "./section/SectionHeading.vue";
 import SettingsSubPanel from "./settingsSubPanel/SettingsSubPanel.vue";
 
-export { Form, VerticalLayoutBase, SettingsSubPanel };
+export { Form, VerticalLayoutBase, SettingsSubPanel, SectionHeading };
