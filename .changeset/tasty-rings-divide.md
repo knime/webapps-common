ee229d3a6680567f878fb61d070b3ad9d13ebc8b
Rupert Ettrich <rupert.ettrich@tngtech.com>
2025-03-18 09:13:29 +0100

    HUB-9784: add isWindows to navigator utils
    HUB-9784 (Switch the single items download GET :data calls to GET /artifacts  plus /downloads/DOWNLOAD_ID/status )

---
+++ .changeset/tasty-rings-divide.md
@@ -0,0 +1,5 @@
+---
+"@knime/utils": patch
+---
+
+add isWindows check to navigator utils
--- packages/utils/src/navigator.ts
+++ packages/utils/src/navigator.ts
@@ -1,9 +1,16 @@
 /**
  *
- * @returns true when the platform of the user is Mac
+ * @returns true if the platform of the user is Mac
  */
 export const isMac = (): boolean =>
   navigator?.userAgent?.toLowerCase()?.includes("mac");
+
+/**
+ *
+ * @returns true if the platform of the user is Windows
+ */
+export const isWindows = (): boolean =>
+  navigator?.userAgent?.toLowerCase()?.includes("windows");
 
 /**
  * @deprecated Since version 1.3.6.  Use `hotkeys.ts` instead.
