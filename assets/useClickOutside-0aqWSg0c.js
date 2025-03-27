d67b611a8e59a69019e63e0618b196531b1bb5af
bitbucket-pipelines <commits-noreply@bitbucket.org>
2025-03-27 13:27:00 +0000

    Deploy to GitHub pages

--- assets/useClickOutside-0aqWSg0c.js
+++
@@ -1,1 +0,0 @@
-import{Z as u,u as d,aj as r,j as f,ak as c}from"./index-CPQ6JZIw.js";const p=({targets:s,callback:n},i=f(!0))=>{let e;const o=()=>{e=c(s[0],n,{ignore:s})},t=()=>{typeof e<"u"&&e()};u(()=>d(i),a=>{a?o():t()},{immediate:!0}),r(t)};export{p as u};
