db22cfb5271195ea30f5bf67a2fee2677167f485
bitbucket-pipelines <commits-noreply@bitbucket.org>
2025-04-02 13:35:12 +0000

    Deploy to GitHub pages

--- assets/trash-CLi1Jgvj.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as o,b as t}from"./index-CgG797fR.js";const n={xmlns:"http://www.w3.org/2000/svg",fill:"none",stroke:"#000","stroke-linejoin":"round",viewBox:"0 0 32 32"},s=t("path",{d:"m26 7-3 22.5H9L6 7M4 7h24M12 7V2.5h8V7"},null,-1),r=[s];function c(a,h){return e(),o("svg",n,[...r])}const l={render:c};export{l as T};
