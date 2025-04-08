a5c359f7e8bf0b76b426e7490e04688df1a85b95
Rupert Ettrich <rupert.ettrich@tngtech.com>
2025-04-08 09:09:33 +0200

    HUB-10052: add forgotten export for useAutoCloseOnCompletion composable
    HUB-10052 (Add download with async downlaod flow in knime-ui)

---
+++ .changeset/grumpy-oranges-clean.md
@@ -0,0 +1,5 @@
+---
+"@knime/components": patch
+---
+
+add forgotten export for useAutoCloseOnCompletion composable
--- packages/components/src/composables/index.ts
+++ packages/components/src/composables/index.ts
@@ -7,5 +7,6 @@ export * from "./hints";
 export * from "./useNameValidator";
 export * from "./useFileSizeFormatting";
 export * from "./useBeforeUnload";
+export * from "./useAutoCloseOnCompletion";
 
 export { useClickOutside, useDropdownNavigation, useKeyPressedUntilMouseClick };
