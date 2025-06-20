3108ec1c330023856ba53d58e32fc827cdbb7491
bitbucket-pipelines <commits-noreply@bitbucket.org>
2025-06-20 08:43:28 +0000

    Deploy to GitHub pages

--- assets/AutoPlayVideo-C-KtTbo_.js
+++
@@ -1,6 +0,0 @@
-import{h as n,i,o as a,c as r,b as e,j as d,k as l,w as s,u as c,A as u,l as p,d as m,e as _,t as y,_ as v}from"./index-BZfwzlVL.js";import{C as f}from"./CodeExample-Cn_8DBn4.js";const g={class:"grid-container"},x={class:"grid-item-12"},V={class:"toggle"},k={class:"examples"},B=`<AutoPlayVideo aspect-ratio="16:9">
-  <source
-    src="https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
-    type="video/mp4"
-  />
-  </AutoPlayVideo>`,b=n({__name:"AutoPlayVideo",setup(h){const t=i(!1);return(A,o)=>(a(),r("section",null,[e("div",g,[e("div",x,[o[2]||(o[2]=e("p",null," Plays a video on render. Videos have a fixed aspect ratio 256:135 or 16:10 or 16:9. ",-1)),e("p",V,[e("a",{href:"#",onClick:o[0]||(o[0]=d(P=>t.value=!t.value,["prevent"]))},"Toggle AutoPlayVideo component")]),e("div",k,[t.value?(a(),l(c(u),{key:0,"aspect-ratio":"16:9"},{default:s(()=>o[1]||(o[1]=[e("source",{src:"https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4",type:"video/mp4"},null,-1)])),_:1})):p("",!0)]),m(f,{summary:"Show usage example"},{default:s(()=>[_(y(B))]),_:1})])])]))}}),N=v(b,[["__scopeId","data-v-8aa644d5"]]);export{N as default};
