70eb9da1e8b786b011d295a1643cb620858e5ec1
Helian Rivera <helian.rivera@knime.com>
2025-07-23 14:48:33 +0200

    allow Open Font License on the license checker
    - remove manual font license override

--- packages/licenses/config/license.config.js
+++ packages/licenses/config/license.config.js
@@ -14,6 +14,7 @@ export default {
     "Zlib",
     "Unlicense",
     "BlueOak-1.0.0",
+    "OFL-1.1",
   ],
 
   // e.g. 'nuxt@2.0.0'
@@ -41,21 +42,6 @@ export default {
   ],
 
   // manually add packages to the credits, e.g.:
-  // {name: 'myownpackage', licenses: 'MyOwnLicense 2.0', licenseText: 'Custom license text'}
-  manualPackages: [
-    {
-      name: "Roboto Font",
-      repository: "https://fonts.google.com/specimen/Roboto",
-      licenses: "Apache-2.0",
-      licenseText:
-        'Copyright 2011 Google Inc.\n\nLicensed under the Apache License, Version 2.0 (the "License");\nyou may not use this file except in compliance with the License.\nYou may obtain a copy of the License at\n\n    http://www.apache.org/licenses/LICENSE-2.0\n\nUnless required by applicable law or agreed to in writing, software\ndistributed under the License is distributed on an "AS IS" BASIS,\nWITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\nSee the License for the specific language governing permissions and\nlimitations under the License.',
-    },
-    {
-      name: "Roboto Mono Font",
-      repository: "https://fonts.google.com/specimen/Roboto+Mono",
-      licenses: "Apache-2.0",
-      licenseText:
-        'Copyright 2015 Google Inc.\n\nLicensed under the Apache License, Version 2.0 (the "License");\nyou may not use this file except in compliance with the License.\nYou may obtain a copy of the License at\n\n    http://www.apache.org/licenses/LICENSE-2.0\n\nUnless required by applicable law or agreed to in writing, software\ndistributed under the License is distributed on an "AS IS" BASIS,\nWITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\nSee the License for the specific language governing permissions and\nlimitations under the License.',
-    },
-  ],
+  // { name: 'myownpackage', licenses: 'MyOwnLicense 2.0', licenseText: 'Custom license text' }
+  manualPackages: [],
 };
