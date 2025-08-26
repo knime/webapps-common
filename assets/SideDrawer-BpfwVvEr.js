b2c60724d7410312e9a3c330ae210a5598261e80
bitbucket-pipelines <commits-noreply@bitbucket.org>
2025-08-26 16:11:17 +0000

    Deploy to GitHub pages

---
+++ assets/SideDrawer-BpfwVvEr.js
@@ -0,0 +1,61 @@
+import{C}from"./CodeExample-CLYvv6dc.js";import{S as v,a as g}from"./SideDrawerHeader-CU0KnGlq.js";import{h as _,i as w,K as x,c as S,o as D,b as s,d as t,w as d,e as a,u as i,C as o,t as n,_ as k}from"./index-Cgznq6yl.js";const E={class:"grid-container"},$={class:"grid-item-12 wrapper"},B={class:"contents-side-drawer"},I={class:"grid-container"},M={class:"grid-item-12 wrapper"},H={class:"contents-side-drawer"},q={class:"grid-container"},W={class:"grid-item-12 wrapper"},N={class:"contents-side-drawer"},P={class:"grid-container"},T={class:"grid-item-12 wrapper"},V={class:"contents-side-drawer"},A={class:"grid-container"},L={class:"grid-item-12"},O=`<script>
+import SideDrawer from '~/webapps-common/ui/components/SideDrawer.vue';
+import Button from '~/webapps-common/ui/components/Button.vue';
+
+export default {
+    components: {
+        SideDrawer,
+        Button,
+    },
+    data() {
+        return {
+            isExpanded: false
+        };
+    },
+    computed: {
+        expandedMessage() {
+            return this.isExpanded ? 'expanded' : 'not expanded';
+        }
+    }
+
+};
+<\/script>
+
+<template>
+  <SideDrawer
+      class="side-drawer"
+      :is-expanded="isExpanded"
+    >
+      <div class="contents-side-drawer">
+        <h4>something here</h4>
+        <p>And a lot more Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, nemo nostrum repellat voluptas,
+          nesciunt sequi velit earum iusto iste beatae hic perspiciatis deserunt exercitationem aut sapiente quas culpa
+          sint alias.</p>
+        <Button
+          with-border
+          @click="isExpanded = false"
+        >
+          Close me!
+        </Button>
+      </div>
+    </SideDrawer>
+</template>
+
+<style lang="postcss" scoped>
+
+h4 {
+  margin: 0;
+}
+
+.contents-side-drawer {
+  padding: 30px;
+  box-sizing: border-box;
+  background-color: var(--knime-white);
+  height: 100%;
+}
+
+</style>
+`,f=`And a lot more Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Enim, nemo nostrum repellat voluptas, nesciunt sequi velit earum
+          iusto iste beatae hic perspiciatis deserunt exercitationem aut
+          sapiente quas culpa sint alias.`,z=_({__name:"SideDrawer",setup(K){const u=w(!1),p=w(!1),m=w(!1),l=w(!1),h=x(()=>u.value?"expanded":"not expanded"),c=x(()=>p.value?"expanded (with custom styles)":"not expanded (so you can't see the custom styles)"),b=x(()=>m.value?"expanded (with header)":"not expanded (so you can't see the header)"),y=x(()=>l.value?"expanded (with sub drawer header)":"not expanded (so you can't see the sub drawer header)");return(j,e)=>(D(),S("div",null,[s("section",null,[s("div",E,[s("div",$,[e[10]||(e[10]=s("p",null," Provides an expandable drawer to the right side which can be filled with arbitrary content. On small screens it will take up the whole width. ",-1)),t(i(o),{primary:"",onClick:e[0]||(e[0]=r=>u.value=!u.value)},{default:d(()=>e[9]||(e[9]=[a(" Draw it! ",-1)])),_:1,__:[9]}),s("p",null,"I am "+n(h.value),1)])])]),t(i(v),{class:"side-drawer","is-expanded":u.value},{default:d(()=>[s("div",B,[e[12]||(e[12]=s("h4",null,"something here",-1)),s("p",null,n(f)),t(i(o),{"with-border":"",onClick:e[1]||(e[1]=r=>u.value=!1)},{default:d(()=>e[11]||(e[11]=[a(" Close me! ",-1)])),_:1,__:[11]})])]),_:1},8,["is-expanded"]),s("section",null,[s("div",I,[s("div",M,[e[14]||(e[14]=s("p",null,[a(" It is possible to override some styles of the drawer by using the "),s("code",null,"styleOverrides"),a(" prop. ")],-1)),t(i(o),{primary:"",onClick:e[2]||(e[2]=r=>p.value=!p.value)},{default:d(()=>e[13]||(e[13]=[a(" Draw it! ",-1)])),_:1,__:[13]}),s("p",null,"I am "+n(c.value),1)])])]),t(i(v),{class:"side-drawer","is-expanded":p.value,"style-overrides":{width:"50%"}},{default:d(()=>[s("div",H,[e[16]||(e[16]=s("h4",null,"something here",-1)),s("p",null,n(f)),t(i(o),{"with-border":"",onClick:e[3]||(e[3]=r=>p.value=!1)},{default:d(()=>e[15]||(e[15]=[a(" Close me! ",-1)])),_:1,__:[15]})])]),_:1},8,["is-expanded"]),s("section",null,[s("div",q,[s("div",W,[e[18]||(e[18]=s("p",null,"Provides an expandable drawer with header",-1)),t(i(o),{primary:"",onClick:e[4]||(e[4]=r=>m.value=!m.value)},{default:d(()=>e[17]||(e[17]=[a(" Draw it! ",-1)])),_:1,__:[17]}),s("p",null,"I am "+n(b.value),1)])])]),t(i(v),{class:"side-drawer","is-expanded":m.value,"style-overrides":{width:"50%"}},{default:d(()=>[t(i(g),{title:"My header",description:"This is a description"}),s("div",N,[e[20]||(e[20]=s("h4",null,"something here",-1)),s("p",null,n(f)),t(i(o),{"with-border":"",onClick:e[5]||(e[5]=r=>m.value=!1)},{default:d(()=>e[19]||(e[19]=[a(" Close me! ",-1)])),_:1,__:[19]})])]),_:1},8,["is-expanded"]),s("section",null,[s("div",P,[s("div",T,[e[22]||(e[22]=s("p",null,"Provides an expandable drawer with sub drawer header",-1)),t(i(o),{primary:"",onClick:e[6]||(e[6]=r=>l.value=!l.value)},{default:d(()=>e[21]||(e[21]=[a(" Draw it! ",-1)])),_:1,__:[21]}),s("p",null,"I am "+n(y.value),1)])])]),t(i(v),{class:"side-drawer","is-expanded":l.value,"style-overrides":{width:"50%"}},{default:d(()=>[t(i(g),{title:"My header",description:"This is a description",onClose:e[7]||(e[7]=r=>l.value=!1),"is-sub-drawer":!0}),s("div",V,[e[24]||(e[24]=s("h4",null,"something here",-1)),s("p",null,n(f)),t(i(o),{"with-border":"",onClick:e[8]||(e[8]=r=>l.value=!1)},{default:d(()=>e[23]||(e[23]=[a(" Close me! ",-1)])),_:1,__:[23]})])]),_:1},8,["is-expanded"]),s("section",null,[s("div",A,[s("div",L,[t(C,{summary:"Show usage example"},{default:d(()=>[a(n(O))]),_:1})])])])]))}}),Q=k(z,[["__scopeId","data-v-03e20caa"]]);export{Q as default};
---
+++ assets/TextArea-CoDmJeK2.js
@@ -0,0 +1,18 @@
+import{C as m}from"./CodeExample-CLYvv6dc.js";import{_ as V,r,c,o as x,b as l,e as o,d as u,t as s,w as d}from"./index-Cgznq6yl.js";import{T as v}from"./TextArea-D-_N9_az.js";const f="",I=`<TextArea
+  v-model="inputValue"
+  title="Insert text"
+/>
+<TextArea
+  v-model="inputValue2"
+  placeholder="I'm a placeholder."
+/>
+<TextArea
+  v-model="inputValue3"
+  :is-valid="false"
+/>
+<TextArea
+  v-model="inputValue4"
+  placeholder="I'm a placeholder."
+  :cols="48"
+  :rows="8"
+/>`,g={components:{TextArea:v,CodeExample:m},data(){return{codeExample:I,inputValue:"Default settings",inputValue2:"",inputValue3:"Invalid style",inputValue4:"I have 8 rows (4 is default) and 48 columns (12 is the default)."}},computed:{code(){return f}}},T={class:"grid-container"},A={class:"grid-item-6 inputs"},_={class:"grid-item-6"},w={class:"grid-container"},C={class:"grid-item-12"};function E(b,e,y,U,t,p){const a=r("TextArea",!0),i=r("CodeExample");return x(),c("div",null,[l("section",null,[e[8]||(e[8]=l("div",{class:"grid-container"},[l("div",{class:"grid-item-12"},[l("p",null,[o(" Multi-line string input with optional invalid styling. It acts as a form element, so it emits "),l("code",null,"input"),o(" events and it has a "),l("code",null,"value"),o(". ")])])],-1)),l("div",T,[l("div",A,[u(a,{modelValue:t.inputValue,"onUpdate:modelValue":e[0]||(e[0]=n=>t.inputValue=n),title:"Insert text"},null,8,["modelValue"]),u(a,{modelValue:t.inputValue2,"onUpdate:modelValue":e[1]||(e[1]=n=>t.inputValue2=n),placeholder:"I'm a placeholder."},null,8,["modelValue"]),u(a,{modelValue:t.inputValue3,"onUpdate:modelValue":e[2]||(e[2]=n=>t.inputValue3=n),"is-valid":!1},null,8,["modelValue"]),e[4]||(e[4]=l("br",null,null,-1)),u(a,{modelValue:t.inputValue4,"onUpdate:modelValue":e[3]||(e[3]=n=>t.inputValue4=n),placeholder:"I'm a placeholder.",cols:48,rows:8},null,8,["modelValue"])]),l("div",_,[o(" Input 1: "+s(t.inputValue)+" ",1),e[5]||(e[5]=l("br",null,null,-1)),o(" Input 2: "+s(t.inputValue2)+" ",1),e[6]||(e[6]=l("br",null,null,-1)),o(" Input 3: "+s(t.inputValue3)+" ",1),e[7]||(e[7]=l("br",null,null,-1)),o(" Input 4: "+s(t.inputValue4),1)])])]),l("section",null,[l("div",w,[l("div",C,[u(i,{summary:"Show usage example"},{default:d(()=>[o(s(t.codeExample),1)]),_:1}),u(i,{summary:"Show TextArea.vue source code"},{default:d(()=>[o(s(p.code),1)]),_:1})])])])])}const S=V(g,[["render",E],["__scopeId","data-v-09f42e34"]]);export{S as default};
