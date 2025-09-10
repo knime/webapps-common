515e827435ce1ea628c87e34e171a40ad5aba4ec
bitbucket-pipelines <commits-noreply@bitbucket.org>
2025-09-10 11:33:27 +0000

    Deploy to GitHub pages

--- assets/InlineMessage-CS-P1dYV.js
+++
@@ -1,8 +0,0 @@
-import{C as o}from"./CodeExample-ibKGFEFz.js";import{I as s}from"./InlineMessage-Dfle2UXF.js";import{h as l,c as m,o as d,b as a,d as e,u as t,w as n,e as i,t as c,_ as u}from"./index-B0fv7C7B.js";import"./rocket-CcoFradT.js";const p={class:"grid-container"},f={class:"grid-item-12"},h={class:"wrapper"},_="",g=`<InlineMessage
-  type="info"
-  title="Info"
-  description="Here is a message that informs the user"
-/>
-<InlineMessage variant="info" title="Info">
-  Here is a message that contains a <a href="/">link</a>.
-</InlineMessage>`,I=l({__name:"InlineMessage",setup(v){return(w,r)=>(d(),m("section",null,[a("div",p,[a("div",f,[a("div",h,[e(t(s),{variant:"info",title:"Info",description:"Here is a message that informs the user"}),e(t(s),{variant:"warning",title:"Warning",description:"Here is a message that warns the user"}),e(t(s),{variant:"error",title:"Error",description:"Here is a message that tells user about error and what to do"}),e(t(s),{variant:"success",title:"Success",description:"Here is a message that informs the user about success"}),e(t(s),{variant:"promotion",title:"Promotion",description:"Here is a message that informs the user about KNIME news"}),e(t(s),{variant:"info",title:"Info"},{default:n(()=>r[0]||(r[0]=[i(" Here is a message that contains a ",-1),a("a",{href:"/"},"link",-1),i(". ",-1)])),_:1,__:[0]})]),e(o,{summary:"Show usage example"},{default:n(()=>[i(c(g))]),_:1}),e(o,{summary:"Show InlineMessage.vue source code"},{default:n(()=>[i(c(_))]),_:1})])])]))}}),E=u(I,[["__scopeId","data-v-88db569a"]]);export{E as default};
