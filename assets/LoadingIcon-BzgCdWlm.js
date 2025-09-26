ae3e1ea99e5e60c8a469269a5178724e183f6398
bitbucket-pipelines <commits-noreply@bitbucket.org>
2025-09-26 09:58:37 +0000

    Deploy to GitHub pages

---
+++ assets/LoadingIcon-BzgCdWlm.js
@@ -0,0 +1,25 @@
+import{C as p}from"./CodeExample-DxaYbo_s.js";import{_ as m,r as s,c as l,o as g,b as o,d as e,w as c,e as i,t as d}from"./index-BRdCoBMt.js";import{L as u}from"./LoadingIcon-CJahnEA6.js";import"./svgWithTitle-BJVCvQtu.js";const _="",x=`
+<script>
+import LoadingIcon from '~/webapps-common/ui/components/LoadingIcon.vue';
+
+export default {
+    components: {
+        LoadingIcon
+    }
+};
+<\/script>
+
+<template>
+  <LoadingIcon class="loading-icon" />
+</template>
+
+<style lang="postcss" scoped>
+  .loading-icon {
+    width: 24px;
+    height: 24px;
+    stroke-width: calc(32px / 24);
+    margin: auto;
+    stroke: var(--knime-masala);
+  }
+</style>
+`,f={components:{LoadingIcon:u,CodeExample:p},data(){return{code:_,codeExample:x}}},I={class:"grid-container"},L={class:"grid-item-12"};function h(v,n,w,C,a,E){const r=s("LoadingIcon",!0),t=s("CodeExample");return g(),l("section",null,[o("div",I,[o("div",L,[n[0]||(n[0]=o("p",null,"Animated loading icon",-1)),e(r,{class:"loading-icon"}),e(t,{summary:"Show usage example"},{default:c(()=>[i(d(a.codeExample),1)]),_:1}),e(t,{summary:"Show LoadingIcon.vue source code"},{default:c(()=>[i(d(a.code),1)]),_:1})])])])}const S=m(f,[["render",h],["__scopeId","data-v-128a4a22"]]);export{S as default};
