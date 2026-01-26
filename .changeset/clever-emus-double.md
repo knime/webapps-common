12ff5ea7b04c0260cb2fe90f7dbc824f9d533e12
Robin O. <80898166+thresharx@users.noreply.github.com>
2026-01-26 16:49:21 +0100

    HUB-12165: adjust nuxt eslint config and readme (#150)
    HUB-12165 (fix nuxt eslint config)

---
+++ .changeset/clever-emus-double.md
@@ -0,0 +1,5 @@
+---
+"@knime/eslint-config": patch
+---
+
+unwrap nuxt config as it needs to be wrapped with nuxt helpers in the nuxt project
--- packages/lint/nuxt3.js
+++ packages/lint/nuxt3.js
@@ -1,25 +1,26 @@
-import { createConfigForNuxt } from "@nuxt/eslint-config";
-
 import createKnimeVueTSConfig from "./vue3-typescript.js";
 
 const createKnimeNuxtConfig = (tsconfig) => {
   const vue3TsConfig = createKnimeVueTSConfig(tsconfig);
-  // nuxt provides factory function for config customization
-  return createConfigForNuxt(undefined, ...vue3TsConfig, {
-    languageOptions: {
-      globals: {
-        createError: true,
-        defineNuxtConfig: true,
-        defineNuxtPlugin: true,
-        defineNuxtRouteMiddleware: true,
-        markRaw: true,
-        navigateTo: true,
-        useAsyncData: true,
-        useNuxtApp: true,
-        useRoute: true,
+
+  return [
+    ...vue3TsConfig,
+    {
+      languageOptions: {
+        globals: {
+          createError: true,
+          defineNuxtConfig: true,
+          defineNuxtPlugin: true,
+          defineNuxtRouteMiddleware: true,
+          markRaw: true,
+          navigateTo: true,
+          useAsyncData: true,
+          useNuxtApp: true,
+          useRoute: true,
+        },
       },
     },
-  });
+  ];
 };
 
 export default createKnimeNuxtConfig;
