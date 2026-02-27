e048c8910af0d3fe612aa463b0c05c845879e80b
Vitomir Budimir <13198821+hejtful@users.noreply.github.com>
2026-02-27 09:20:44 +0100

    NXT-4471: Wrap SkeletonItem in a div element to fix SSR style bug
    NXT-4471

---
+++ .changeset/sixty-ads-ring.md
@@ -0,0 +1,5 @@
+---
+"@knime/components": patch
+---
+
+Wrap SkeletonItem in a div element to fix SSR style bug
--- packages/components/src/components/SkeletonItem/SkeletonItem.vue
+++ packages/components/src/components/SkeletonItem/SkeletonItem.vue
@@ -84,7 +84,7 @@ const styles = computed(() => {
 </script>
 
 <template>
-  <template v-if="loading">
+  <div v-if="loading" class="wrapper">
     <div
       v-for="index in repeat"
       :key="index"
@@ -92,7 +92,7 @@ const styles = computed(() => {
       v-bind="$attrs"
       :style="styles"
     />
-  </template>
+  </div>
   <slot v-else v-bind="$attrs" />
 </template>
 
@@ -101,6 +101,10 @@ const styles = computed(() => {
   to {
     background-position-x: -200%;
   }
+}
+
+.wrapper {
+  display: contents;
 }
 
 .skeleton-item {
