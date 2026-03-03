303371d5b1a9b207bc2da863465e133ea339d345
Robin Offel <robin.offel@knime.com>
2026-03-03 12:20:55 +0100

    HUB-12405: clean up ec config
    HUB-12405 (Migrate executionDropdownRenderer)

--- packages/jsonforms/src/uiComponents/configs/executionContexts.config.ts
+++ packages/jsonforms/src/uiComponents/configs/executionContexts.config.ts
@@ -16,36 +16,6 @@ export const EXECUTION_CONTEXT_SHARED_STATUS = Object.freeze({
   DEFAULT: "Default",
 });
 
-export const EXECUTION_CONTEXT_CAPACITY = Object.freeze({
-  SMALL: "small",
-  MEDIUM: "medium",
-  LARGE: "large",
-});
-export const EXECUTION_CONTEXT_VCORES = Object.freeze({
-  [EXECUTION_CONTEXT_CAPACITY.SMALL]: 2,
-  [EXECUTION_CONTEXT_CAPACITY.MEDIUM]: 4,
-  [EXECUTION_CONTEXT_CAPACITY.LARGE]: 8,
-});
-
-export const EXECUTION_CONTEXT_MEMORY_IN_MB = Object.freeze({
-  [EXECUTION_CONTEXT_CAPACITY.SMALL]: 8,
-  [EXECUTION_CONTEXT_CAPACITY.MEDIUM]: 16,
-  [EXECUTION_CONTEXT_CAPACITY.LARGE]: 32,
-});
-
-export const EXECUTION_CONTEXT_DESCRIPTIONS = Object.freeze({
-  [EXECUTION_CONTEXT_CAPACITY.SMALL]:
-    "Suitable for lightweight data processing with low complexity workflows. Slightly slower than your average home computer but good enough for repetitive, lightweight automation.",
-  [EXECUTION_CONTEXT_CAPACITY.MEDIUM]:
-    "Suitable for most data processing use cases with medium complex workflows. Similar to the performance of KNIME Analytics Platform on an average home PC.",
-  [EXECUTION_CONTEXT_CAPACITY.LARGE]:
-    "For more demanding use cases with more data, complex workflows and more parallel processing and faster computation times required.",
-});
-
-export const PRODUCT_METADATA_BASE_FIELD = "knime.hub.product.base";
-export const EXECUTION_BASE_PRODUCT_ID = "execution-payg-base";
-export const SHARED_EXECUTION_CONTEXT = "hub:global";
-
 export const EXECUTION_CONTEXT_STATUS_TO_CLASS = {
   [EXECUTION_CONTEXT_STATUS.RUNNING]: "status-running",
   [EXECUTION_CONTEXT_STATUS.STOPPED]: "status-stopped",
