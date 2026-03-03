102080f1010767dbbc530c7b70f43c761b5242b7
Robin O. <80898166+thresharx@users.noreply.github.com>
2026-03-03 13:47:05 +0100

    HUB-12414: add link renderer (#203)
    HUB-12414 (Migrate linkRenderer)

---
+++ packages/jsonforms/src/uiComponents/LinkControl.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { KdsLinkButton } from "@knime/kds-components";
+
+import type { VueControlProps } from "../higherOrderComponents";
+
+const props = defineProps<VueControlProps<void>>();
+
+const alignment =
+  props.control.uischema.options?.alignment === "right"
+    ? "flex-end"
+    : "flex-start";
+
+const { to, leadingIcon, trailingIcon } = props.control.uischema.options || {};
+</script>
+
+<template>
+  <div class="link-wrapper">
+    <KdsLinkButton
+      class="link-button"
+      :label="control.label"
+      :to="to"
+      :leading-icon="leadingIcon"
+      :trailing-icon="trailingIcon"
+      size="small"
+      variant="transparent"
+    />
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.link-wrapper {
+  display: flex;
+  justify-content: v-bind(alignment);
+  width: 100%;
+}
+</style>
