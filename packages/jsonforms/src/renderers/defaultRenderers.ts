00d5b28bf265d9416b95ee0f0c77b48a2a0748ca
Robin O. <80898166+thresharx@users.noreply.github.com>
2026-03-05 17:40:21 +0100

    HUB-12408: add quantity renderer (#209)
    HUB-12408 (Replace and migrate use of QuantityInput with KdsNumberInput)

--- packages/jsonforms/src/renderers/defaultRenderers.ts
+++ packages/jsonforms/src/renderers/defaultRenderers.ts
@@ -31,6 +31,7 @@ import { localTimeRenderer } from "./localTimeRenderer";
 import { multiSelectListBoxRenderer } from "./multiSelectListBoxRenderer";
 import { nameFilterRenderer } from "./nameFilterRenderer";
 import { numberRenderer } from "./numberRenderer";
+import { quantityRenderer } from "./quantityRenderer";
 import { radioRenderer } from "./radioRenderer";
 import { richTextRenderer } from "./richTextRenderer";
 import { sectionLayoutRenderer } from "./sectionLayoutRenderer";
@@ -81,6 +82,7 @@ export const controls = {
   singleSelectRenderer,
   gridSelectionRenderer,
   timeframesRenderer,
+  quantityRenderer,
   // without label:
   simpleButtonRenderer,
   textMessageRenderer,
