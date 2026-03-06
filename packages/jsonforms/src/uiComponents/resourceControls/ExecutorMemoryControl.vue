8e86e2dc602722300d078092bfcb31a6560e459c
Robin O. <80898166+thresharx@users.noreply.github.com>
2026-03-06 12:32:59 +0100

    HUB-12409: add resource renderers (#223)
    * HUB-12409: add resource renderers
    * HUB-12409: add tests and remove need for useStaticCurrentUsage option
    
    HUB-12409 (Replace and migrate use of TeamResourcesInputWrapper and ExecutorCoresInput with KdsResourceInput)

---
+++ packages/jsonforms/src/uiComponents/resourceControls/ExecutorMemoryControl.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import type { JsonSchema } from "@jsonforms/core";
+
+import type { VueControlPropsForLabelContent } from "../../higherOrderComponents";
+
+import ResourceControlBase from "./ResourceControlBase.vue";
+
+const props = defineProps<VueControlPropsForLabelContent<number>>();
+
+const secondaryValue = computed(
+  () =>
+    (props.control.schema as JsonSchema & { secondaryValue?: number })
+      .secondaryValue || 0,
+);
+const donutTitle = computed(
+  () => props.control.uischema.options?.donutTitle || "",
+);
+const currentUsage = computed(
+  () => props.control.uischema.options?.currentUsage || 0,
+);
+const donutMax = computed(() => props.control.uischema.options?.donutMax);
+
+const totalUse = computed(() => secondaryValue.value + currentUsage.value);
+
+const infoText = computed(
+  () =>
+    `This execution context will use a maximum of ${secondaryValue.value} GB RAM.
+    In total, ${totalUse.value} GB of ${donutMax.value} GB available for this team will be used.`,
+);
+</script>
+
+<template>
+  <ResourceControlBase v-bind="props">
+    <template #donut-info>
+      <div class="info-container">
+        <div class="chart-title">{{ donutTitle }}</div>
+        <div class="chart-description">
+          {{ infoText }}
+        </div>
+      </div>
+    </template>
+  </ResourceControlBase>
+</template>
+
+<style lang="postcss" scoped>
+.info-container {
+  display: flex;
+  flex-direction: column;
+
+  & .chart-title {
+    font: var(--kds-font-base-title-small);
+  }
+
+  & .chart-description {
+    font: var(--kds-font-base-body-small);
+  }
+}
+</style>
